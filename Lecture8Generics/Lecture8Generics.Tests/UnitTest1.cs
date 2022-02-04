bc55134ea6da9bb0c3b86b70d1ee6802e00905ee
Grant Erickson <grant@IntelliTect.com>
2022-02-03 16:11:42 -0800

    Middle of class

--- Lecture8Generics/Lecture8Generics.Tests/UnitTest1.cs
+++ Lecture8Generics/Lecture8Generics.Tests/UnitTest1.cs
@@ -17,7 +17,7 @@ namespace Lecture8Generics.Tests
         {
             Dictionary<int, string> dictionary = GetTestDictionary();
 
-            Assert.AreEqual("Inigo Montoya", dictionary.Get(42));
+            Assert.AreEqual("Inigo Montoya", dictionary[42]);
         }
 
 
@@ -28,6 +28,34 @@ namespace Lecture8Generics.Tests
 
             Assert.IsTrue(dictionary.KeyExists(42));
         }
+
+        [TestMethod]
+        public void Count()
+        {
+            Dictionary<int, string> dictionary = GetTestDictionary();
+
+            Assert.AreEqual(3, dictionary.Count);
+        }
+
+        [TestMethod]
+        public void UpdateValue()
+        {
+            Dictionary<int, string> dictionary = GetTestDictionary();
+            dictionary[42] = "Day";
+
+            Assert.AreEqual("Day", dictionary[42]);
+        }
+
+        [TestMethod]
+        public void Remove()
+        {
+            Dictionary<int, string> dictionary = GetTestDictionary();
+            dictionary.Remove(42);
+
+            Assert.IsFalse(dictionary.KeyExists(42));
+            Assert.AreEqual(2, dictionary.Count);
+        }
+
         private static Dictionary<int, string> GetTestDictionary()
         {
             Dictionary<int, string> dictionary = new();
--- Lecture8Generics/Lecture8Generics/Dictionary.cs
+++ Lecture8Generics/Lecture8Generics/Dictionary.cs
@@ -7,16 +7,42 @@
         private List<KeyValuePair<TKey,TValue>> _List = new();
         public void Add(TKey key,TValue value)
         {
+            if (KeyExists(key)) { throw new ArgumentException("key already exists"); }
             _List.Add(new KeyValuePair<TKey,TValue>(key, value));
         }
 
-        public TValue Get(TKey key)
+        public TValue this[TKey key]
         {
-            foreach(var kvp in _List)
+            get
             {
-                if(key.Equals(kvp.Key))
+                foreach (var kvp in _List)
                 {
-                    return kvp.Value;
+                    if (key.Equals(kvp.Key))
+                    {
+                        return kvp.Value;
+                    }
+                }
+                throw new KeyNotFoundException($"{key} not in dictionary");
+            }
+            set
+            {
+                // see if exists
+                if (KeyExists(key)) {
+                    // find kvp
+                    Remove(key);
+                }
+                Add(key, value);
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            foreach (var kvp in _List)
+            {
+                if (key.Equals(kvp.Key))
+                {
+                    _List.Remove(kvp);
+                    return;
                 }
             }
             throw new KeyNotFoundException($"{key} not in dictionary");
@@ -26,12 +52,14 @@
         {
             foreach (var kvp in _List)
             {
-                if (key.Equals(kvp))
+                if (key.Equals(kvp.Key))
                 {
                     return true;
                 }
             }
             return false;
         }
+
+        public int Count { get { return _List.Count; } }
     }
 }
