684548eebf2dcd288537ff4175379514705a88a0
Mark Michaelis <mark@IntelliTect.com>
2022-02-06 21:27:19 -0800

    Added assignment 5&6

---
+++ Assignment/Assignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Assignment
+{
+    public class Person : IPerson
+    {
+        public Person(string firstName, string lastName, IAddress address, string emailAddress)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            EmailAddress = emailAddress;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IAddress Address { get;set; }
+        public string EmailAddress { get; set; }
+    }
+}
