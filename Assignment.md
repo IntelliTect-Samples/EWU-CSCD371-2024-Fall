9d7782030fffd7fa5a76473e647a41b5cd3a505e
Benjamin Michaelis <benjamin@michaelis.net>
2023-01-02 16:31:44 -0800

    Add assignment 1

---
+++ Assignment.md
@@ -0,0 +1,10 @@
+# Assignment
+
+- Pull Request title includes "Assignment 1" in the title ✔❌
+- Pull Request targets Assignment 1 branch ✔❌
+- Issue 1: Application no longer crashes ✔❌
+- Issue 2: Unit test properly passes ✔❌
+
+### Extra Credit
+- Issue 3: Feature request implemented ✔❌
+- Issue 3: Feature request unit tested ✔❌
---
+++ PrincessBrideTrivia/PrincessBrideTrivia.Tests/ProgramTests.cs
@@ -0,0 +1,90 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace PrincessBrideTrivia.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void LoadQuestions_RetrievesQuestionsFromFile()
+        {
+            string filePath = Path.GetRandomFileName();
+            try
+            {
+                // Arrange
+                GenerateQuestionsFile(filePath, 2);
+
+                // Act
+                Question[] questions = Program.LoadQuestions(filePath);
+
+                // Assert 
+                Assert.AreEqual(2, questions.Length);
+            }
+            finally
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("1", true)]
+        [DataRow("2", false)]
+        public void DisplayResult_ReturnsTrueIfCorrect(string userGuess, bool expectedResult)
+        {
+            // Arrange
+            Question question = new Question();
+            question.CorrectAnswerIndex = "1";
+
+            // Act
+            bool displayResult = Program.DisplayResult(userGuess, question);
+
+            // Assert
+            Assert.AreEqual(expectedResult, displayResult);
+        }
+
+        [TestMethod]
+        public void GetFilePath_ReturnsFileThatExists()
+        {
+            // Arrange
+
+            // Act
+            string filePath = Program.GetFilePath();
+
+            // Assert
+            Assert.IsTrue(File.Exists(filePath));
+        }
+
+        [TestMethod]
+        [DataRow(1, 1, "100%")]
+        [DataRow(5, 10, "50%")]
+        [DataRow(1, 10, "10%")]
+        [DataRow(0, 10, "0%")]
+        public void GetPercentCorrect_ReturnsExpectedPercentage(int numberOfCorrectGuesses, 
+            int numberOfQuestions, string expectedString)
+        {
+            // Arrange
+
+            // Act
+            string percentage = Program.GetPercentCorrect(numberOfCorrectGuesses, numberOfQuestions);
+
+            // Assert
+            Assert.AreEqual(expectedString, percentage);
+        }
+
+
+        private static void GenerateQuestionsFile(string filePath, int numberOfQuestions)
+        {
+            for (int i = 0; i < numberOfQuestions; i++)
+            {
+                string[] lines = new string[5];
+                lines[0] = "Question " + i + " this is the question text";
+                lines[1] = "Answer 1";
+                lines[2] = "Answer 2";
+                lines[3] = "Answer 3";
+                lines[4] = "2";
+                File.AppendAllLines(filePath, lines);
+            }
+        }
+    }
+}
