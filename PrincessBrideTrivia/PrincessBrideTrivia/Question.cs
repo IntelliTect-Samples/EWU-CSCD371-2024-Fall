a541adb45ba174d4602ec4e3507ba5c0da52ce87
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-12-29 17:37:04 -0800

    Don't expose public instance field

--- PrincessBrideTrivia/PrincessBrideTrivia/Question.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Question.cs
@@ -2,7 +2,7 @@
 
 public class Question
 {
-    public string Text;
-    public string[] Answers;
-    public string CorrectAnswerIndex;
+    public string Text { get; set; }
+    public string[] Answers { get; set; }
+    public string CorrectAnswerIndex { get; set; }
 }
