c25f5e201ce4af881ae72ebd60c93aa00d378acc
Leo Marlow <lmarlow@ewu.edu>
2024-09-28 19:43:38 -0700

    Added QuestionRandomizer function

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace PrincessBrideTrivia;
+﻿using Microsoft.VisualBasic;
+using System.Security.Cryptography;
+
+namespace PrincessBrideTrivia;
 
 public class Program
 {
@@ -66,11 +69,12 @@ public class Program
     public static Question[] LoadQuestions(string filePath)
     {
         string[] lines = File.ReadAllLines(filePath);
+        int[] questionIndex = QuestionRandomizer(lines.Length / 5);
 
         Question[] questions = new Question[lines.Length / 5];
         for (int i = 0; i < questions.Length; i++)
         {
-            int lineIndex = i * 5;
+            int lineIndex = questionIndex[i] * 5; 
             string questionText = lines[lineIndex];
 
             string answer1 = lines[lineIndex + 1];
@@ -90,5 +94,18 @@ public class Program
         }
 
         return questions;
+    }
+    public static int[] QuestionRandomizer(int numberOfQuestions) 
+    {
+        int[] randomArray = new int[numberOfQuestions];
+
+        for (int i = 0; i < numberOfQuestions; i++)
+        {
+            randomArray[i] = i;   
+        }
+
+        Random.Shared.Shuffle(randomArray);
+
+        return randomArray;
     }
 }
