71a35a28c169770935ba980f6630276252739f4e
Kmiller <kmiller71@ewu.edu>
2024-10-01 21:20:07 -0700

    Fixed GetPercentCorrect
    Cast numberCorrectAnswers and numberOfQuestions to doubles.

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -21,7 +21,7 @@ public class Program
 
     public static string GetPercentCorrect(int numberCorrectAnswers, int numberOfQuestions)
     {
-        return (numberCorrectAnswers / numberOfQuestions * 100) + "%";
+        return ((double)numberCorrectAnswers / (double)numberOfQuestions * 100) + "%";
     }
 
     public static bool AskQuestion(Question question)
