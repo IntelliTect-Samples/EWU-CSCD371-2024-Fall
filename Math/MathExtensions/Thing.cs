d5611c7ac366bb920ca05aa08624795b223f06dc
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-10-03 15:27:01 -0700

    First half of lecture

---
+++ Math/MathExtensions/Thing.cs
@@ -0,0 +1,44 @@
+﻿namespace MathExtensions;
+
+public class Thing
+{
+    public Thing()
+    {
+        Age = 0;
+    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Name
+    {
+        get => FirstName + " " + LastName;
+        set
+        {
+            string[] names = value.Split(' ');
+            if (names.Length == 2)
+            {
+                FirstName = names[0];
+                LastName = names[1];
+            }
+            else
+            {
+                throw new ArgumentException("Name must contain a first and last name.");
+            }
+        }
+    }
+
+    private float _age;
+
+    public float Age
+    {
+        get { return _age; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age cannot be negative.");
+            }
+            _age = value;
+        }
+    }
+}
---
+++ Math/MathTest/ThingTests.cs
@@ -0,0 +1,69 @@
+﻿using MathExtensions;
+
+namespace MathTest;
+
+[TestClass]
+public sealed class ThingTests
+{
+    [TestMethod]
+    public void Name_InigoMontoya_Success()
+    {
+        // Arrange
+        string name = "Inigo Montoya";
+        // Act
+        Thing thing = new();
+        thing.Name = name;
+        // Assert
+        Assert.AreEqual(name, thing.Name);
+    }
+
+    [TestMethod]
+    public void Age_42_Success()
+    {
+        // Arrange
+        float age = 42;
+        // Act
+        Thing thing = new();
+        thing.Age = age;
+        // Assert
+        Assert.AreEqual(age, thing.Age);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Age_SetNegativeNumber_ThrowsException()
+    {
+        // Arrange
+        Thing thing = new();
+        // Act
+        thing.Age = -1;
+        // Assert
+        // Assert is managed by the Attribute [ExpectedException]
+    }
+
+    [TestMethod]
+    public void FirstName_Inigo_Success()
+    {
+        // Arrange
+        string firstName = "Inigo";
+        // Act
+        Thing thing = new();
+        thing.FirstName = firstName;
+        // Assert
+        Assert.AreEqual(firstName, thing.FirstName);
+    }
+
+    [TestMethod]
+    public void Name_Set_AssignsFirstName()
+    {
+        // Arrange
+        string name = "Inigo Montoya";
+        Thing thing = new();
+        // Act
+        thing.Name = name;
+        // Assert
+        Assert.AreEqual("Inigo", thing.FirstName);
+    }
+
+
+}
