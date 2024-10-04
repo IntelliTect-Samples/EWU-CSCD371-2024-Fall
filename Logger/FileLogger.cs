ba943c6d49d7722e74e31470459b94edded967a8
Windeko1 <twoody1@ewu.edu>
2024-10-03 19:19:19 -0700

    Added FileLogger class, and made it pass the test from FileLoggerTests

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Logger;
+public class FileLogger : BaseLogger
+{
+    public override string ClassName { get; set; }
+
+    public override void Log(LogLevel logLevel, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+}
