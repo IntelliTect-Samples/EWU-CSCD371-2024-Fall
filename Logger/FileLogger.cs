ce5139642e0114657441d2641325a5f811bb077a
Oscar Ruelas <oruelas@ewu.edu>
2024-10-04 21:23:14 -0700

    How about now

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
-using System.Text;
 
 
 namespace Logger;
@@ -18,20 +17,7 @@ public class FileLogger : BaseLogger
 
     public override void Log(LogLevel level, string message)
     {
-        //var logEntry = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture)} {ClassName} {level}: {message}{Environment.NewLine}";
-        
-        var logEntryBuilder = new StringBuilder();
-        logEntryBuilder.Append(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture));
-        logEntryBuilder.Append(" ");
-        logEntryBuilder.Append(ClassName);
-        logEntryBuilder.Append(" ");
-        logEntryBuilder.Append(level);
-        logEntryBuilder.Append(": ");
-        logEntryBuilder.Append(message);
-        logEntryBuilder.Append(Environment.NewLine);
-
-        string logEntry = logEntryBuilder.ToString();
-
+        var logEntry = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture)} {ClassName} {level}: {message}{Environment.NewLine}";
         File.AppendAllText(_filePath, logEntry);
     }
 
