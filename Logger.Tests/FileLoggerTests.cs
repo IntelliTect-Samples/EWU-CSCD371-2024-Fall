1ec8975817f805eca1dbfbf7d1a5650713acdb26
Windeko <tylerjameswoody@gmail.com>
2024-10-04 23:42:52 -0700

    Created a test for the FileLogger constructor before I implement the feature.

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -10,10 +10,27 @@ public class FileLoggerTests
     {
         //Arrange
         string className = "TestClass";
+
         //Act
         FileLogger logger = new();
         logger.ClassName = className;
+
         //Assert
         Assert.AreEqual(className, logger.ClassName);
+    }
+
+    [TestMethod]
+    public void FileLogger_TestInitializeFilePath_True()
+    {
+        //Arrange
+        string filePath = "TestPath";
+
+        //Act
+        FileLogger logger = new();
+        logger.FileLogger(filePath);
+
+        //Assert
+        Assert.AreEqual(filePath, logger.FileLogger);
+
     }
 }
