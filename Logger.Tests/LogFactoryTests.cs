e238d14f5182b5b21007ef7d614f47eadae1477e
Windeko <tylerjameswoody@gmail.com>
2024-10-04 23:25:33 -0700

    Created a test for LogFactory's CreateLogger where it simulates not being configured.

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logger.Tests;
 
@@ -6,4 +7,21 @@ namespace Logger.Tests;
 public class LogFactoryTests
 {
 
+    /*
+     * This test expects CreateLogger to return null when there isn't a file path configured for LogFactory
+     * because I haven't implemented the ConfigureFileLogger method, resulting in the factory being unconfigured.
+     */
+
+    [TestMethod]
+    public void CreateLogger_NoConfiguration_Null()
+    {
+        //Arrange
+        var factory = new LogFactory();
+
+        //Act
+        var logger = factory.CreateLogger("TestClass");
+
+        //Assert
+        Assert.IsNull(logger);
+    }
 }
