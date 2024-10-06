cd8018dc98307abdb5f010570645fb1f2bf90375
Jake Lucas <156277910+JacobLucas02@users.noreply.github.com>
2024-10-05 22:45:14 -0700

    Update BaseLogger.cs

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -1,7 +1,15 @@
-﻿namespace Logger;
+namespace Logger;
 
 public abstract class BaseLogger
 {
+    public string ClassName {
+        get;
+        set;
+    }
+
+    public BaseLogger(string className) {
+        ClassName = className;
+    }
+
     public abstract void Log(LogLevel logLevel, string message);
 }
-
