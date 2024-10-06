af851a6a587058a02f7e02443e65b905665fb345
Brady Dullanty <bdullanty@ewu.edu>
2024-10-06 13:16:12 -0700

    Added in ConfireFileLogger_GivenString_StoresValue test

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class LogFactoryTests
 {
+    [TestMethod]
+    [DataRow("")]
+    [DataRow("./")]
+    [DataRow("/C:/Users/test/Documents")]
+
+    public void ConfigureFileLogger_GivenString_StoresValue(string path)
+    {
+        //arrange
+        LogFactory factory = new();
+        //act
+        factory.ConfigureFileLogger(path);
+        //assert
+        Assert.AreEqual(factory.FilePath, path);
+    }
 
 }
