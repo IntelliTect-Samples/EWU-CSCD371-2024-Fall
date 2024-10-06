21c2b1ac5111460887b002e489463811b4b1560b
Brady Dullanty <bdullanty@ewu.edu>
2024-10-06 13:58:53 -0700

    Created the FileLogger class, as well as a constructor and log override

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    public class FileLogger : BaseLogger
+    {
+
+        public string Path { get; private set; }
+
+        public FileLogger(String path) { 
+         this.Path = path;
+        }
+        
+        public override void Log(LogLevel logLevel, string message)
+        {
+            //set info in file.
+        }
+    }
+}
