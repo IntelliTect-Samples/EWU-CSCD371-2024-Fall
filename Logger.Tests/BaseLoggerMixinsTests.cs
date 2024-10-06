3ddb564fe3befa045cd53dfd5bf7556c2e782154
Brady Dullanty <bdullanty@ewu.edu>
2024-10-06 10:01:59 -0700

    Added in Information_WithValidData_LogsMessage method

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -119,6 +119,41 @@ public class BaseLoggerMixinsTests
         // Assert
         //Assertion done with [ExpectedException] above
     }
+    [TestMethod]
+    //Format 
+    [DataRow("Message {0}", "Message 2", new object[] { 2 })]
+    [DataRow("Message {0}", "Message 2", new object[] { "2" })]
+    [DataRow("Message {0}", "Message hello.", new object[] { "hello." })]
+    [DataRow("Message {0}{0}", "Message repeatrepeat", new object[] { "repeat" })]
+    [DataRow("Message {0}{1}", "Message hello.2", new object[] { "hello.", 2 })]
+    [DataRow("Message {1}{0}", "Message hello.2", new object[] { 2, "hello." })]
+    [DataRow("Message {0}{1}", "Message hello.2", new object[] { "hello.", 2, " third" })]
+    [DataRow("Message {2}{1}{0}", "Message third2hello.", new object[] { "hello.", 2, "third" })]
+    //Empty stuff
+    [DataRow("Empty Message: {0}", "Empty Message: ", new object[] { "" })]
+    [DataRow("", "", new object[] { "test" })]
+    //null tests
+    [DataRow("Empty Message: {0}", "Empty Message: ", new object[] { null })]
+    [DataRow(null, "Null message passed in", new object[] { "String here" })]
+    [DataRow("", "", null)]
+
+    public void Information_WithValidData_LogsMessage(string message, string result, object[] insertionValues)
+    {
+
+        // Arrange
+        var logger = new TestLogger();
+
+        // Act
+        logger.Information(message, insertionValues);
+
+        // Assert
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
+        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[0].LogLevel);
+        Assert.AreEqual(result, logger.LoggedMessages[0].Message);
+    }
+
+
+
     [TestCategory("Debug")]
 
 
