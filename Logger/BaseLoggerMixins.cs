128b2ea311a7fd7f8fe330332d381150e8dfc55d
Eric Vo <tvo12@ewu.edu>
2024-10-05 23:18:20 -0700

    BaseLoggerMixin Update

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,6 +1,43 @@
 ﻿namespace Logger;
 
+using System;
+
+
 public static class BaseLoggerMixins
 {
+    public static void Error(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger), "Logger is null");
+        }
+        logger.Log(LogLevel.Error, string.Format(message, args));
+    }
 
+    public static void Warning(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger), "Logger is null");
+        }
+        logger.Log(LogLevel.Warning, string.Format(message, args));
+    }
+
+    public static void Information(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger), "Logger is null");
+        }
+        logger.Log(LogLevel.Information, string.Format(message, args));
+    }
+
+    public static void Debug(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger), "Logger is null");
+        }
+        logger.Log(LogLevel.Debug, string.Format(message, args));
+    }
 }
