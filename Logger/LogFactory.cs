d903ceb3d307608eec0105121ecdde58c4f1a508
Brady Dullanty <bdullanty@ewu.edu>
2024-10-06 13:17:15 -0700

    Added in method to set file logger path

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,11 +1,20 @@
+﻿using System;
+
 namespace Logger;
 
 public class LogFactory
+    
 {
+
     public string FilePath { get; set; }
+
+    public void ConfigureFileLogger(string path)
+    {
+       this.FilePath = path;
+    }
+
     public BaseLogger CreateLogger(string className)
     {
-
         return null;
     }
 }
