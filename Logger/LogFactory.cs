eec465c994b4749ff9fefc1b7934c78f1560179a
Eric Vo <tvo12@ewu.edu>
2024-10-05 21:17:22 -0700

    Add ConfigureFileLogger

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,10 +1,29 @@
 ﻿namespace Logger;
-
+using System.IO;
+using System.Reflection;
 public class LogFactory
 {
     public BaseLogger CreateLogger(string className)
     {
 
         return null;
+    }
+
+    private string? _filePath;
+    public void ConfigureFileLogger(string filePath)
+    {
+        if (filePath == null) {
+            return;
+        }
+        string? assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        if (assemblyPath != null)
+        {
+            _filePath = Path.Combine(assemblyPath, filePath);
+        }
+        else
+        {
+            _filePath = filePath;
+        }
     }
 }
