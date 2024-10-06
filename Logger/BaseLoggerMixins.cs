15cbc6791786c9738dbc083b35d1d703f49fa591
Brady Dullanty <bdullanty@ewu.edu>
2024-10-06 09:57:17 -0700

    Refractored Mixins error and logging
    Also added in Information method

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -8,32 +8,41 @@ public static class BaseLoggerMixins
     //without modifiying the original class, using a concept called Extension Methods
 
     //The functionality we are extending is to take in strings and format them for the BaseLogger class
-    
+
     //We expect our Extension Methods to have paramaters of:
     //MethodName(This BaseLogger logger, String v, param var[] stuffToInsert)
     //"This BaseLogger logger" is indicating this method is actually really an extension of abstract BaseLogger,
     //and thus these method can be used as a Baselogger method in the directory
     //ex: BaseLoggerChild.Error()
-    
-    public static void Error(this BaseLogger logger, string baseMessage, params object[] objectsToInsert)
-    {
-        if (logger is null) throw new ArgumentNullException("BaseLoggerMixins.Error was passed in null for logger");
-        if (baseMessage is null) baseMessage = "Null message passed in";
-        if (objectsToInsert is null) objectsToInsert = new object[]{ };
 
-        string formatResult = string.Format(baseMessage, objectsToInsert);
-        logger.Log(LogLevel.Error, formatResult);
 
-    }
-    public static void Warning(this BaseLogger logger, string baseMessage, params object[] objectsToInsert)
+
+    public static void CheckAndLog(BaseLogger logger, string baseMessage, object[] objectsToInsert, LogLevel level)
     {
         if (logger is null) throw new ArgumentNullException("BaseLoggerMixins.Error was passed in null for logger");
         if (baseMessage is null) baseMessage = "Null message passed in";
         if (objectsToInsert is null) objectsToInsert = new object[] { };
-
         string formatResult = string.Format(baseMessage, objectsToInsert);
-        logger.Log(LogLevel.Warning, formatResult);
+        logger.Log(level, formatResult);
 
     }
+
+    public static void Error(this BaseLogger logger, string baseMessage, params object[] objectsToInsert)
+    {
+         CheckAndLog(logger, baseMessage, objectsToInsert,LogLevel.Error);
+   
+    }
+    public static void Warning(this BaseLogger logger, string baseMessage, params object[] objectsToInsert)
+    {
+        CheckAndLog(logger, baseMessage, objectsToInsert,LogLevel.Warning);
+    }
+
+    
+    public static void Information(this BaseLogger logger, string baseMessage, params object[] objectsToInsert)
+    {
+        CheckAndLog(logger, baseMessage, objectsToInsert,LogLevel.Information);
+    
+    }
+
 
 }
