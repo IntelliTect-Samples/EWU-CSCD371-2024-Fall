5e485fe85c076101dafb710eba1781a1122ab2c0
Austin Harrison <aharrison13@ewu.edu>
2024-10-06 19:37:32 -0700

    Changed the LogFactory field to public to pass the test.

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -2,14 +2,12 @@
 
 public class LogFactory
 {
-    private string _filepath = string.Empty;
+    public string _filepath = string.Empty;
 
-    public string testFilePath = string.Empty;
 
     public void ConfigureFileLogger(string filePath)
     {
         _filepath = filePath;
-        testFilePath = filePath;
     }
     public BaseLogger? CreateLogger(string className)
     {
