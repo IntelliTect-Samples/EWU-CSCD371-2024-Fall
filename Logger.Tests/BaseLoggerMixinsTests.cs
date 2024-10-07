849ef722c2a02ebf2e75c9f2e68b5c9220ed2cbf
Windeko1 <twoody1@ewu.edu>
2024-10-07 16:37:37 -0700

    Added a new test which will test the Error, Warning, Information, and Debug. I decided I'm going to add babck the extension methods, since the purpose of the extension methods is to be helpful for the user during any library calls.

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -46,6 +46,50 @@ public class BaseLoggerMixinsTests
         Assert.AreEqual($"Message for {level}", logged.Message);
     }
 
+    /*
+     * Each datarow specifies a method name as a string and a corresponding message.
+     * This effectively demonstrates invoing different logging methods based on the input from the DataRow.
+     * The switch statement is handling the conversion from string method name to the method call.
+     * After I invoke the logging method, the test will check whether the log methods are working as intended from the assertion.
+     */
+
+    [TestMethod]
+    [DataRow("Error", "Error message")]
+    [DataRow("Warning", "Warning message")]
+    [DataRow("Information", "Information message")]
+    [DataRow("Debug", "Debug message")]
+    public void Log_WithVariousMethods_LogsCorrectly(string logMethod, string message)
+    {
+        // Arrange
+        var logger = new TestLogger();
+
+        // Act
+        switch (logMethod)
+        {
+            case "Error":
+                BaseLoggerMixins.Error(logger, message);
+                break;
+            case "Warning":
+                BaseLoggerMixins.Warning(logger, message);
+                break;
+            case "Information":
+                BaseLoggerMixins.Information(logger, message);
+                break;
+            case "Debug":
+                BaseLoggerMixins.Debug(logger, message);
+                break;
+            default:
+                throw new InvalidOperationException("Unsupported log method");
+        }
+
+        // Assert
+        Assert.AreEqual(1, logger.LoggedMessages.Count);
+        var loggedMessage = logger.LoggedMessages[0];
+        Assert.AreEqual(message, loggedMessage.Message);
+        Assert.AreEqual(logMethod, loggedMessage.LogLevel.ToString());
+    }
+
+
     [TestMethod]
     public void ClassName_TestClass_Success()
     {
