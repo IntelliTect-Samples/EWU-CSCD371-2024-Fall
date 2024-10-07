c476221e7d9fe53811cf5961fd6eb543ec72b22a
Eric Leachman <91997367+theRoice@users.noreply.github.com>
2024-10-07 16:47:58 -0700

    Fixed some nullable errors

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -2,7 +2,7 @@
 
 public abstract class BaseLogger
 {
-    public string ClassName { get; set; }
+    public string? ClassName { get; set; }
     public abstract void Log(LogLevel logLevel, string message);
 }
 
