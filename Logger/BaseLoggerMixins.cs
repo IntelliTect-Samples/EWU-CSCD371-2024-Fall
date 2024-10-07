8f69931b3cc10df47640d08d3aa36810a589071c
Windeko1 <twoody1@ewu.edu>
2024-10-06 19:33:58 -0700

    Added the Warning method in BaseLoggerMixins

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -15,4 +15,16 @@ public static class BaseLoggerMixins
 
         logger.Log(LogLevel.Error, formattedMessage);
     }
+
+    public static void Warning(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger), "The logger instance can't be null.");
+        }
+
+        string formattedMessage = string.Format(message, args);
+
+        logger.Log(LogLevel.Warning, formattedMessage);
+    }
 }
