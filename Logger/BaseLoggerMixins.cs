0ba0c1dcdd230f15cd5772b184e19009973a313c
Windeko1 <twoody1@ewu.edu>
2024-10-07 16:50:10 -0700

    Added extension methods that use the log method as a helper method.

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -4,6 +4,7 @@ namespace Logger;
 
 public static class BaseLoggerMixins
 {
+    // General log method
     public static void Log(this BaseLogger logger, LogLevel level, string message, params object[] args)
     {
         if (logger == null)
@@ -13,5 +14,26 @@ public static class BaseLoggerMixins
 
         string formattedMessage = string.Format(message, args);
         logger.Log(level, formattedMessage);
+    }
+
+    // Specific log methods
+    public static void Error(this BaseLogger logger, string message, params object[] args)
+    {
+        Log(logger, LogLevel.Error, message, args);
+    }
+
+    public static void Warning(this BaseLogger logger, string message, params object[] args)
+    {
+        Log(logger, LogLevel.Warning, message, args);
+    }
+
+    public static void Information(this BaseLogger logger, string message, params object[] args)
+    {
+        Log(logger, LogLevel.Information, message, args);
+    }
+
+    public static void Debug(this BaseLogger logger, string message, params object[] args)
+    {
+        Log(logger, LogLevel.Debug, message, args);
     }
 }
