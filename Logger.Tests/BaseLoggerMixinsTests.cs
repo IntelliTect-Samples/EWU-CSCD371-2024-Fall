a1bc86d844be66e7b6e142a0b5b360988be70350
Windeko <tylerjameswoody@gmail.com>
2024-10-06 22:25:26 -0700

    Removed the helper method, all of the other uneccesary tests inside the BaseLoggerMixinsTests, and all of the LogLevels inside of the BaseLoggerMixins and had both the tests and the mixins be more dynamic with less WET code.

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -9,114 +9,6 @@ namespace Logger.Tests;
 public class BaseLoggerMixinsTests
 {
     [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
-    public void Error_WithNullLogger_ThrowsException()
-    {
-        // Arrange
-
-        // Act
-        BaseLoggerMixins.Error(null, "This will throw an exception.");
-
-        // Assert
-    }
-
-    [TestMethod]
-    public void Error_WithData_LogsMessage()
-    {
-        // Arrange
-        var logger = new TestLogger();
-
-        // Act
-        logger.Error("Message {0}", 42);
-
-        // Assert
-        Assert.AreEqual(1, logger.LoggedMessages.Count);
-        Assert.AreEqual(LogLevel.Error, logger.LoggedMessages[0].LogLevel);
-        Assert.AreEqual("Message 42", logger.LoggedMessages[0].Message);
-    }
-
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
-    public void Warning_WithNullLogger_ThrowsException()
-    {
-        // Arrange
-
-        // Act
-        BaseLoggerMixins.Warning(null, "This should throw an exception.");
-
-        // Assert
-    }
-
-    [TestMethod]
-    public void Warning_WithData_LogsMessage()
-    {
-        // Arrange
-        var logger = new TestLogger();
-
-        // Act
-        logger.Warning("Warning about {0}", "condition");
-
-        // Assert
-        Assert.AreEqual(1, logger.LoggedMessages.Count);
-        Assert.AreEqual(LogLevel.Warning, logger.LoggedMessages[0].LogLevel);
-        Assert.AreEqual("Warning about condition", logger.LoggedMessages[0].Message);
-    }
-
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
-    public void Information_WithNullLogger_ThrowsException()
-    {
-        // Arrange
-
-        // Act
-        BaseLoggerMixins.Information(null, "This should throw an exception.");
-
-        // Assert
-    }
-
-    [TestMethod]
-    public void Information_WithData_LogsMessage()
-    {
-        // Arrange
-        var logger = new TestLogger();
-
-        // Act
-        logger.Information("Warning about {0}", "condition");
-
-        // Assert
-        Assert.AreEqual(1, logger.LoggedMessages.Count);
-        Assert.AreEqual(LogLevel.Information, logger.LoggedMessages[0].LogLevel);
-        Assert.AreEqual("Warning about condition", logger.LoggedMessages[0].Message);
-    }
-
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
-    public void Debug_WithNullLogger_ThrowsException()
-    {
-        // Arrange
-
-        // Act
-        BaseLoggerMixins.Debug(null, "This should throw an exception.");
-
-        // Assert
-    }
-
-    [TestMethod]
-    public void Debug_WithData_LogsMessage()
-    {
-        // Arrange
-        var logger = new TestLogger();
-
-        // Act
-        logger.Debug("Warning about {0}", "condition");
-
-        // Assert
-        Assert.AreEqual(1, logger.LoggedMessages.Count);
-        Assert.AreEqual(LogLevel.Debug, logger.LoggedMessages[0].LogLevel);
-        Assert.AreEqual("Warning about condition", logger.LoggedMessages[0].Message);
-    }
-
-    [TestMethod]
     [DataRow(LogLevel.Error)]
     [DataRow(LogLevel.Warning)]
     [DataRow(LogLevel.Information)]
@@ -125,32 +17,12 @@ public class BaseLoggerMixinsTests
     public void Log_WithNullLogger_ThrowsException(LogLevel level)
     {
         // Arrange
+        BaseLogger logger = null;
 
         // Act
-        InvokeLogWithNullLogger(level);
+        BaseLoggerMixins.Log(logger, level, "This should throw an exception.");
 
         // Assert
-    }
-
-    private void InvokeLogWithNullLogger(LogLevel level)
-    {
-        switch (level)
-        {
-            case LogLevel.Error:
-                BaseLoggerMixins.Error(null, "This should throw an exception.");
-                break;
-            case LogLevel.Warning:
-                BaseLoggerMixins.Warning(null, "This should throw an exception.");
-                break;
-            case LogLevel.Information:
-                BaseLoggerMixins.Information(null, "This should throw an exception.");
-                break;
-            case LogLevel.Debug:
-                BaseLoggerMixins.Debug(null, "This should throw an exception.");
-                break;
-            default:
-                throw new ArgumentException("Unsupported LogLevel");
-        }
     }
 
     [TestMethod]
