ecee100264110018bf213664ff64f26a9233a3a0
Austin Harrison <aharrison13@ewu.edu>
2024-10-06 18:46:29 -0700

    Made test to test the filepath field in LogFactory (Will fail because field does not exist yet)

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -24,4 +24,35 @@ public class LogFactoryTests
         //Assert
         Assert.IsNull(logger);
     }
+
+    [TestMethod]
+    public void LogFactory_FilePathField_Success()
+    {
+        //Arrange
+        string filePath = "TestPath";
+
+        //Act
+        LogFactory factory = new();
+        factory.filepath = filePath;
+
+        //Assert
+        Assert.AreEqual(filePath, factory.FilePath);
+
+    }
+
+    //[TestMethod]
+    //public void ConfigureFileLogger_WithConfiguration_Success()
+    //{
+    //    //Arrange
+    //    LogFactory factory = new();
+    //    string filePath = "TestPath";
+
+    //    //Act
+    //    factory.ConfigureFileLogger(filePath);
+
+    //    //Assert
+    //    Assert.AreEqual(filePath, factory.FilePath);
+
+    //}
+}
 }
