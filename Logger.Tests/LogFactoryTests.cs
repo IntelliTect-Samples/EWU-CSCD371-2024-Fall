7998bfeafec55f562f4ba7207be58f2bd1a6f32f
Parker Brown <pbrown34@ewu.edu>
2024-10-07 17:30:52 -0700

    Working on Tests

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
 namespace Logger.Tests;
 
 [TestClass]
@@ -8,7 +11,26 @@ public class LogFactoryTests
     [TestMethod]
     public void LogFactory_Creation_Test_Successful()
     {
-
+        //Arrange
+        var logFactory = new LogFactory();
+        logFactory.ConfigureFileLogger();
+        //Act
+        var testLogger = logFactory.CreateLogger("Test");
+        //Assert
+        Assert.IsNotNull(testLogger);
+    }
+    [TestMethod]
+    public void ConfigureFileLogger_FilePathCorrect()
+    {
+        //Arrange
+        var logFactory = new LogFactory();
+        var logger = logFactory.FilePath;
+        string expectedFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        //Act
+        logFactory.ConfigureFileLogger(expectedFilePath);
+        //Assert
+        Assert.AreEqual(expectedFilePath, logger.FilePath);
+        
     }
 
 }
