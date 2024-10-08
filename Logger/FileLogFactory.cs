0d5b60f57bd5c0e370796a0f50f9f59437ac8e21
Brady <bdullanty@ewu.edu>
2024-10-07 19:40:42 -0700

    Implemented concrete class FileLogFactory from LogFactory

---
+++ Logger/FileLogFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Logger;
+
+public class FileLogFactory : LogFactory
+    
+{
+
+    public void ConfigureFileLogger(string? path)
+    {
+       this.FilePath = path;
+    }
+
+    public override FileLogger? CreateLogger()
+    {
+                if (this.FilePath is null) return null;
+                FileLogger fileLogger = new FileLogger(this.FilePath){ClassName = "FileLogger" };
+                return fileLogger;
+        
+    }
+}
