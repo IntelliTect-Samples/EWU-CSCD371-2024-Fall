5947298ca113c4de89cb07f5753cb0c31610b237
Eric Vo <tvo12@ewu.edu>
2024-10-07 21:04:22 -0700

    FileLogger Error

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -4,19 +4,25 @@ using System.IO;
 namespace Logger;
 
 public class FileLogger: BaseLogger {
-    public string FilePath;
+    private string _filePath;
     public override string ClassName { get; }
+
+    public string FilePath
+    {
+        get => _filePath;
+        set => _filePath = value;
+    }
 
     public FileLogger(string filePath, string className)
     {
-        FilePath = filePath;
+        _filePath = filePath;
         ClassName = className;
     }
 
     public override void Log(LogLevel logLevel, string message)
     {
         string logEntry = $"{DateTime.Now:MM/dd/yyyy hh:mm:ss tt} {ClassName} {logLevel}: {message}";
-        File.AppendAllText(FilePath, logEntry + Environment.NewLine);
+        File.AppendAllText(_filePath, logEntry + Environment.NewLine);
     }
 
 }
