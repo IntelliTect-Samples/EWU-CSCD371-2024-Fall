b51f6a1eb78536e73e8e64b668a3f5db1ee6120e
Eric Vo <tvo12@ewu.edu>
2024-10-07 19:58:25 -0700

    LogFactoryTests Update

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,9 +1,69 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using System.Reflection;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class LogFactoryTests
 {
+    protected string _filePath { get; set; }
+    protected FileLogger _logger { get; set; }
+
+    protected string _newPath { get; set; }
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\log.txt";
+        _logger = new FileLogger(_filePath, "Test");
+    }
+
+    [TestMethod]
+    public void LogFactory_CreateLogger_ReturnsFileLogger()
+    {
+        // Arrange
+        LogFactory facLog = new LogFactory();
+        // Act
+        facLog.ConfigureFileLogger("log.txt");
+        // Assert
+        Assert.IsInstanceOfType(facLog.CreateLogger("Test"), typeof(FileLogger));
+    }
+
+    [TestMethod]
+    public void LogFactory_CreateLogger_ReturnsNull()
+    {
+        // Arrange
+        LogFactory facLog = new LogFactory();
+        // Act
+        facLog.ConfigureFileLogger(null);
+        // Assert
+        Assert.IsNull(facLog.CreateLogger("Test"));
+    }
+
+    [TestMethod]
+    public void LogFactory_CreateLogger_ReturnsFileLoggerWithCorrectPath()
+    {
+        // Arrange
+        LogFactory facLog = new LogFactory();
+        // Act
+        _newPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\logNew.txt";
+        facLog.ConfigureFileLogger(_newPath);
+        // Assert
+        Assert.AreEqual(_newPath, ((FileLogger)facLog.CreateLogger("Test"))._filePath);
+    }
+
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        if(File.Exists(_filePath) | File.Exists(_newPath))
+        {
+            File.Delete(_newPath);
+            File.Delete(_filePath);
+        }
+    }
+
 
 }
