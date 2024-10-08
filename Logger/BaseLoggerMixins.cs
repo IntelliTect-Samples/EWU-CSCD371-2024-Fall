33011b7dd3d216ce40125426d7134d388b54f26a
Parker Brown <pbrown34@ewu.edu>
2024-10-07 20:37:27 -0700

    fixed argument throw

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -9,25 +9,25 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger logger, string message, params object[] args) // takes in the BaseLogger, the message string, and the args array
     {
-        if (logger == null) throw new ArgumentNullException(nameof(logger));
+        ArgumentNullException.ThrowIfNull(nameof(logger));
         logger.Log(LogLevel.Error, string.Format(CultureInfo.InvariantCulture, message, args));
     }
 
     public static void Warning(this BaseLogger logger, string message, params object[] args)
     {
-        if (logger == null) throw new ArgumentNullException(nameof(logger));
+        ArgumentNullException.ThrowIfNull(nameof(logger));
         logger.Log(LogLevel.Warning, string.Format(CultureInfo.InvariantCulture, message, args));
     }
 
     public static void Information(this BaseLogger logger, string message, params object[] args)
     {
-        if (logger == null) throw new ArgumentNullException(nameof(logger));
+        ArgumentNullException.ThrowIfNull(nameof(logger));
         logger.Log(LogLevel.Information, string.Format(CultureInfo.InvariantCulture, message, args));
     }
 
     public static void Debug(this BaseLogger logger, string message, params object[] args)
     {
-        if (logger == null) throw new ArgumentNullException(nameof(logger));
+        ArgumentNullException.ThrowIfNull(nameof(logger));
         logger.Log(LogLevel.Debug, string.Format(CultureInfo.InvariantCulture, message, args));
     }
 }
