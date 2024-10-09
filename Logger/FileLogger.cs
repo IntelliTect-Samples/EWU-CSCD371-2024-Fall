17d28fd7a39ae13d55759a92cb86c45828bb3492
kflannery2001 <kflannery@ewu.edu>
2024-10-08 20:00:59 -0700

    Updated/finished LogFactory + FileLogger

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Logger
 {
     public class FileLogger : BaseLogger
     {
+        private string _filePath { get; }
+        public FileLogger(string filePath)
+        {
+            _filePath = filePath;
+        }
         public override void Log(LogLevel logLevel, string message)
         {
-            throw new NotImplementedException();
+            //Format ex: "10/7/2019 12:38:59 AM FileLoggerTests Warning: Test message"
+            string formattedMessage = $"{DateTime.Now} {ClassName} {logLevel}: {message}";
+
+            //Print log message to file
+            File.AppendAllText(_filePath, formattedMessage + Environment.NewLine);
         }
     }
 }
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -4,9 +4,24 @@ namespace Logger;
 
 public class LogFactory
 {
+    private string? _filePath;
+
+    public string ConfigureFilePath(string filePath)
+    {
+        ArgumentNullException.ThrowIfNull(filePath);
+        _filePath = filePath;
+        return _filePath;
+    }
     public BaseLogger? CreateLogger(string className)
     {
         ArgumentNullException.ThrowIfNull(className);
-        return new FileLogger() { ClassName = className };
+
+        //If no file path is configured, return null
+        if (string.IsNullOrWhiteSpace(_filePath))
+        {
+            return null;
+        }
+
+        return new FileLogger(_filePath) { ClassName = className };
     }
 }
