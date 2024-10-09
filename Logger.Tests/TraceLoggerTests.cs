b3413faa5202676e4a106e6234605724dfecccec
Windeko1 <twoody1@ewu.edu>
2024-10-08 18:54:55 -0700

    Added a simple FileLogger class, and added a TraceLoggerTests class that will have TestTraceListener class encapsulated inside of it

---
+++ Logger.Tests/TraceLoggerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests;
+
+[TestClass]
+public class TraceLoggerTests
+{
+    private class TestTraceListener : TraceListener
+    {
+        public List<string> Messages { get; } = new List<string>();
+
+        public override void Write(string? message)
+        {
+            if (message == null)
+            {
+                Messages.Add("Warning: Attempted to write a null message");
+                return;
+            }
+            Messages.Add(message);
+        }
+
+        public override void WriteLine(string? message)
+        {
+            if (message == null)
+            {
+                Messages.Add("Warning: Attempted to write a null message");
+                return;
+            }
+            Messages.Add(message);
+        }
+    }
+
+}
---
+++ Logger/TraceLogger.cs
@@ -0,0 +1,11 @@
+﻿namespace Logger;
+
+public class TraceLogger : BaseLogger
+{
+    public string ClassName { get; set; } = string.Empty;
+
+    public void Log(LogLevel logLevel, string message)
+    {
+
+    }
+}
