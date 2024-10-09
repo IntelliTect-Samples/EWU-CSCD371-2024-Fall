cb8deac6042cbda5fb22c159c2e97a2dbf3f19c5
Eric Leachman <eleachman04@ewu.edu>
2024-10-08 20:00:05 -0700

    Added sidetest file to debug file access

---
+++ Logger.Tests/SideTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests
+{
+    [TestClass]
+    public class SideTests
+    {
+        [TestMethod]
+        [DataRow("C:\\Users\\ericm\\temp", LogLevel.Error, "Hello!")]
+        //[DataRow("C:\\Users\\ericm\\repos\\EWU-CSCD371-2024-Fall\\Logger.Tests\\bin\\Debug\\net7.0\\testfile.txt")]
+        //[DataRow("C:\\Users\\ericm\\repos\\EWU-CSCD371-2024-Fall\\Logger.Tests\\bin\\Debug\\net7.0", LogLevel.Error, "Hello!")]
+        public void TestFileCanBeAppended(string filePath, LogLevel logLevel, string message)
+        {
+            // Arrange
+            filePath = System.IO.Path.Combine(filePath, logLevel + ".txt");
+            string initialText = "Initial content.";
+            string appendText = " Appended content.";
+
+            // Ensure the file is created and has initial content
+            File.WriteAllText(filePath, initialText);
+
+            // Act
+            File.AppendAllText(filePath, appendText);
+            string result = File.ReadAllText(filePath);
+
+            // Assert
+            Assert.IsTrue(result.Contains(appendText), "The file could not be appended.");
+        }
+    }
+}
