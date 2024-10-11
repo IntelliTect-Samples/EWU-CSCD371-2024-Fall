f286ef2d100c3b1c05ae2cc804ede93122b00051
Ben Knoll <bknoll@ewu.edu>
2024-10-10 19:58:18 -0700

    BaseLoggerMixins Update
    * Removed if statements for ThrowIfNull()
    * Cleaned up Whitespace

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -7,35 +7,24 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger? logger, string message, params object[] args)
     {
-        if (logger is null)
-        {
-            throw new ArgumentNullException(nameof(logger), "Logger is null");
-        }
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         logger.Log(LogLevel.Error, string.Format(new CultureInfo("en-US"),message,args));
     }
+
     public static void Warning(this BaseLogger? logger, string message, params object[] args)
     {
-        if (logger is null)
-        {
-            throw new ArgumentNullException(nameof(logger), "Logger is null");
-        }
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         logger.Log(LogLevel.Warning, string.Format(new CultureInfo("en-US"), message, args));
     }
+
     public static void Information(this BaseLogger? logger, string message, params object[] args)
     {
-        if (logger is null)
-        {
-            throw new ArgumentNullException(nameof(logger), "Logger is null");
-        }
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         logger.Log(LogLevel.Information, string.Format(new CultureInfo("en-US"), message, args));
     }
     public static void Debug(this BaseLogger? logger, string message, params object[] args)
     {
-        if (logger is null)
-        {
-            throw new ArgumentNullException(nameof(logger), "Logger is null");
-        }
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         logger.Log(LogLevel.Debug, string.Format(new CultureInfo("en-US"), message, args));
     }
-
 }
