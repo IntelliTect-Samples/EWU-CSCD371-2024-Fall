14e3b0c1b2628365d5e081cfdae2a5ea18452d01
Brady <bdullanty@ewu.edu>
2024-10-12 16:27:28 -0700

    Added test for second implementation

--- CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -14,7 +14,7 @@ public class JokeServiceTests
     public void GetJoke_NoArgs_ReturnsJoke()
     {
         // Arrange
-        JsonJokeService service = new();
+        JokeService service = new();
         string joke;
 
         // Act
@@ -23,23 +23,5 @@ public class JokeServiceTests
         // Assert
         Assert.NotNull(joke);
         Assert.NotEqual("", joke);
-        //Assert.AreEqual("hi", joke); //Uncomment line to see result
-    }
-
-
-    [Theory]
-    [InlineData("{ \"joke\": \"A joke here!\" }", "A joke here!")]
-    public void ParseJsonJoke_ValidJson_ReturnsString(string jsonString, string expected)
-    {
-        // Arrange
-        JsonJokeService service = new();
-        string? response;
-
-        // Act
-        response =service.ParseJokeFromJsonString(jsonString);
-        // Assert
-        Assert.NotNull(response);
-        Assert.Equal(expected, response);
-        //Assert.AreEqual("expected:"+expected, response); //Uncomment line to see result
     }
 }
---
+++ CanHazFunny/CanHazFunny.Tests/JsonJokeServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny.Tests;
+
+
+public class JsonJokeServiceTests
+{
+    [Fact]
+    public void GetJoke_NoArgs_ReturnsJoke()
+    {
+        // Arrange
+        JsonJokeService service = new();
+        string joke;
+
+        // Act
+        joke = service.GetJoke();
+
+        // Assert
+        Assert.NotNull(joke);
+        Assert.NotEqual("", joke);
+        //Assert.AreEqual("hi", joke); //Uncomment line to see result
+    }
+
+
+    [Theory]
+    [InlineData("{ \"joke\": \"A joke here!\" }", "A joke here!")]
+    public void ParseJsonJoke_ValidJson_ReturnsString(string jsonString, string expected)
+    {
+        // Arrange
+        JsonJokeService service = new();
+        string? response;
+
+        // Act
+        response =service.ParseJokeFromJsonString(jsonString);
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal(expected, response);
+        //Assert.AreEqual("expected:"+expected, response); //Uncomment line to see result
+    }
+}
