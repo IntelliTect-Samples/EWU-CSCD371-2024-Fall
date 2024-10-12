873a4f5d2375b2850b48e5d73398129884b9e19a
Brady <bdullanty@ewu.edu>
2024-10-12 16:24:46 -0700

    Added Interface to Json implementation

--- CanHazFunny/CanHazFunny/JsonJokeService.cs
+++ CanHazFunny/CanHazFunny/JsonJokeService.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 
 namespace CanHazFunny;
 
-public class JsonJokeService
+public class JsonJokeService : IJokeService
 {
     private HttpClient HttpClient { get; } = new();
 
