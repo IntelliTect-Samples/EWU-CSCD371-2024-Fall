11e05486f9baa0e2cd45473bcc933c2f80e51e7c
Brady <bdullanty@ewu.edu>
2024-10-12 16:19:44 -0700

    Added Display Service Tests class and a test for it

---
+++ CanHazFunny/CanHazFunny.Tests/DisplayServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+namespace CanHazFunny.Tests
+{
+    public class DisplayServiceTests
+    {
+        [Theory]
+        [InlineData("Bad Joke Here")]
+        public void DisplayJoke_ValidString_DisplaysJoke(string joke)
+        {
+            //Arrange
+            StringWriter consoleOut = new();
+            Console.SetOut(consoleOut);
+            DisplayService displayService = new();
+            //Act
+            displayService.DisplayJoke(joke);
+            //Assert
+            Assert.Equal(consoleOut.ToString().Trim(),joke.Trim());
+            
+        }
+    }
+}
