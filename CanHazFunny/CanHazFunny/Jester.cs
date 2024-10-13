88ff81917e06af9f6feb11415cca63f2132dda75
Eric Vo <tvo12@ewu.edu>
2024-10-13 11:46:45 -0700

    Jester constructor

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -3,4 +3,17 @@ namespace CanHazFunny;
 
 internal class Jester
 {
+    private readonly IJokeService JokeService;
+    private readonly IJokeTeller JokeTeller;
+
+    public Jester(IJokeService jokeService, IJokeTeller jokeTeller)
+    {
+        JokeService = jokeService;
+        JokeTeller = jokeTeller;
+    }
+
+    public void TellJoke()
+    {
+        
+    }
 }
