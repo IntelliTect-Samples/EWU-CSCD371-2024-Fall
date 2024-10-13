5944f3ebf8edb2a7945530c188057ce8f5b6770e
David <david_e_moore@hotmail.com>
2024-10-12 19:58:41 -0700

    Add References to the CanHazFunny Project for Interfaces, rebuilt Interfaces Project.

--- CanHazFunny/Interface/IJokeService.cs
+++ CanHazFunny/Interface/IJokeService.cs
@@ -1,6 +1,6 @@
 namespace Interfaces
 {
-    public interface IJesterService
+    public interface IJokeService
     {
         string GetJoke(); //Applying this interface to JokeService to return a string
     }
