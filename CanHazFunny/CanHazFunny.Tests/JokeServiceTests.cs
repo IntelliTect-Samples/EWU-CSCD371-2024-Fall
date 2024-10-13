7419d2726421dfebedade8af5718acf18c73f0fe
Austin Harrison <aharrison13@ewu.edu>
2024-10-13 14:09:36 -0700

    Added test for IJokesServices

--- CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -10,5 +10,18 @@ namespace CanHazFunny.Tests
     [TestClass]
     public class JokeServiceTests
     {
+        [TestMethod]
+        public void JokeServiceInterface_UseMoq_Success()
+        {
+            // Arrange
+            var jokeService = new Moq.Mock<IJokeService>();
+            jokeService.Setup(x => x.GetJoke()).Returns("Why did the chicken cross the road? To get to the other side.");
+
+            // Act
+            var joke = jokeService.Object.GetJoke();
+
+            // Assert
+            Assert.AreEqual("Why did the chicken cross the road? To get to the other side.", joke);
+        }
     }
 }
