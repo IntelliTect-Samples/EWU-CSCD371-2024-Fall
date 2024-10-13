03f5391e64531e69e7ed073cce52c4fe793327f1
Leo Marlow <lmarlow@ewu.edu>
2024-10-12 20:38:48 -0700

    Wrote a small menu system

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -1,13 +1,39 @@
 ﻿namespace CanHazFunny;
 
 using CanHazFunny;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Boolean keepTellingJokes = true;
         Jester jester = new();
-        jester.TellJoke();
+
+
+        while (keepTellingJokes == true)
+        {
+           
+            System.Console.WriteLine("Would you like to hear a joke? (Y/N)");
+            string response = Console.ReadLine().ToUpper();
+            if (response == "N")
+            {
+                keepTellingJokes = false;
+            }
+            else if (response == "Y")
+            {
+                jester.TellJoke();
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid input. Please enter Y or N.");
+            }
+        
+        
+        }
+
+
+        System.Console.WriteLine("Goodbye! Its been fun!");
 
         //Feel free to use your own setup here - this is just provided as an example
         //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
