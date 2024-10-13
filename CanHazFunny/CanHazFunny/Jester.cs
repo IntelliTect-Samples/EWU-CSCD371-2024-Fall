b2c535ca7d59e64a65c48b306cc0666b73459a46
Austin Harrison <aharrison13@ewu.edu>
2024-10-13 16:37:52 -0700

    Started to define Jester, created the neccessary fields to make test pass.

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using CanHazFunny;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CanHazFunny
+namespace CanHazFunny;
+public class Jester
 {
-    public class Jester
+    private readonly IJokeService _jokeService;
+    private readonly IDisplayJokes _displayJokes;
+    public Jester(IJokeService jokeService, IDisplayJokes displayJokes)
     {
+
+        _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
+        _displayJokes = displayJokes ?? throw new ArgumentNullException(nameof(displayJokes));
 
     }
 }
