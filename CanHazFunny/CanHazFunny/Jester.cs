745a5ed9818b96f9750f15552aa5b311cd895fd0
Windeko <tylerjameswoody@gmail.com>
2024-10-12 22:59:10 -0700

    Added initial implementation for Jester class that has its fields and its constructor.

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,7 +1,16 @@
 ﻿
+using System;
+
 namespace CanHazFunny.Tests;
 
 public class Jester
 {
+    private readonly IDisplayJokes _displayService;
+    private readonly IJokeService _jokeService;
 
+    public Jester(IDisplayJokes dispalyService, IJokeService jokeService)
+    {
+        _displayService = dispalyService ?? throw new ArgumentNullException(nameof(dispalyService));
+        _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
+    }
 }
