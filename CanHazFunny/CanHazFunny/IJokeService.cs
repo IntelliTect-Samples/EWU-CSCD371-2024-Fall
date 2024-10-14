07adce4ff9924827bbdade132a38505a69c5e457
kflannery2001 <kflannery@ewu.edu>
2024-10-14 15:58:35 -0700

    Created all needed classes/interfaces. Fixed Program.cs so we get a joke when ran

---
+++ CanHazFunny/CanHazFunny/IJokeService.cs
@@ -0,0 +1,6 @@
+﻿namespace CanHazFunny;
+
+public interface IJokeService
+{
+    string GetJoke();
+}
---
+++ CanHazFunny/CanHazFunny/JokeOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny;
+
+public class JokeOutput : IJokeOutput
+{
+    public void WriteJoke(string joke)
+    {
+        Console.WriteLine(joke);
+    }
+}
