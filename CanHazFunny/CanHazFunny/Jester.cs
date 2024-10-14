ae87993abc361150b8fcbc9f4b16434e84cc5053
Jake Lucas <156277910+JacobLucas02@users.noreply.github.com>
2024-10-13 19:52:13 -0700

    Created Jester Class

---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,27 @@
+namespace CanHazFunny
+{
+    internal class Jester
+    {
+        private IJokeService jokeService;
+        private IOutputService outputService;
+
+        public Jester(ConsoleOutputService outputService, JokeService jokeService)
+        {
+            this.outputService = outputService;
+            this.jokeService = jokeService;
+        }
+
+        public void TellJoke()
+        {
+            string joke;
+
+            do
+            {
+                joke = jokeService.GetJoke();
+            }
+            while (joke.Contains("Chuck Norris"));
+
+            outputService.WriteJoke(joke);
+        }
+    }
+}
