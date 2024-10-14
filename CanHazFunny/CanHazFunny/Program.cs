d9ca721afdec7ed2e2807ac2959904ead2127b31
Eric Vo <tvo12@ewu.edu>
2024-10-13 23:41:09 -0700

    public main method

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -1,11 +1,11 @@
-﻿namespace CanHazFunny;
+﻿    namespace CanHazFunny;
 
-public class Program
-{
-    static void Main()
+    public class Program
     {
-        //Feel free to use your own setup here - this is just provided as an example
-        //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
-        new Jester(new JokeService(), new ConsoleJoke()).TellJoke();
+        public static void Main()
+        {
+            //Feel free to use your own setup here - this is just provided as an example
+            //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
+            new Jester(new JokeService(), new ConsoleJoke()).TellJoke();
+        }
     }
-}
