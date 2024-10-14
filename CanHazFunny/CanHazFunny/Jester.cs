28967b4b51219f392355eeef907c94f3ea26aadf
Eric Leachman <eleachman04@ewu.edu>
2024-10-13 17:12:04 -0700

    added nullable and null forgiving operators where necessary

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -8,16 +8,16 @@ using System.Threading.Tasks;
 namespace CanHazFunny;
 public class Jester
 {
-    private IJokeService _jokeService;
-    private IDisplayJokes _displayJokes;
+    private IJokeService? _jokeService;
+    private IDisplayJokes? _displayJokes;
     public IJokeService JokeService
     {
-        get => _jokeService;
+        get => _jokeService!;
         set => _jokeService = value ?? throw new ArgumentNullException(nameof(value));
     }
     public IDisplayJokes DisplayJokes
     {
-        get => _displayJokes;
+        get => _displayJokes!;
         set => _displayJokes = value ?? throw new ArgumentNullException(nameof(value));
     }
 
