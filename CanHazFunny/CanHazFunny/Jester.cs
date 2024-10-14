8de4431bc45fe63e26c03ae77cdfd7387bae0740
ccornell88 <129800566+ccornell88@users.noreply.github.com>
2024-10-14 16:02:03 -0700

    Create Jester Class
    Added Jester class

---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace CanHazFunny
+{
+    public class Jester
+    {
+        private IJokeService _jokeService; // hold instance of joke service
+        private IOutput _output; // hold instance of output service
+
+        // Constructor
+        public Jester(IJokeService jokeService, IOutput output)
+        {
+            // Assign dependencies to private fields
+            _jokeService = jokeService;
+            _output = output;
+        }
+
+
+        // Retrieves a joke from joke service and will display it using
+        // output service. Uses while loop to filter out jokes containing
+        // Chuck Norris.
+        public void TellJoke()
+        {
+            string joke = _jokeService.GetJoke();
+
+            while (joke.Contains("Chuck Norris"))
+            {
+                joke = _jokeService.GetJoke();
+            }
+            _output.WriteJoke(joke);
+        }
+    }
+}
+
