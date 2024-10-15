492151b26aa6c3208d6ba9931b7801d1e36dca2b
Windeko <tylerjameswoody@gmail.com>
2024-10-15 10:09:30 -0700

    Added a check for nullity in the constructor, and if it is, it will create a new instance of the DisplayService class

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -10,7 +10,9 @@ public class Jester
 
     public Jester(IDisplayJokes jokeDisplayer, ITellJokes jokeTeller)
     {
+        if (jokeDisplayer is null) jokeDisplayer = new DisplayService();
         JokeDisplayer = jokeDisplayer;
+
         JokeTeller = jokeTeller;
     }
 }
