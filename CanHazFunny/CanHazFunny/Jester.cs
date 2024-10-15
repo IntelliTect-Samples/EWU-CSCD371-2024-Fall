b418d6db5842c9606435880db03013f4d1c9930d
Jake Lucas <156277910+JacobLucas02@users.noreply.github.com>
2024-10-14 23:54:27 -0700

    Update Jester.cs

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,29 +1,28 @@
 using System;
 
-namespace CanHazFunny
+namespace CanHazFunny;
+
+public class Jester
 {
-    public class Jester
+    private IJokeService _jokeService;
+    private IOutputService _outputService;
+
+    public Jester(IOutputService outputService, IJokeService jokeService)
     {
-        private IJokeService _jokeService;
-        private IOutputService _outputService;
-
-        public Jester(IOutputService outputService, IJokeService jokeService)
-        {
-            _outputService = outputService ?? throw new ArgumentNullException(nameof(outputService));
-            _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
-        }
-
-        public void TellJoke()
-        {
-            string joke;
-
-            do
-            {
-                joke = _jokeService.GetJoke();
-            }
-            while (joke.Contains("Chuck Norris"));
-
-            _outputService.WriteJoke(joke);
-        }
+        _outputService = outputService ?? throw new ArgumentNullException(nameof(outputService));
+        _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
     }
+
+    public void TellJoke()
+    {
+        string joke;
+
+        do
+        {
+            joke = _jokeService.GetJoke();
+        }
+        while (joke.Contains("Chuck Norris"));
+
+        _outputService.WriteJoke(joke);
+     }
 }
