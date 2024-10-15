6987963e64b0ab21678c5e6e046b06091fca55f9
Windeko <tylerjameswoody@gmail.com>
2024-10-15 09:50:19 -0700

    Created initial test for a jester constructor

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -7,5 +7,18 @@ namespace CanHazFunny.Tests;
 
 public class JesterTests
 {
-    
+    [Fact]
+    public void Constructor_JsonJokeService_CreatesValidJester()
+    {
+
+        //Arrange
+        DisplayService displayService = new();
+        JsonJokeService jsonJokeService = new();
+
+        //Act
+        Jester jester = new Jester(displayService, jsonJokeService);
+
+        //Assert
+        Assert.NotNull(jester);
+    }
 }
