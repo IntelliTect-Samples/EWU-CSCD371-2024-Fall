484ee667bc726b3552462eb6b869b03cae2707d1
Brady Dullanty <bdullanty@ewu.edu>
2024-10-15 11:57:41 -0700

    Made Main method public

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -2,7 +2,7 @@
 
 public sealed class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         //Feel free to use your own setup here - this is just provided as an example
         //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
