7c5e478c9619e90b2b696475e45769330817cb0d
Ben Knoll <bknoll@ewu.edu>
2024-10-14 21:41:44 -0700

    Added Chuck Norris test
    * Ensures that no variations of chuck norris is accepted as a joke

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using Moq;
 
 namespace CanHazFunny.Tests;
 
@@ -23,6 +24,27 @@ public class JesterTests
     {
         string uppercase = "THISISSOMEUPPERCASE";
         Assert.Equal("thisissomeuppercase", uppercase.ToLowerInvariant());
+    }
+
+    [Fact]
+    public void TellJoke_ChuckNorrisJokeRetrieved_JokeNotContainsChuckNorris()
+    {
+        var serviceMock = new Mock<IJokeService>();
+        var outputMock = new Mock<IJokeOutput>();
+
+        serviceMock.SetupSequence(foo => foo.GetJoke()).Returns("ULTIMATE ChUck NorrIS")
+            .Returns("Norris joke")
+            .Returns("ChUck joke")
+            .Returns("Valid Joke");
+
+        var Jest = new Jester(outputMock.Object, serviceMock.Object);
+
+        Jest.TellJoke();
+        outputMock.Verify(foo => foo.WriteJoke("Valid Joke"), Times.AtLeastOnce());
+        outputMock.Verify(foo => foo.WriteJoke("norris joke"), Times.Never());
+        outputMock.Verify(foo => foo.WriteJoke("ULTIMATE ChUck NorrIS"), Times.Never());
+        outputMock.Verify(foo => foo.WriteJoke("ChUck joke"), Times.Never());
+
     }
 
 }
