dec7932a57df037bd989427aabf758ec902b5415
Parker Brown <pbrown34@ewu.edu>
2024-10-14 17:17:23 -0700

    Added New Tests

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Globalization;
+using System.IO;
 using Xunit;
 using static CanHazFunny.JokeService;
 
@@ -50,5 +53,40 @@ public class JesterTests
         //Assert
         Assert.NotNull(joke);
     }
-   
+    [Fact]
+    public void Main_WrongInput_ReturnString()
+    {
+        // Arrange
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        var inputs = new[] { "A", "Y" };
+        Console.SetIn(new StringReader(string.Join(Environment.NewLine, inputs)));
+        // Act
+        Program.Main(new string[0]);
+        // Assert
+        var output = stringWriter.ToString();
+        Assert.Contains("Invalid input. Please enter Y or N.", output);
+        Assert.Contains("Would you like to hear a joke? (Y/N) or Change formats (F)", output);
+    }
+   [Fact]
+   public void Main_SwitchToJson_SwitchToHttp()
+   {
+            // Arrange
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        // Simulate invalid input
+        var inputs = new[] { "F", "F" }; 
+        Console.SetIn(new StringReader(string.Join(Environment.NewLine, inputs)));
+
+        // Act
+        Program.Main(new string[0]);
+
+        // Assert
+        var output = stringWriter.ToString();
+        Assert.Contains("Format changed to: Json" + System.Environment.NewLine, output);
+        Assert.Contains("Would you like to hear a joke? (Y/N) or Change formats (F)", output);
+        Assert.Contains("Format changed to: Http" + System.Environment.NewLine, output);
+        Assert.Contains("Would you like to hear a joke? (Y/N) or Change formats (F)", output);
+   }
 }
