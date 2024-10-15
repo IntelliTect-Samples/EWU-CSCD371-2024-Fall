625e37f57c6321ef0c2dee4ae8adaadcb586ca41
Oscar Ruelas <oruelas@ewu.edu>
2024-10-14 17:46:49 -0700

    Unit tested the implementation that writes the joke out to the screen

---
+++ CanHazFunny/CanHazFunny.Tests/JokeOutputTest.cs
@@ -0,0 +1,28 @@
+﻿using Interfaces;
+using Moq;
+using Xunit;
+
+namespace CanHazFunny.Tests;
+
+public class JokeOutputTest
+{
+    [Fact]
+    public void Jester_TellJoke_WritesJokeToOutput()
+    {
+        // Arrange
+        var jokeServiceMock = new Mock<IJokeService>();
+        jokeServiceMock.Setup(js => js.GetJoke()).Returns("This is a not joke take C# seriously");
+
+        var jokeOutputMock = new Mock<IJokeOutput>();
+
+        Jester jester = new(jokeServiceMock.Object, jokeOutputMock.Object);
+
+        // Act
+        jester.TellJoke();
+
+        // Assert
+        jokeOutputMock.Verify(joke => joke.OutputJoke("This is a not joke take C# seriously"), 
+            Times.Once, "Joke was not written to output");
+    }
+
+}
