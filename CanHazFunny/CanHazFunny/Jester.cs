0a63a6b381762ec07a8801de7b60d9039a6331b9
Windeko <tylerjameswoody@gmail.com>
2024-10-15 09:45:22 -0700

    Removed code for major refactor

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -5,21 +5,5 @@ namespace CanHazFunny.Tests;
 
 public class Jester
 {
-    private readonly IDisplayJokes _displayService;
-    private readonly IJokeService _jokeService;
-
-    public Jester(IDisplayJokes displayService, IJokeService jokeService)
-    {
-        _displayService = displayService ?? throw new ArgumentNullException(nameof(displayService));
-        _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
-    }
-
-    public void TellJoke()
-    {
-        var joke = _jokeService.GetJoke();
-        if (!joke.Contains("Chuck Norris"))
-        {
-            _displayService.DisplayJoke(joke);
-        }
-    }
+    
 }
