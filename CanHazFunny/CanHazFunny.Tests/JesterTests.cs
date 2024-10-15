d3f068675bd6c2adfd280e49b8d1a30880ceb49e
Ben Knoll <bknoll@ewu.edu>
2024-10-14 20:35:42 -0700

    Added Properties
    * Added Properties for JokeService and JokeOutput in Jester (Along with tests)
    * Added check for Chuck Norris
    * Added JokeServiceTests

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,7 +1,28 @@
+using System;
 using Xunit;
 
 namespace CanHazFunny.Tests;
 
 public class JesterTests
 {
+
+    [Fact]
+    public void JokeOutput_ServiceSetNull_ArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() =>  new Jester(new JokeOutput(), null!) );
+    }
+
+    [Fact]
+    public void JokeOutput_OutputSetNull_ArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Jester(null!, new JokeService()));
+    }
+
+    [Fact]
+    public void ToLower_PassedValidString_ReturnsLowercaseString()
+    {
+        string uppercase = "THISISSOMEUPPERCASE";
+        Assert.Equal("thisissomeuppercase", uppercase.ToLower());
+    }
+
 }
