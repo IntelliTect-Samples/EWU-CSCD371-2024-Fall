0b86380b7629a45753d5c0719de35fe66c4f91cc
Jake Lucas <156277910+JacobLucas02@users.noreply.github.com>
2024-10-14 23:57:38 -0700

    Update JesterTests.cs

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,49 +1,48 @@
 using Moq;
 using Xunit;
 
-namespace CanHazFunny.Tests
+namespace CanHazFunny.Tests;
+
+public class JesterTests
 {
-    public class JesterTests
+    [Fact]
+    public void TellJoke_ReturnsValidJoke_JokePrinted()
     {
-        [Fact]
-        public void TellJoke_ReturnsValidJoke_JokePrinted()
-        {
-            // Arrange
-            var mockJokeService = new Mock<IJokeService>();
-            var mockOutputService = new Mock<IOutputService>();
+        // Arrange
+        var mockJokeService = new Mock<IJokeService>();
+        var mockOutputService = new Mock<IOutputService>();
 
-            string outputJoke = string.Empty;
-            mockJokeService.Setup(js => js.GetJoke()).Returns("Why did the chicken cross the road? To get to the other side!");
-            mockOutputService.Setup(os => os.WriteJoke(It.IsAny<string>())).Callback<string>(joke => outputJoke = joke);
-            Jester jester = new Jester(mockOutputService.Object, mockJokeService.Object);
+        string outputJoke = string.Empty;
+        mockJokeService.Setup(js => js.GetJoke()).Returns("Why did the chicken cross the road? To get to the other side!");
+        mockOutputService.Setup(os => os.WriteJoke(It.IsAny<string>())).Callback<string>(joke => outputJoke = joke);
+        Jester jester = new(mockOutputService.Object, mockJokeService.Object);
 
-            // Act
-            jester.TellJoke();
+        // Act
+        jester.TellJoke();
 
-            // Assert
-            Assert.Equal("Why did the chicken cross the road? To get to the other side!", outputJoke);
-        }
+        // Assert
+        Assert.Equal("Why did the chicken cross the road? To get to the other side!", outputJoke);
+    }
 
-        [Fact]
-        public void TellJoke_SkipsChuckNorrisJoke_ReturnsValidJoke()
-        {
-            // Arrange
-            var mockJokeService = new Mock<IJokeService>();
-            var mockOutputService = new Mock<IOutputService>();
+    [Fact]
+    public void TellJoke_SkipsChuckNorrisJoke_ReturnsValidJoke()
+    {
+        // Arrange
+        var mockJokeService = new Mock<IJokeService>();
+        var mockOutputService = new Mock<IOutputService>();
 
-            string outputJoke = string.Empty;
-            mockJokeService.SetupSequence(js => js.GetJoke())
-                .Returns("Chuck Norris walks into a bar.")
-                .Returns("Why did the chicken cross the road? To get to the other side!");
-            mockOutputService.Setup(os => os.WriteJoke(It.IsAny<string>()))
-                .Callback<string>(joke => outputJoke = joke);
-            Jester jester = new Jester(mockOutputService.Object, mockJokeService.Object);
+        string outputJoke = string.Empty;
+        mockJokeService.SetupSequence(js => js.GetJoke())
+            .Returns("Chuck Norris walks into a bar.")
+            .Returns("Why did the chicken cross the road? To get to the other side!");
+        mockOutputService.Setup(os => os.WriteJoke(It.IsAny<string>()))
+            .Callback<string>(joke => outputJoke = joke);
+        Jester jester = new(mockOutputService.Object, mockJokeService.Object);
 
-            // Act
-            jester.TellJoke();
+        // Act
+        jester.TellJoke();
 
-            // Assert
-            Assert.Equal("Why did the chicken cross the road? To get to the other side!", outputJoke);
-        }
+        // Assert
+        Assert.Equal("Why did the chicken cross the road? To get to the other side!", outputJoke);
     }
 }
