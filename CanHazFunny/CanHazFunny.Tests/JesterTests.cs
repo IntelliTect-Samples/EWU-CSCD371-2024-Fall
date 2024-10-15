8f5f9faad06811a852e51041cdd269f15b748e19
dpalmer10 <dpalmer10@ewu.edu>
2024-10-15 16:12:59 -0700

    added some new Jester tests and modified TellJoke test

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,5 +1,7 @@
 using Xunit;
 using CanHazFunny;
+using System;
+using System.IO;
 
 namespace CanHazFunny.Tests;
 
@@ -22,22 +24,42 @@ public class JesterTests
     [Fact]
     public void JokeService_SetJokeServiceNull_ArgumentNullException()
     {
-        Assert.Throws<System.ArgumentNullException>(
+        Assert.Throws<ArgumentNullException>(
             () => new Jester(null!, new ConsoleJoke()));
     }
 
     [Fact]
     public void JokeTeller_SetJokeTellerNull_ArgumentNullException()
     {
-        Assert.Throws<System.ArgumentNullException>(
+        Assert.Throws<ArgumentNullException>(
             () => new Jester(new JokeService(), null!));
+    }
+
+    [Fact]
+    public void JokeService_SetNullToInitializedJester_ArgumentNullException()
+    {
+        Jester jester = new(new JokeService(), new ConsoleJoke());
+        Assert.Throws<ArgumentNullException>(
+            () => jester.JokeService = null!);
+    }
+
+    [Fact]
+    public void JokeTeller_SetNullToInitializedJester_ArgumentNullException()
+    {
+        Jester jester = new(new JokeService(), new ConsoleJoke());
+        Assert.Throws<ArgumentNullException>(
+            () => jester.JokeTeller = null!);
     }
 
     [Fact]
     public void TellJoke_GetsJokeWithNoChuckNorris_PrintsJoke()
     {
+        StringWriter writer = new();
+        Console.SetOut(writer);
+
         Jester jester = new(new JokeService(), new ConsoleJoke());
         jester.TellJoke();
-        Assert.DoesNotContain("Chuck Norris", System.Console.Out.ToString());
+
+        Assert.DoesNotContain("Chuck Norris", writer.ToString());
     }
 }
