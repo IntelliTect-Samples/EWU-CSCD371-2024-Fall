bedac38f54f97f92901dafc8099649a0855946f0
ccornell88 <129800566+ccornell88@users.noreply.github.com>
2024-10-15 23:10:50 -0700

    Added JokeServiceTests

---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Moq;
+using System;
+
+namespace CanHazFunny.Tests
+{
+    public class JokeServiceTests
+    {
+        [Fact]
+        public void IJokeService_GetJoke_ReturnsExpectedJoke()
+        {
+            // Arrange
+            var jokeServiceMock = new Mock<IJokeService>();
+            string expectedJoke = "What do you call fake spaghetti? An impasta!";
+            jokeServiceMock.Setup(service => service.GetJoke()).Returns(expectedJoke);
+
+            // Act
+            string actualJoke = jokeServiceMock.Object.GetJoke();
+
+            // Assert
+            Assert.Equal(expectedJoke, actualJoke);
+        }
+    }
+}
