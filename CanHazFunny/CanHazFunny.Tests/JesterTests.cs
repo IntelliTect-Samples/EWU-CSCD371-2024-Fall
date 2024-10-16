902095dbcd690544c393fcdf0799c44e0564a33e
ccornell88 <129800566+ccornell88@users.noreply.github.com>
2024-10-15 20:07:41 -0700

    Added Test
    Confirms joke is retrieved and output correctly when TellJoke() is called.

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,7 +1,28 @@
+using System;
 using Xunit;
+using Moq;
 
 namespace CanHazFunny.Tests;
 
 public class JesterTests
 {
+    [Fact]
+    public void TellJoke_JokeRetrieved_SuccessfullyReceived()
+
+    {
+        //Arrange
+        var serviceMock = new Mock<IJokeService>();
+        var outputMock = new Mock<IOutputService>();
+
+
+        serviceMock.Setup(foo => foo.GetJoke()).Returns("Why is 6 afraid of 7? Because 7 ate 9!");
+
+        var jester = new Jester(outputMock.Object, serviceMock.Object);
+
+        //Act
+        jester.TellJoke();
+
+        //Assert
+        outputMock.Verify(foo => foo.WriteJoke("Why is 6 afraid of 7? Because 7 ate 9!"), Times.Once());
+    }
 }
