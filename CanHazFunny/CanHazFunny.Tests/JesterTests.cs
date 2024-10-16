0a48787e6115db85303d7fb8cd5545a1c22b4c16
ccornell88 <129800566+ccornell88@users.noreply.github.com>
2024-10-15 20:26:11 -0700

    Added Test
    Confirms if Chuck Norris joke is retrieved, a new joke is chosen.

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -8,15 +8,12 @@ public class JesterTests
 {
     [Fact]
     public void TellJoke_JokeRetrieved_SuccessfullyReceived()
-
     {
         //Arrange
         var serviceMock = new Mock<IJokeService>();
         var outputMock = new Mock<IOutputService>();
 
-
         serviceMock.Setup(foo => foo.GetJoke()).Returns("Why is 6 afraid of 7? Because 7 ate 9!");
-
         var jester = new Jester(outputMock.Object, serviceMock.Object);
 
         //Act
@@ -24,5 +21,26 @@ public class JesterTests
 
         //Assert
         outputMock.Verify(foo => foo.WriteJoke("Why is 6 afraid of 7? Because 7 ate 9!"), Times.Once());
+    }
+
+    [Fact]
+    public void TellJoke_ChuckNorrisJokeRetrieved_NewJokeChosenAndOutputCorrectly()
+    {
+        //Arrange
+        var serviceMock = new Mock<IJokeService>();
+        var outputMock = new Mock<IOutputService>();
+
+        serviceMock.SetupSequence(foo => foo.GetJoke())
+            .Returns("Chuck Norris joke")
+            .Returns("Valid joke");        
+
+        var jester = new Jester(outputMock.Object, serviceMock.Object);
+
+        //Act
+        jester.TellJoke();
+
+        //Assert
+        outputMock.Verify(foo => foo.WriteJoke("Chuck Norris joke"), Times.Never());
+        outputMock.Verify(foo => foo.WriteJoke("Valid joke"), Times.Once());
     }
 }
