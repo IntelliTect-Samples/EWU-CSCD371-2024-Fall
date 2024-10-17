39304b4dccdab9104384f38e0144c605b3e8fc60
Windeko <tylerjameswoody@gmail.com>
2024-10-16 21:40:42 -0700

    Added method paramater names to ArgumentNullException

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -9,9 +9,9 @@ public class Jester
 
     public Jester(IDisplayJokes jokeDisplayer, ITellJokes jokeTeller)
     {
-        JokeDisplayer = jokeDisplayer ?? throw new ArgumentNullException($"{nameof(jokeDisplayer)} cannot be null");
+        JokeDisplayer = jokeDisplayer ?? throw new ArgumentNullException(nameof(jokeDisplayer), $"{nameof(jokeDisplayer)} cannot be null");
 
-        JokeTeller = jokeTeller ?? throw new ArgumentNullException($"{nameof(jokeTeller)} cannot be null.");
+        JokeTeller = jokeTeller ?? throw new ArgumentNullException(nameof(jokeTeller), $"{nameof(jokeTeller)} cannot be null.");
     }
 
     public void TellJoke()
