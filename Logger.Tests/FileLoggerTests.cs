1d19424490bf2527bcda8bdf456fdce88235b425
Brady <bdullanty@ewu.edu>
2024-10-19 22:39:40 -0700

    Added in Constructor and FileLogger CreateLogger tests

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -5,11 +5,112 @@ namespace Logger.Tests;
 public class FileLoggerTests : FileLoggerTestsBase
 {    
     [Fact]
-    public void Create_GivenClassAndValidFileName_Success()
+    public void Constructor_GivenClassAndValidFileName_Success()
     {
         Assert.Equal(nameof(FileLoggerTests), Logger.LogSource);
         Assert.Equal(FilePath, Logger.FilePath);
     }
+
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Constructor_InvalidFilePath_ThrowsException(string? filePath)
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        Assert.ThrowsAny<Exception>( () =>
+        new FileLogger(nameof(FileLoggerTests), filePath!) );
+
+    }
+
+
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Constructor_InvalidLogSource_ThrowsException(string? logSource)
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        Assert.ThrowsAny<Exception>(() =>
+        new FileLogger(logSource!,FilePath));
+
+    }
+
+
+    [Fact]
+    public void Constructor_NullFileLoggerConfiguration_ThrowsNullReferenceException()
+    {
+
+        //Arrange
+        //Act
+
+        //Assert
+        Assert.Throws<NullReferenceException>(() =>
+        new FileLogger(null!));
+    }
+
+    [Fact]
+    
+    public void Constructor_ValidFileLoggerConfiguration_Constructs()
+    {
+        //Arrange
+        FileLoggerConfiguration config = new(FilePath, nameof(FileLoggerTests));
+        //Act
+        FileLogger logger =new(config);
+        //Assert
+        Assert.NotNull(logger);
+        Assert.Equal(FilePath, logger.FilePath);
+        Assert.Equal(nameof(FileLoggerTests),logger.LogSource);
+    }
+
+
+
+    //TODO:  ILogger.CreateLogger test for FileLogger
+
+    //In the[class name] [method name] method, We pass in [input] We expect it to [expected]
+    [Fact]
+    public void CreateLogger_ValidFileLoggerConfiguration_ReturnsFileLogger()
+    {
+        //Arrange(input)
+        FileLoggerConfiguration config = new(FilePath,nameof(FileLoggerTests));
+
+        //Act(call method)
+        FileLogger logger = FileLogger.CreateLogger(config);
+
+        //Assert(Assert for expect)
+        Assert.NotNull(logger);
+        Assert.Equal(nameof(FileLoggerTests),logger.LogSource);
+        Assert.Equal(logger.FilePath, FilePath);
+    }
+
+
+    //In the [classname][methodname] method, given [input], we expect [expection]
+    [Fact]
+    public void CreateLogger_NullFileLoggerConfiguration_ThrowsNullReferenceException ()
+    {
+        //Arrange(set up inputs for action)
+        //Act(Calls defined method ^^^^^)
+
+        //Assert(checks for expected result)
+        Assert.Throws<NullReferenceException>(()=>
+        FileLogger.CreateLogger(null!)
+        );
+
+    }
+
+
+
+
+
+
 
     [Fact]
     public async Task Log_Message_FileAppended()
