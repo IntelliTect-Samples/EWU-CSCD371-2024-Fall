67ab6d93188573c90309d9cccb86b19ae5fb1a80
Brady <bdullanty@ewu.edu>
2024-10-20 15:47:29 -0700

    Added in FullName record class

---
+++ Logger/FullName.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Logger;
+
+
+
+
+/// <summary>
+/// We chose to make the FullName Record a Reference Type (a class over a struct), because names can change
+/// and we want programs that contain that record to update. If we did a struct, it would copy but not update existing references.<br/><br/>
+///
+/// This class is mutable as it has setters that allow the name to change <br/>
+/// 
+/// </summary>
+public record class FullName
+{
+    private string? _first;
+    private string? _middle;
+    private string? _last;
+
+    /// <summary>
+    /// First is init only, and is never null.
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    public required string First { 
+        get => _first!;
+        set{
+            if( string.IsNullOrWhiteSpace(value)) throw new ArgumentException(nameof(value),$"Tried to set a null or empty value for {nameof(First)}");
+            _first = value;
+        } 
+    }
+    public required string Last
+    {
+        get => _last!;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException(nameof(value), $"Tried to set a null or empty value for {nameof(Last)}");
+            _last = value;
+        }
+    }
+    public string? Middle {
+        get {
+            return _middle ?? "";
+        }
+        init 
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException(nameof(Middle),$"{nameof(Middle)} was null or white space");
+            _middle = value;
+        }
+    }
+
+}
+
