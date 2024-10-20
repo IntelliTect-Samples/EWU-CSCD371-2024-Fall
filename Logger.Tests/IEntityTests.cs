264e8a779e6daac5fee5e13cf311c826cb604161
Brady <bdullanty@ewu.edu>
2024-10-20 16:05:51 -0700

    Added IEntity Tests

---
+++ Logger.Tests/IEntityTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Logger.Tests;
+
+public class IEntityTests
+{
+    [Fact]
+    public void Constructor_ValidInputs_IDK()
+    {
+        //Arrange
+        Guid guid = Guid.NewGuid();
+
+        //Act
+        var entity = new TestEntity() { Name = "My Full Name", Id = guid };
+
+        //Assert
+        Assert.NotNull(entity);
+        Assert.Equal("My Full Name", entity.Name);
+        Assert.Equal(guid, entity.Id);
+
+    }
+}
