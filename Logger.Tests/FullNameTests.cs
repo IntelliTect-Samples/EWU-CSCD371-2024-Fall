75903f807ef2e7807e93626d963ea7ef31bbbded
Austin Harrison <aharrison13@ewu.edu>
2024-10-21 13:06:50 -0700

    Created test to make sure default middle name was being set to empty if it was not provided. It will fail

--- Logger.Tests/FullNameTests.cs
+++ Logger.Tests/FullNameTests.cs
@@ -40,4 +40,20 @@ public class FullNameTests
         Assert.Equal(middleName, fullName.MiddleName);
         Assert.Equal(lastName, fullName.LastName);
     }
+
+    [Fact]
+    public void RecordCreation_NameWithOutMiddleName_Success()
+    {
+        // Arrange
+        string firstName = "Inigo";
+        string lastName = "Montoya";
+
+        // Act
+        FullName fullName = new(firstName, lastName);
+
+        // Assert
+        Assert.Equal(firstName, fullName.FirstName);
+        Assert.Equal(lastName, fullName.LastName);
+        Assert.Equal(string.Empty, fullName.MiddleName);
+    }
 }
