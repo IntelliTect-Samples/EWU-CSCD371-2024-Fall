264d9ae8bd4fabacb3b6e5105be3231940958526
Windeko <tylerjameswoody@gmail.com>
2024-10-20 18:05:35 -0700

    Added checks for Argument Exceptions. Added fields for backing properties.

--- Logger/Book.cs
+++ Logger/Book.cs
@@ -2,8 +2,48 @@
 
 public record class Book
 {
-    public required string ISBN { get; init; }
-    public required string Author { get; init; }
-    public required int PublicationYear { get; init; }
-    public required string Title { get; init; }
+    private string? _isbn;
+    private string? _author;
+    private int? _publicationYear;
+    private string? _title;
+
+    public required string ISBN
+    {
+        get => _isbn!;
+        init
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException(nameof(value), $"{nameof(ISBN)} cannot be null or empty.");
+            _isbn = value;
+        }
+    }
+
+    public required string Author
+    {
+        get => _author!;
+        init
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException(nameof(value), $"{nameof(Author)} cannot be null or empty.");
+            _author = value;
+        }
+    }
+
+    public required int PublicationYear
+    {
+        get => _publicationYear ?? throw new InvalidOperationException($"{nameof(PublicationYear)} is not set.");
+        init
+        {
+            if (value <= 0) throw new ArgumentException(nameof(value), $"{nameof(PublicationYear)} must be greater than zero.");
+            _publicationYear = value;
+        }
+    }
+
+    public required string Title
+    {
+        get => _title!;
+        init
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException(nameof(value), $"{nameof(Title)} cannot be null or empty.");
+            _title = value;
+        }
+    }
 }
