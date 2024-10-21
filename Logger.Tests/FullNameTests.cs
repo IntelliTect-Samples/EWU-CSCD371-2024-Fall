15a607dd7d63d8b2b3fe64cda05d7fea10f61bac
Austin Harrison <aharrison13@ewu.edu>
2024-10-21 12:14:36 -0700

    Start of test driven development. Created a test for full name record. (Test will fail because there is no implementation)

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Logger.Tests;
+
+public class FullNameTests
+{
+    [Fact]
+    public void RecordCreation_WithFullName_Success()
+    {
+        // Arrange
+
+        // Act
+        FullName fullName = new();
+
+        // Assert
+        Assert.NotNull(fullName);
+        Assert.IsType<FullName>(fullName);
+    }
+}
