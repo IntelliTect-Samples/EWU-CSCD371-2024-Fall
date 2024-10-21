2fb090c213b04b1c2caa2110a6fe0a99c1311ba4
EL <eleachman04@ewu.edu>
2024-10-21 14:29:04 -0700

    Test written for interface

---
+++ Logger.Tests/IEntityTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Logger.Tests;
+
+public class IEntityTests
+{
+
+    [Fact]
+    public void MockEntity_SetAndGetProperties_Success()
+    {
+        //Arrange
+        var mockEntity = new MockEntity();
+        var newGuid = Guid.NewGuid();
+
+        //Act
+        mockEntity.Id = newGuid;
+        mockEntity.Name = "Test Name";
+
+        //Assert
+        Assert.Equal(newGuid, mockEntity.Id);
+        Assert.Equal("Test Name", mockEntity.Name);
+    }
+}
+
+public class MockEntity : IEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+}
+
