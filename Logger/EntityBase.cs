7c3239377f66ad9c737771fa4d0c2232bb587a4b
EL <eleachman04@ewu.edu>
2024-10-21 14:55:12 -0700

    Wrote base Entity

---
+++ Logger/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public abstract class EntityBase : IEntity
+    {
+        public Guid Id { get; private set; }
+
+        public EntityBase() {
+            Id = Guid.NewGuid();
+        }
+
+        public abstract string Name { get; init; }
+    }
+}
