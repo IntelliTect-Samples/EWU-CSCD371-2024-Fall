491940c7dfc143ca6649017a2afa7f97c7eecadc
Brady <bdullanty@ewu.edu>
2024-10-20 18:01:49 -0700

    Added TestEntity From Abstract class

---
+++ Logger.Tests/TestEntityFromAbstract.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Logger.Tests;
+
+public record class TestEntityFromAbstract : Entity
+{
+    private FullName fullName;
+    public TestEntityFromAbstract(string firstName,string lastName,string? middleName = null)
+    {
+        fullName = new() { First = firstName, Last = lastName, Middle = middleName };
+    }
+    protected override string ParseName()
+    {
+        return fullName.ToString();
+    }
+}
