5f0a9cbc690a24880d46faaa4a93b29a461ba3d0
Oscar Ruelas <oruelas@ewu.edu>
2024-10-21 10:51:24 -0700

    implemented FullName.cs

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -1,3 +1,22 @@
 namespace Logger;
 
-public record FullName();
+public record FullName(string FirstName, string? MiddleName, string LastName)
+{
+    
+    /*
+        w/out this override I get the following error for all properties:
+        Positional property 'Logger.FullName.[propertyName]' 
+        is never accessed (except in implicit Equals/ToString implementations)
+     */
+    public override string ToString()
+    {
+        if(MiddleName is null)
+        {
+            return $"{FirstName} {LastName}";
+        }
+        else
+        {
+            return $"{FirstName} {MiddleName} {LastName}";
+        }
+    }
+}
