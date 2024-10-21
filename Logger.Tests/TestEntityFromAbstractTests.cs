7d1f0e4f2a07f803dc8aa79d4c55828da25c718e
Brady <bdullanty@ewu.edu>
2024-10-20 18:00:22 -0700

    Created test class for testing a test abstract implementation

---
+++ Logger.Tests/TestEntityFromAbstractTests.cs
@@ -0,0 +1,29 @@
+﻿
+using Xunit;
+
+namespace Logger.Tests;
+
+public class TestEntityFromAbstractTests
+{
+    [Fact]
+    public void Constructor_FirstAndLast_CreatesTestEntityFromAbstract()
+    {
+        //Arrange
+        
+        //Act
+        TestEntityFromAbstract testEntityFromAbstract = new("first","last");
+        //Assert
+        Assert.NotNull(testEntityFromAbstract);
+        Assert.Equal("first last", testEntityFromAbstract.Name);
+    }
+    [Fact]
+    public void Constructor_FirstMiddleLast_CreatesTestEntityFromAbstract()
+    {
+        //Arrange
+
+        //Act
+        TestEntityFromAbstract testEntityFromAbstract = new("first", "last", "middle");
+        //Assert
+        Assert.Equal("first middle last", testEntityFromAbstract.Name);
+    }
+}
