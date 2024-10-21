b03f1481f34a12230ff3bbd83bd8be77d9819778
Windeko <tylerjameswoody@gmail.com>
2024-10-20 17:30:35 -0700

    Created initial test for Book record

---
+++ Logger.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Logger.Tests;
+
+public class BookTests
+{
+    [Theory]
+    [InlineData("ISBN", "Author", 1998, "Title")]
+    public void Constructor_AllParamaters_MakesRecord(string isbn, string author, int publicationYear, string title)
+    {
+        //Arrange
+        string expectedIsbn = "ISBN";
+        string expectedAuthor = "Author";
+        int expectedPublicationYear = 1998;
+        string expectedTitle = "Title";
+
+        //Act
+        Book book = new() { ISBN = isbn, Author = author, PublicationYear = publicationYear, Title = title };
+
+        //Assert
+        Assert.NotNull(book);
+        Assert.Equal(book.ISBN, expectedIsbn);
+        Assert.Equal(book.Author, expectedAuthor);
+        Assert.Equal(book.PublicationYear, expectedPublicationYear);
+        Assert.Equal(book.Title, expectedTitle);
+    }
+}
