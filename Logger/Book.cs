b2380b6e29b1fee3dbadfb5d51e466c1b9da6a6a
Windeko <tylerjameswoody@gmail.com>
2024-10-20 17:31:04 -0700

    Created initial Book record

---
+++ Logger/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Logger;
+
+public record class Book
+{
+    public required string ISBN { get; init; }
+    public required string Author { get; init; }
+    public required int PublicationYear { get; init; }
+    public required string Title { get; init; }
+}
