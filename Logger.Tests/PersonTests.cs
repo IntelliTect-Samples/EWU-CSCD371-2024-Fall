cafa5b873418b86710752d379207db2bb2d4e161
EL <eleachman04@ewu.edu>
2024-10-22 13:10:12 -0700

    Added PersonTests to begin development on Person class

---
+++ Logger.Tests/PersonTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Logger.Tests;
+
+public class PersonTests
+{
+
+    [Fact]
+    public void Person_Constructed_exists()
+    {
+        // Arrange
+        string name = "John Doe";
+
+        // Act
+        TestPerson person = new(name);
+
+        // Assert
+        Assert.NotNull(person);
+    }
+
+}
+
+public class TestPerson(string name) : Person
+{
+    public override string Name { get; } = name;
+}
