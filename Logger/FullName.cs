1533c1f03f0167bd7fc47fd5ec0dd37f636632f1
ccornell88 <cjcornell38@gmail.com>
2024-10-22 13:34:22 -0700

    Create FullName Record
    Added comments per the instructions.

---
+++ Logger/FullName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Logger
+{
+    // So FullName record holds the first, middle(optional), and LastName of a person.
+    // I implemented this as a reference type (record class)
+    // I did the because a persons full name can be thought of as one object.
+    // If FullName were a value type, every time you pass a Student instance around,
+    // you would be copying both the Student object and the FullName object
+
+    // The FullName record is immutable by default.
+    // So,  FirstName, MiddleName, and LastName can only be set during object creation and cannot be changed afterward.
+    // Immutability ensures that once created, the data remains unchanged, and is improtant when people have the same name.
+
+    public record class FullName(string FirstName, string LastName, string? MiddleName = null)
+    {
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(MiddleName))
+            {
+                return $"{FirstName} {LastName}";
+            }
+            else
+            {
+                return $"{FirstName} {MiddleName} {LastName}";
+            }
+        }
+    }
+}
