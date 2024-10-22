6e309c21bedd25fd56f247d2427b38d5b93040b9
EL <eleachman04@ewu.edu>
2024-10-22 14:09:36 -0700

    Added person class

---
+++ Logger/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Logger;
+
+public abstract record class Person(FullName NameDetails) : EntityBase
+{
+    public FullName NameDetails { get; set; } = NameDetails;
+
+    public override string Name => NameDetails.ToString();
+}
