60e75e2c7b035e9fca90ecf9f350f4a5e0ff0eaf
Austin Harrison <aharrison13@ewu.edu>
2024-10-21 22:57:06 -0700

    Made a test class for Student, Employee, and Book. Made a test in Student that will fail because Student does not exist.

---
+++ Logger.Tests/StudentTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Logger.Tests;
+
+public class StudentTests
+{
+    [Fact]
+    public void StudentCreation_NotNull_ReturnsNotNull()
+    {
+        //Act
+        Student student = new();
+
+        //Assert
+        Assert.NotNull(student);
+    }
+}
