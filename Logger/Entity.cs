c23c47c84ef006ed043d65a16f22662226cdf6a0
Windeko <tylerjameswoody@gmail.com>
2024-10-22 10:07:05 -0700

    Changed ID property to be more readable

--- Logger/Entity.cs
+++ Logger/Entity.cs
@@ -3,7 +3,11 @@
 public abstract record class Entity : IEntity
 {
     private Guid _guid;
-    public Guid Id { get => _guid!; init { _guid = Guid.NewGuid(); } }
+    public Guid Id 
+    { 
+        get => _guid!; 
+        init { _guid = Guid.NewGuid(); } 
+    }
 
     public string Name => ParseName();
     protected abstract string ParseName();
