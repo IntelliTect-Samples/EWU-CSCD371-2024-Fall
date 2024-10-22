1487a088210f0ca260a295e77485c613f2f51283
Windeko <tylerjameswoody@gmail.com>
2024-10-21 20:04:58 -0700

    added a test ensuring null is handled properly

--- Logger.Tests/EmployeeTests.cs
+++ Logger.Tests/EmployeeTests.cs
@@ -9,17 +9,17 @@ public class EmployeeTests
     public void Constructor_AllParamaters_MakesEmployee(string employeeId, string position, string first, string last, string middle)
     {
         //Arrange
-        Employee employee = new("EmployeeID", "Position", "First", "Last", "Middle");
+        Employee expectedEmployee = new("EmployeeID", "Position", "First", "Last", "Middle");
 
         //Act
 
         //Assert
-        Assert.NotNull(employee);
-        Assert.Equal(employee.EmployeeID, employeeId);
-        Assert.Equal(employee.Position, position);
-        Assert.Equal(employee.EmployeeFullName.First, first);
-        Assert.Equal(employee.EmployeeFullName.Middle, middle);
-        Assert.Equal(employee.EmployeeFullName.Last, last);
+        Assert.NotNull(expectedEmployee);
+        Assert.Equal(expectedEmployee.EmployeeID, employeeId);
+        Assert.Equal(expectedEmployee.Position, position);
+        Assert.Equal(expectedEmployee.EmployeeFullName.First, first);
+        Assert.Equal(expectedEmployee.EmployeeFullName.Middle, middle);
+        Assert.Equal(expectedEmployee.EmployeeFullName.Last, last);
     }
 
     [Theory]
@@ -27,15 +27,30 @@ public class EmployeeTests
     public void Constructor_AllParamatersExceptMiddle_MakesEmployee(string employeeId, string position, string first, string last)
     {
         //Arrange
-        Employee employee = new("EmployeeID", "Position", "First", "Last", "Middle");
+        Employee expectedEmployee = new("EmployeeID", "Position", "First", "Last", "Middle");
 
         //Act
 
         //Assert
-        Assert.NotNull(employee);
-        Assert.Equal(employee.EmployeeID, employeeId);
-        Assert.Equal(employee.Position, position);
-        Assert.Equal(employee.EmployeeFullName.First, first);
-        Assert.Equal(employee.EmployeeFullName.Last, last);
+        Assert.NotNull(expectedEmployee);
+        Assert.Equal(expectedEmployee.EmployeeID, employeeId);
+        Assert.Equal(expectedEmployee.Position, position);
+        Assert.Equal(expectedEmployee.EmployeeFullName.First, first);
+        Assert.Equal(expectedEmployee.EmployeeFullName.Last, last);
+    }
+
+    [Theory]
+    [InlineData(null, "Position", "First", "Last", "Middle")]
+    [InlineData("EmployeeID", null, "First", "Last", "Middle")]
+    [InlineData("EmployeeID", "Position", null, "Last", "Middle")]
+    [InlineData("EmployeeID", "Position", "First", null, "Middle")]
+    public void Constructor_BadParamaters_ThrowsException(string? employeeId, string? position, string? first, string? last, string? middle)
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        Assert.ThrowsAny<ArgumentException>(() => new Employee(employeeId!, position!, first!, last!, middle!));
     }
 }
