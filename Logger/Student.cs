770ce18473f3ead66a6bb33ac4475dd0a6459001
ccornell88 <cjcornell38@gmail.com>
2024-10-22 14:07:33 -0700

    Created Student.cs
    with calculated property, no backing field.

---
+++ Logger/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public record Student(FullName FullName) : BaseEntity
+    {
+        // calculated property, no backing 
+        public override string Name => FullName.ToString();
+    }
+}
