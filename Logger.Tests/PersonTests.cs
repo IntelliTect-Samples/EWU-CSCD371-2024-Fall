775397413001c1346d2d36972cb9c1cb06a0d716
EL <eleachman04@ewu.edu>
2024-10-22 14:09:23 -0700

    Added tests

--- Logger.Tests/PersonTests.cs
+++ Logger.Tests/PersonTests.cs
@@ -11,21 +11,33 @@ public class PersonTests
 {
 
     [Fact]
-    public void Person_Constructed_exists()
+    public void Initialization_Constructed_exists()
     {
         // Arrange
-        string name = "John Doe";
+        FullName fullName = new("Inigo", "Montoya", "Ella");
 
         // Act
-        TestPerson person = new(name);
+        TestPerson person = new(fullName);
 
         // Assert
         Assert.NotNull(person);
     }
 
+    [Fact]
+    public void Initialization_GivenINput_ReturnsExpectedName()
+    {
+        // Arrange
+        FullName fullName = new("Inigo", "Montoya", "Ella");
+        String expected = "Inigo Ella Montoya";
+        //Act
+        TestPerson person = new(fullName);
+
+        //Assert
+        Assert.Equal(expected, person.Name);
+    }
 }
 
-public class TestPerson(string name) : Person
+public record class TestPerson(FullName NameDetails) : Person(NameDetails)
 {
-    public override string Name { get; } = name;
+
 }
