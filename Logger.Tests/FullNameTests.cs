75519d3feefee93496fa1d4cae205a4838e5d114
kflannery2001 <kflannery@ewu.edu>
2024-10-22 12:57:22 -0700

    FullName and FullNameTests

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace Logger.Tests;
+
+public class FullNameTests
+{
+    [Fact]
+    public void Create_FullName_Success()
+    {
+        // Arrange
+        FullName testName = new FullName("Kevin", "Riain", "Flannery");
+        // Act
+
+        // Assert
+        Assert.Equal("Kevin", testName.firstName);
+        Assert.Equal("Riain", testName.middleName);
+        Assert.Equal("Flannery", testName.lastName);
+    }
+
+    [Fact]
+    public void Create_FullNameWithoutMiddleName_Success()
+    {
+        // Arrange
+        FullName testName = new("Kevin", "Flannery");
+        // Act
+
+        // Assert
+        Assert.Equal("Kevin", testName.firstName);
+        Assert.Null(testName.middleName);
+        Assert.Equal("Flannery", testName.lastName);
+    }
+
+    [Fact]
+    public void ToString_Success()
+    {
+        // Arrange
+        FullName testName = new("Kevin", "Riain", "Flannery");
+        // Act
+        string result = testName.ToString();
+        // Assert
+        Assert.Equal("Kevin Riain Flannery", result);
+    }
+}
---
+++ Logger/FullName.cs
@@ -0,0 +1,10 @@
+﻿namespace Logger;
+
+// We chose to define a value type here because we want to ensure that the FullName is immutable.
+// The type is immutable because it is a readonly struct, furthermore, your name isn't going to change (for the most part).
+public readonly record struct FullName(string firstName, string? middleName, string lastName)
+{
+    public FullName(string firstName, string lastName) : this(firstName, null, lastName) { }
+
+    public override string ToString() => middleName is null ? $"{firstName} {lastName}" : $"{firstName} {middleName} {lastName}";
+}
