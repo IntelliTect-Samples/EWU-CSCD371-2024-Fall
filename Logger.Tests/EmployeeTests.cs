03ecccd260b714b5ff58d81863332acd9246040e
Windeko <tylerjameswoody@gmail.com>
2024-10-21 19:39:37 -0700

    Created initial test for Employee record

---
+++ Logger.Tests/EmployeeTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Logger.Tests;
+
+public class EmployeeTests
+{
+    [Fact]
+    public void Constructor_AllParamaters_MakesEmployee()
+    {
+        //Arrange
+        Employee employee = new("EmployeeID", "Position", "First", "Last", "Middle");
+
+        //Act
+
+        //Assert
+        Assert.NotNull(employee);
+    }
+}
