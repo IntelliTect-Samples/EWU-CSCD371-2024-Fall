f9381f67d7249c92eb6512822dd35dca3cf74cc3
Windeko <tylerjameswoody@gmail.com>
2024-10-22 21:51:37 -0700

    Small changes to whitespace/null description

--- Logger/Student.cs
+++ Logger/Student.cs
@@ -1,13 +1,7 @@
-using System.Collections.Generic;
-using System.Reflection;
-
 namespace Logger;
 
 public record class Student(string id, string school, string first, string last, string? middle = null) : Person(first, last, id, middle)
 {
-
-    public string SchoolName = school ?? throw new ArgumentNullException(nameof(school), "School string was null");
+    public string SchoolName = school ?? throw new ArgumentNullException(nameof(school), $"{nameof(school)} was null");
     public string? GradeLevel { get; set; }
-
-
 }
