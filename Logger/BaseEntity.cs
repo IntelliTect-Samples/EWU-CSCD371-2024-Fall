96f4700b992a65b40abc7709befe4f3019ff8b4a
kflannery2001 <kflannery@ewu.edu>
2024-10-22 18:13:55 -0700

    created BaseEntity

---
+++ Logger/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger;
+
+public abstract record class BaseEntity : IEntity
+{
+    // Every entity has an Id
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    // Each entity decides how to use Name
+    public abstract string Name { get; set; }
+}
