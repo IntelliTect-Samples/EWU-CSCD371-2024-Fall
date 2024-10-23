1da1dd056bf24d56e4789df5add540bc284b07c6
Brady <bdullanty@ewu.edu>
2024-10-22 21:45:13 -0700

    Ensured we passed middle name down through person class

--- Logger/Person.cs
+++ Logger/Person.cs
@@ -6,10 +6,10 @@ public abstract record class Person : Entity
 
 {
 
-    public Person(string first, string last, string id, string middle = "")
+    public Person(string first, string last, string id, string? middle = null)
     {
-        MyFullName = new FullName() { First = first,Last = last};
-        Id = id;
+        MyFullName = new FullName() { First = first,Last = last, Middle = middle} ;
+        Id = id ?? throw new ArgumentNullException();
     }
     public FullName MyFullName { get; set; } 
     public string Id { get; init; }
