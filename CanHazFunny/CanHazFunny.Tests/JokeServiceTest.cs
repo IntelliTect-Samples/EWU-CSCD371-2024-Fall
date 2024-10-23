ab75a3594cbcba0fd40010d6e40a7985a24138de
Kmiller <kmiller71@ewu.edu>
2024-10-23 01:16:19 -0700

    Oops I Did It Again
    Missing ;

--- CanHazFunny/CanHazFunny.Tests/JokeServiceTest.cs
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTest.cs
@@ -6,25 +6,24 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace CanHazFunny.Tests
+namespace CanHazFunny.Tests;
+
+public class JokeServiceTest
 {
-    public class JokeServiceTest
+    [Fact]
+    public void IJokeService_AquireJoke_ReturnsExpectedJoke()
     {
-        [Fact]
-        public void IJokeService_AquireJoke_ReturnsExpectedJoke()
-        {
-            //Arrange
-            var mockjokeService = new Mock<IJokeService>();
+        //Arrange
+        var mockjokeService = new Mock<IJokeService>();
 
-            string expectedJoke = "*Refrence to Other Funny Joke*";
+        string expectedJoke = "*Refrence to Other Funny Joke*";
 
-            mockjokeService.Setup(service => service.GetJoke()).Returns(expectedJoke);
-            //Act
-            string retreviedJoke = mockjokeService.Object.GetJoke();
+        mockjokeService.Setup(service => service.GetJoke()).Returns(expectedJoke);
+        //Act
+        string retreviedJoke = mockjokeService.Object.GetJoke();
 
-            //Assert
-            Assert.Equal(expectedJoke, retreviedJoke);
-        }
-
+        //Assert
+        Assert.Equal(expectedJoke, retreviedJoke);
     }
+
 }
