e13188a5092abe90d5921ca5c7ec33bed0e45271
Austin Harrison <aharrison13@ewu.edu>
2024-10-23 15:04:11 -0700

    Added Comment for FullName class

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -2,6 +2,12 @@
 
 public readonly record struct FullName
 {
+    // FullName is defined as a record struct (value type) because names are typically small lightweight data. 
+    // Value types are more appropriate for immutable,short-lived data that represents a single value, like a name.
+
+    // FullName is immutable because it's defined as a readonly record struct.
+    // This ensures that once a FullName instance is created, its properties cannot be changed.
+    // If we need to change a name, we ensured that we can just create a new FullName record and replace it in the classes that use it.
 
     public string FirstName { get; }
     public string LastName { get; }
