730f59a25b1d9149df72bc50439cb7631a4ae378
Windeko <tylerjameswoody@gmail.com>
2024-10-22 22:21:30 -0700

    Renamed MyFullName to PersonFullName

--- Logger/Person.cs
+++ Logger/Person.cs
@@ -4,13 +4,13 @@ public abstract record class Person : Entity
 {
     public Person(string first, string last, string id, string? middle = null)
     {
-        MyFullName = new FullName() { First = first,Last = last, Middle = middle} ;
+        PersonFullName = new FullName() { First = first, Last = last, Middle = middle } ;
         Id = id ?? throw new ArgumentNullException(nameof(id), $"{nameof(id)} was null.");
     }
-    public FullName MyFullName { get; set; } 
+    public FullName PersonFullName { get; set; } 
     public string Id { get; init; }
     protected override string ParseName()
     {
-        return MyFullName.ToString();
+        return PersonFullName.ToString();
     }
 }
