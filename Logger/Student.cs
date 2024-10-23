22b21861205d979a5f88b225153388457c396a23
Brady <bdullanty@ewu.edu>
2024-10-22 21:38:37 -0700

    Refactored student using person abstract class as base

--- Logger/Student.cs
+++ Logger/Student.cs
@@ -1,19 +1,13 @@
-﻿namespace Logger;
+﻿using System.Collections.Generic;
+using System.Reflection;
 
-public record class Student : Entity
+namespace Logger;
+
+public record class Student(string school, string id, string first, string last, string middle = "") : Person(first, last, "123455", middle)
 {
-    public FullName MyFullName { get; set; }
-    public string SchoolName { get; set; }
+
+    public string SchoolName = school ?? throw new ArgumentNullException(nameof(school), "School string was null");
     public string? GradeLevel { get; set; }
 
-    public Student(string school, string first, string last, string? middle = null)
-    {
-        MyFullName = new() { First = first, Last = last, Middle = middle };
-        SchoolName = school ?? throw new ArgumentNullException(nameof(school),"School string was null");
-    }
 
-    protected override string ParseName()
-    {
-        return MyFullName.ToString();
-    }
 }
