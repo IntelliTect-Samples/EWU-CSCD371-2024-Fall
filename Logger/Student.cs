6c8e31cbbd79cfa572e6ff0e33f53c68df335d0a
Windeko <tylerjameswoody@gmail.com>
2024-10-22 21:18:59 -0700

    Added an ID property to Student

--- Logger/Student.cs
+++ Logger/Student.cs
@@ -3,13 +3,15 @@
 public record class Student : Entity
 {
     public FullName MyFullName { get; set; }
+    public string ID { get; init; }
     public string SchoolName { get; set; }
     public string? GradeLevel { get; set; }
 
-    public Student(string school, string first, string last, string? middle = null)
+    public Student(string studentID, string school, string first, string last, string? middle = null)
     {
         MyFullName = new() { First = first, Last = last, Middle = middle };
         SchoolName = school ?? throw new ArgumentNullException(nameof(school),"School string was null");
+        ID = studentID ?? throw new ArgumentNullException(nameof(studentID), $"{nameof(studentID)} was null.");
     }
 
     protected override string ParseName()
