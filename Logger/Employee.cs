7bb5eb6a535a6f5bc3fcdec5eb38bc583aab8627
Windeko <tylerjameswoody@gmail.com>
2024-10-22 22:21:15 -0700

    Refactored Employee to use the Person parent record

--- Logger/Employee.cs
+++ Logger/Employee.cs
@@ -1,20 +1,5 @@
 ﻿namespace Logger;
-
-public record class Employee : Entity
+public record class Employee (string id, string position, string first, string last, string? middle = null) : Person(first, last, id, middle)
 {
-    public FullName EmployeeFullName { get; set; }
-    public string ID { get; init; }
-    public string Position { get; set; }
-
-    public Employee(string employeeID, string position, string first, string last, string? middle = null)
-    {
-        EmployeeFullName = new() { First = first, Last = last, Middle = middle };
-        ID = employeeID ?? throw new ArgumentNullException(nameof(employeeID), $"{nameof(employeeID)} was null.");
-        Position = position ?? throw new ArgumentNullException(nameof(position), $"{nameof(position)} was null.");
-    }
-    //We implemented this implicitly as we felt that parseName works well on implementations and forcing a cast does not help.
-    protected override string ParseName()
-    {
-        return EmployeeFullName.ToString();
-    }
+    public string Position = position ?? throw new ArgumentNullException(nameof(position), $"{nameof(position)} was null");
 }
