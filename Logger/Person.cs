9f50669cb8c45708f8c0a99c69729395b55b43f1
Brady <bdullanty@ewu.edu>
2024-10-22 21:38:23 -0700

    Added Person Class

---
+++ Logger/Person.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Logger;
+
+public abstract record class Person : Entity
+
+{
+
+    public Person(string first, string last, string id, string middle = "")
+    {
+        MyFullName = new FullName() { First = first,Last = last};
+        Id = id;
+    }
+    public FullName MyFullName { get; set; } 
+    public string Id { get; init; }
+    protected override string ParseName()
+    {
+        return MyFullName.ToString();
+    }
+
+}
