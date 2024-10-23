4b51014edc3e41f4f5b2dec04dfea1015b928f8b
ccornell88 <cjcornell38@gmail.com>
2024-10-23 10:57:48 -0700

    Create FullName Tests

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace Logger.Tests
+{
+    public class FullNameTests
+    {
+        [Fact]
+        public void FullName_WithMiddleName_ShouldReturnCorrectFormat()
+        {
+            // Arrange
+            var fullName = new FullName("Chris", "Cornell", "J");
+
+            // Act
+            var result = fullName.ToString();
+
+            // Assert
+            Assert.Equal("Chris J Cornell", result);
+        }
+
+        [Fact]
+        public void FullName_WithoutMiddleName_ShouldReturnCorrectFormat()
+        {
+            // Arrange
+            var fullName = new FullName("Chris", "Cornell", " ");
+
+            // Act
+            var result = fullName.ToString();
+
+            // Assert
+            Assert.Equal("Chris Cornell", result);
+
+        }
+
+
+
+        [Fact]
+        public void FullName_WithBlankMiddleName_ShouldSetMiddleNameToNull()
+        {
+            // Arrange
+            var fullName = new FullName("Chris", "Cornell", " ");
+
+            // Act & Assert
+            Assert.Null(fullName.MiddleName); 
+        }
+    }
+    
+}
