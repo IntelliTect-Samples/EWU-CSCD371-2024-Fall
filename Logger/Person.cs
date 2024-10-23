7483af56a4c933fa15dcf2252157df1cc017d23d
Austin Harrison <aharrison13@ewu.edu>
2024-10-22 21:19:18 -0700

    Chnaged some naming conventions

--- Logger/Person.cs
+++ Logger/Person.cs
@@ -2,17 +2,17 @@
 
 public abstract record class Person(FullName NameDetails) : EntityBase
 {
-    public FullName NameDetails { get; set; } = NameDetails;
+    public FullName NameRecord { get; set; } = NameDetails;
 
     public override string Name
     {
         get
         {
-            if (!string.IsNullOrWhiteSpace(NameDetails.MiddleName))
+            if (!string.IsNullOrWhiteSpace(NameRecord.MiddleName))
             {
-                return $"{NameDetails.FirstName} {NameDetails.MiddleName} {NameDetails.LastName}";
+                return $"{NameRecord.FirstName} {NameRecord.MiddleName} {NameRecord.LastName}";
             }
-            return $"{NameDetails.FirstName} {NameDetails.LastName}";
+            return $"{NameRecord.FirstName} {NameRecord.LastName}";
         }
     }
 }
--- Logger/Student.cs
+++ Logger/Student.cs
@@ -2,17 +2,5 @@
 
 public record class Student(FullName NameDetails) : Person(NameDetails)
 {
-    // TODO: Figure out if these should be set or init or read only
-    //public FullName NameDetails { get; set; } = NameDetails;
-    //public override string Name
-    //{
-    //    get
-    //    {
-    //        if (!string.IsNullOrWhiteSpace(NameDetails.MiddleName))
-    //        {
-    //            return $"{NameDetails.FirstName} {NameDetails.MiddleName} {NameDetails.LastName}";
-    //        }
-    //        return $"{NameDetails.FirstName} {NameDetails.LastName}";
-    //    }
-    //}
+
 }
