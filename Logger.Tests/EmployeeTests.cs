e0d52cefe3e8f4e4fd948a08cb2f2fccfb8d81cd
Ben Knoll <bknoll@ewu.edu>
2024-10-22 22:48:08 -0700

    Added Employee

---
+++ Logger.Tests/EmployeeTests.cs
@@ -0,0 +1,66 @@
+﻿using Xunit;
+namespace Logger.Tests;
+
+public class EmployeeTests
+{
+    [Fact]
+    public void Name_SetValid_Success()
+    {
+        // Arrange
+        Employee employee = new Employee();
+        employee.Name = "123456: Billy Jean: Singer";
+
+        // Act
+
+        // Assert
+        Assert.Equal("123456: Billy Jean: Singer", employee.Name);
+    }
+
+    [Fact]
+    public void Name_SetInvalidId_FormatException()
+    {
+        // Arrange
+        Employee employee = new Employee();
+
+        // Act
+
+        // Assert
+        Assert.Throws<FormatException>(() => employee.Name = "123A456: Billy Jean: Engineer");
+    }
+
+    [Fact]
+    public void Name_SetInvalidNameLength_FormatException()
+    {
+        // Arrange
+        Employee employee = new Employee();
+
+        // Act
+
+        // Assert
+        Assert.Throws<ArgumentException>(() => employee.Name = "123456: Billy Jean Tommy Bean: Engineer");
+    }
+
+    [Fact]
+    public void Name_SetNull_ArgumentNullException()
+    {
+        // Arrange
+        Employee employee = new Employee();
+
+        // Act
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() => employee.Name = null!);
+    }
+
+    [Fact]
+    public void Name_SetEmpty_ArgumentException()
+    {
+        // Arrange
+        Employee employee = new Employee();
+
+        // Act
+
+        // Assert
+        Assert.Throws<ArgumentException>(() => employee.Name = "");
+    }
+}
---
+++ Logger/Employee.cs
@@ -0,0 +1,62 @@
+﻿namespace Logger;
+
+public record class Employee : BaseEntity
+{
+    override public string Name
+    {
+        get
+        {
+            return $"{EmployeeId}: {EmployeeFullName}: {EmployeeJobTitle}";
+        }
+        set
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value);
+            string[] members = value.Trim().Split(':');
+
+            try
+            {
+                EmployeeId = Int32.Parse(members[0]);
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("Invalid Student Id");
+            }
+
+            string[] studentNames = members[1].Trim().Split(" ");
+
+            if (studentNames.Length == 2)
+            {
+                EmployeeFullName = new FullName(studentNames[0], studentNames[1]);
+            }
+            else if (studentNames.Length == 3)
+            {
+                EmployeeFullName = new FullName(studentNames[0], studentNames[1], studentNames[2]);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid Student Name", members[1]);
+            }
+
+            EmployeeJobTitle = members[2].Trim();
+        }
+    }
+
+    public int EmployeeId { get; set; }
+
+    public FullName EmployeeFullName { get; set; }
+
+    private string? _employeeJobTitle;
+    public string EmployeeJobTitle
+    {
+        get
+        {
+            return _employeeJobTitle!;
+        }
+        set
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+            _employeeJobTitle = value;
+        }
+    }
+
+}
