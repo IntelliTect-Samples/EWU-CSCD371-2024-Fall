838dfbff27bf61cc2a0f6e040e5118799babdfae
Brady <bdullanty@ewu.edu>
2024-10-22 20:26:48 -0700

    Reimplemented conflict revertion white space fix

--- Logger/Entity.cs
+++ Logger/Entity.cs
@@ -8,7 +8,11 @@ public abstract record class Entity : IEntity
     /// Implementations may have their own Id value, and we want to seperate these.
     /// Ex: Student.Id Should not return a Guid, but instead a value that fits the school guidelines perhaps
     /// </summary>
-    Guid IEntity.Id { get => _guid!; init { _guid = Guid.NewGuid(); } }
+    Guid IEntity.Id 
+    { 
+        get => _guid!; 
+        init { _guid = Guid.NewGuid(); }
+    }
 
     public string Name => ParseName();
     protected abstract string ParseName();
