6f67abb16299b28697be176c25c52de4c22c02a4
ccornell88 <cjcornell38@gmail.com>
2024-10-23 09:22:49 -0700

    Create Book

---
+++ Logger/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public record Book(string Title) : BaseEntity
+    {
+        //No backing field
+        public override string Name => Title;
+    }
+}
