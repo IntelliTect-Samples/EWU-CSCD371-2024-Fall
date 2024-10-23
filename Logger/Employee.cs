a2cd5df0ab71e7811dbe2d93787f828809b8eeb5
ccornell88 <cjcornell38@gmail.com>
2024-10-23 10:56:49 -0700

    Create Employee

---
+++ Logger/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public record Employee(FullName FullName) : BaseEntity
+    {
+        public override string Name => FullName.ToString();
+    }
+}
