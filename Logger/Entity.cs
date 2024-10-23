2a1aced821b534d40a5e76543c5529a82591ea32
Brady <bdullanty@ewu.edu>
2024-10-22 20:24:32 -0700

    Made IEntity.Id to Explicit so we can have an Id value in implementation

--- Logger/Entity.cs
+++ Logger/Entity.cs
@@ -4,7 +4,12 @@ namespace Logger;
 public abstract record class Entity : IEntity
 {
     private Guid _guid;
-    public Guid Id { get => _guid!; init { _guid = Guid.NewGuid(); } }
+    /// <summary>
+    /// This is an Explicit declaration.
+    /// Implementations may have their own Id value, and we want to seperate these.
+    /// Ex: Student.Id Should not return a Guid, but instead a value that fits the school guidelines perhaps
+    /// </summary>
+    Guid IEntity.Id { get => _guid!; init { _guid = Guid.NewGuid(); } }
 
     public string Name => ParseName();
     protected abstract string ParseName();
