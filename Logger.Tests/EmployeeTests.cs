97bdc229241c023d04b5e3f156443425b0d1d153
Austin Harrison <aharrison13@ewu.edu>
2024-10-22 21:26:13 -0700

    Made test for creation of Employee class. It will fail because there is no Employee class.

--- Logger.Tests/EmployeeTests.cs
+++ Logger.Tests/EmployeeTests.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Logger.Tests;
 
 public class EmployeeTests
 {
+    [Fact]
+    public void EmployeeCreation_NotNull_ReturnsNotNull()
+    {
+        //Act
+        FullName fullName = new("Inigo", "Ella", "Montoya");
+        Employee employee = new();
+
+        //Assert
+        Assert.NotNull(employee);
+    }
 }
