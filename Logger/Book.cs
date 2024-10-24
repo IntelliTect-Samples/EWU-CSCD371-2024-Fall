a6baa07083f8dcaf6c07acb2efe8dcadb08bea4b
Eric Leachman <eleachman04@ewu.edu>
2024-10-23 18:46:57 -0700

    added additional details to book

--- Logger/Book.cs
+++ Logger/Book.cs
@@ -4,10 +4,26 @@ namespace Logger;
 
 public record class Book : EntityBase
 {
-    public Book(string name)
-    {
-        Name = name;
-    }
+    public override string Name => $"{Title} by {Author.OutputDetails()}";
 
-    public override string Name { get; }
+    public string Title { get; init; }
+    public FullName Author { get; init; }
+    public string ISBN { get; init; }
+
+    public Book(string title, FullName author, string isbn)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            throw new ArgumentException("Title cannot be null or empty.", nameof(title));
+        }
+        Title = title;
+
+        Author = author;
+
+        if (string.IsNullOrWhiteSpace(isbn))
+        {
+            throw new ArgumentException("ISBN cannot be null or empty.", nameof(isbn));
+        }
+        ISBN = isbn;
+    }
 }
