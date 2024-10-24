1c2e698f5a1323427d9023a87ac4f0c24e8cfe87
Oscar Ruelas <oruelas@ewu.edu>
2024-10-24 14:27:05 -0700

    This should fix the build errors for Book.cs

--- Logger/Book.cs
+++ Logger/Book.cs
@@ -8,11 +8,13 @@ public record Book (string Title, FullName Author, string? Genre, string Publish
     public string Title { get; } = !string.IsNullOrWhiteSpace(Title) 
         ? Title 
         : throw new ArgumentException("Title cannot be null or empty.", nameof(Title));
-    public string Isbn { get; } = !string.IsNullOrWhiteSpace(Isbn) 
-        ? Isbn 
-        : throw new ArgumentException("ISBN cannot be null or empty.", nameof(Isbn).ToLower());
+
+    public string Isbn { get; } = !string.IsNullOrWhiteSpace(Isbn)
+        ? Isbn
+        : throw new ArgumentException("ISBN cannot be null or empty.", nameof(Isbn).ToLowerInvariant());
     
-    public string Publisher { get; } = Publisher ?? throw new ArgumentNullException(nameof(Publisher).ToLower());
+    public string Publisher { get; } = Publisher ?? throw new ArgumentNullException(nameof(Publisher).
+        ToLowerInvariant());
 
     public override string Name => string.IsNullOrWhiteSpace(Author.MiddleName) 
         ? $"{Author.FirstName} {Author.LastName}"
