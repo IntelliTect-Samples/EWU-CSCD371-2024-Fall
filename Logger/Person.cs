a007f912ef46f8558058419fd81f25100fc9aae7
Oscar Ruelas <oruelas@ewu.edu>
2024-10-24 14:41:57 -0700

    fixed Person.cs

--- Logger/Person.cs
+++ Logger/Person.cs
@@ -7,7 +7,7 @@ public record Person(FullName PersonsName, String Ssn, int Age, string DateOfBir
     public FullName PersonsName { get; } = PersonsName ?? throw new ArgumentNullException(nameof(PersonsName));
     public string Ssn { get; } = !string.IsNullOrWhiteSpace(Ssn)
         ? Ssn
-        : throw new ArgumentException("SSN cannot be null or empty.", nameof(Ssn).ToLower());
+        : throw new ArgumentException("SSN cannot be null or empty.", nameof(Ssn).ToLowerInvariant());
     public string DateOfBirth { get; } = !string.IsNullOrWhiteSpace(DateOfBirth)
         ? DateOfBirth
         : throw new ArgumentException("Date of Birth cannot be null or empty.", nameof(DateOfBirth));
