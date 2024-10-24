31771b1ad650796b21e440d57cdd86a1ee699b95
David <david_e_moore@hotmail.com>
2024-10-23 21:36:33 -0700

    Updated Entity Class to extend IEntity, requiring addition of Guid ID and Name.

--- Logger/Entity.cs
+++ Logger/Entity.cs
@@ -1,6 +1,12 @@
 ﻿namespace Logger
 {
-    public class Entity
+    public abstract record class Entity : IEntity
     {
+        // Applying the init setter here on the class.
+        public Guid Id { get; init; }
+
+        // Adding abstract to the class to ensure deriving classes implement the property.
+        public abstract string Name { get; set; }
+
     }
 }
