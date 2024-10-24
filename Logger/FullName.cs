7c5e9d5382a7cd949a8191e966cbe10348450c80
Eric Leachman <eleachman04@ewu.edu>
2024-10-23 18:46:45 -0700

    Updated to provide a unique function instead of overriding toString

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -16,7 +16,7 @@ public readonly record struct FullName
     public FullName(string firstName, string lastName, string middleName = "")
     {
         if (string.IsNullOrWhiteSpace(firstName))
-            throw new ArgumentException($"'{nameof(firstName)}' cannot be null or whitespace.", nameof(firstName));
+            throw new ArgumentException($"'{nameof(lastName)}' cannot be null or whitespace.", nameof(firstName));
 
         if (string.IsNullOrWhiteSpace(lastName))
             throw new ArgumentException($"'{nameof(lastName)}' cannot be null or whitespace.", nameof(lastName));
@@ -26,7 +26,7 @@ public readonly record struct FullName
         MiddleName = middleName ?? throw new ArgumentNullException(nameof(middleName));
     }
 
-    public override string ToString() => MiddleName switch
+    public string OutputDetails() => MiddleName switch
     {
         "" => $"{FirstName} {LastName}",
         _ => $"{FirstName} {MiddleName} {LastName}"
