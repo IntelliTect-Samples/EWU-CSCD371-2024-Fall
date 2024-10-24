02474eb32c85693a7618016bb3b3f8f2c24d4b99
David <david_e_moore@hotmail.com>
2024-10-23 19:57:55 -0700

    Utilized Test Driven Development. Created FullNameTests, then the FullName Class, now we have a failed test.

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Logger.Tests;
+
+public class FullNameTests
+{
+    [Fact]
+    public void PrintFullName_AllArguments_ReturnsFullName()
+    {
+
+        // 1. Arrange
+        FullName fullName = new("John",null,"Doe");
+
+        //2. Act
+        var result = fullName.ToString();
+
+        //3. Assess
+        Assert.Equal("John Doe", result);
+    }
+}
---
+++ Logger/FullName.cs
@@ -0,0 +1,27 @@
+﻿namespace Logger;
+
+public record FullName
+{
+    public FullName(string firstName, string? middleName, string lastName)
+    {
+        // FirstName cannot be null or whitespace. Every person has a first name.
+        FirstName = string.IsNullOrWhiteSpace(firstName)
+                ? throw new ArgumentException($"'{nameof(firstName)}' cannot be null or whitespace.", nameof(firstName))
+                : firstName;
+
+        // MiddleName can be null. Not every person has a middle name.
+
+        MiddleName = string.IsNullOrWhiteSpace(middleName)
+                ? null
+                : middleName;
+
+        // LastName cannot be null or whitespace. Every person has a last name.
+        LastName = string.IsNullOrWhiteSpace(lastName)
+                ? throw new ArgumentException($"'{nameof(lastName)}' cannot be null or whitespace.", nameof(lastName))
+                : lastName;
+    }
+
+    public string FirstName { get; }
+    public string? MiddleName { get; }
+    public string LastName { get; }
+}
