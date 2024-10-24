ea2c9c47b1954db07beb19ad52b97fc2c8092318
David <david_e_moore@hotmail.com>
2024-10-23 21:30:35 -0700

    Created EntityTests. Used Test Driven Development to create Entity. Checking if Interface Applied.

--- Logger.Tests/EntityTests.cs
+++ Logger.Tests/EntityTests.cs
@@ -1,7 +1,34 @@
-﻿using Xunit;
-
+﻿using System.Reflection;
+using Xunit;
 
 namespace Logger.Tests;
 public class EntityTests
 {
+
+    [Fact]
+    public void EntityClass_EntityClass_Exists()
+    {
+        // Arrange
+        // Get the assembly where Entity is supposed to be defined
+        Assembly assembly = Assembly.GetAssembly(typeof(Entity)); // Assuming EntityBase is in the same assembly
+
+        // Act
+        // Attempt to get the type of Entity
+        Type entityType = assembly.GetType("Logger.Entity"); // Adjust the namespace if necessary
+
+        // Assert
+        Assert.NotNull(entityType);
+    }
+
+    [Fact]
+    public void Entity_Should_Implement_IEntity_Interface_With_Instance()
+    {
+        // Arrange
+        var entity = new Entity { Id = Guid.NewGuid(), Name = "Test Entity" };
+
+        // Act & Assert
+        Assert.IsAssignableFrom<IEntity>(entity);
+    }
+
+
 }
