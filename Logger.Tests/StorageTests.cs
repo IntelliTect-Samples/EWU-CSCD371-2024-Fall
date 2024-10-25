3915a6ca6ee9d9f5514b0458d4bc69903b030581
ccornell88 <cjcornell38@gmail.com>
2024-10-24 17:07:09 -0700

    fixed

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -1,104 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
-namespace Logger.Tests
+namespace Logger.Tests;
+
+public class StorageTests
 {
-    public class StorageTests
+    [Fact]
+    public void Add_Employee_ShouldBeSuccessful()
     {
-        [Fact]
-        public void Add_Employee_ShouldBeSuccessful()
-        {
-            // Arrange
-            var storage = new Storage();
-            var employee = new Employee(new FullName("Chris", "J", "Cornell"), "CEO", "BOSS");
+        // Arrange
+        var storage = new Storage();
+        var employee = new Employee(new FullName("Chris", "J", "Cornell"), "CEO", "BOSS");
 
-            // Act
-            storage.Add(employee);
-            var containsEmployee = storage.Contains(employee);
+        // Act
+        storage.Add(employee);
+        var containsEmployee = storage.Contains(employee);
 
-            // Assert
-            Assert.True(containsEmployee);
-        }
+        // Assert
+        Assert.True(containsEmployee);
+    }
 
-        [Fact]
-        public void Add_Student_ShouldBeSuccessful()
-        {
-            // Arrange
-            var storage = new Storage();
-            var student = new Student(new FullName("Chris", "J", "Cornell"), "Senior", "Computer Science");
+    [Fact]
+    public void Add_Student_ShouldBeSuccessful()
+    {
+        // Arrange
+        var storage = new Storage();
+        var student = new Student(new FullName("Chris", "J", "Cornell"), "Senior", "Computer Science");
 
-            // Act
-            storage.Add(student);
-            var containsStudent = storage.Contains(student);
+        // Act
+        storage.Add(student);
+        var containsStudent = storage.Contains(student);
 
-            // Assert
-            Assert.True(containsStudent);
-        }
+        // Assert
+        Assert.True(containsStudent);
+    }
 
-        [Fact]
-        public void Add_Book_ShouldBeSuccessful()
-        {
-            // Arrange
-            var storage = new Storage();
-            var book = new Book("A Painted House");
+    [Fact]
+    public void Add_Book_ShouldBeSuccessful()
+    {
+        // Arrange
+        var storage = new Storage();
+        var book = new Book("A Painted House");
 
-            // Act
-            storage.Add(book);
-            var containsBook = storage.Contains(book);
+        // Act
+        storage.Add(book);
+        var containsBook = storage.Contains(book);
 
-            // Assert
-            Assert.True(containsBook);
-        }
+        // Assert
+        Assert.True(containsBook);
+    }
 
-        [Fact]
-        public void Remove_Student_ShouldBeSuccessful()
-        {
-            // Arrange
-            var storage = new Storage();
-            var student = new Student(new FullName("Chris", "J", "Cornell"), "Senior", "Computer Science");
+    [Fact]
+    public void Remove_Student_ShouldBeSuccessful()
+    {
+        // Arrange
+        var storage = new Storage();
+        var student = new Student(new FullName("Chris", "J", "Cornell"), "Senior", "Computer Science");
 
-            // Act
-            storage.Add(student);
-            storage.Remove(student);
-            var containsStudent = storage.Contains(student);
+        // Act
+        storage.Add(student);
+        storage.Remove(student);
+        var containsStudent = storage.Contains(student);
 
-            // Assert
-            Assert.False(containsStudent);
-        }
+        // Assert
+        Assert.False(containsStudent);
+    }
 
-        [Fact]
-        public void Remove_Book_ShouldBeSuccessful()
-        {
-            // Arrange
-            var storage = new Storage();
-            var book = new Book("To Kill a Mockingbird");
+    [Fact]
+    public void Remove_Book_ShouldBeSuccessful()
+    {
+        // Arrange
+        var storage = new Storage();
+        var book = new Book("To Kill a Mockingbird");
 
-            // Act
-            storage.Add(book);
-            storage.Remove(book);
-            var containsBook = storage.Contains(book);
+        // Act
+        storage.Add(book);
+        storage.Remove(book);
+        var containsBook = storage.Contains(book);
 
-            // Assert
-            Assert.False(containsBook);
-        }
+        // Assert
+        Assert.False(containsBook);
+    }
 
-        [Fact]
-        public void Get_StudentById_ShouldReturnCorrectStudent()
-        {
-            // Arrange
-            var storage = new Storage();
-            var student = new Student(new FullName("Chris", "J", "Cornell"), "Senior", "Computer Science");
+    [Fact]
+    public void Get_StudentById_ShouldReturnCorrectStudent()
+    {
+        // Arrange
+        var storage = new Storage();
+        var student = new Student(new FullName("Chris", "J", "Cornell"), "Senior", "Computer Science");
 
-            // Act
-            storage.Add(student);
-            var retrievedStudent = storage.Get(student.Id);
+        // Act
+        storage.Add(student);
+        var retrievedStudent = storage.Get(student.Id);
 
-            // Assert
-            Assert.Equal(student, retrievedStudent);  // Check that the correct student is retrieved by ID
-        }
+        // Assert
+        Assert.Equal(student, retrievedStudent);
     }
 }
