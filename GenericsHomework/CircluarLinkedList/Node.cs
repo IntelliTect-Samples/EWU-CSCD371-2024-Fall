e061e5e54ff9a88f86d6b7c254f4102f00923e06
Brady <bdullanty@ewu.edu>
2024-10-27 14:50:55 -0700

    Implemented to string method override on Node

--- GenericsHomework/CircluarLinkedList/Node.cs
+++ GenericsHomework/CircluarLinkedList/Node.cs
@@ -2,10 +2,17 @@
 
 public class Node<T>
 {
-    public T Value { get; set; }
+    public T? Value { get; set; }
 
-    public Node(T value)
+    public Node(T? value)
     {
+  
         Value = value;
     }
+
+    public override string? ToString()
+    {
+        return Value?.ToString();
+    }
+
 }
