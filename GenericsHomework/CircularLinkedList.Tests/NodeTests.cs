7b93bb548e32ea7760f9bb36a11629b38ae9c000
Austin Harrison <aharrison13@ewu.edu>
2024-10-27 14:15:12 -0700

    Created test for Node Creation. It will fail because no implementation.

--- GenericsHomework/CircularLinkedList.Tests/NodeTests.cs
+++ GenericsHomework/CircularLinkedList.Tests/NodeTests.cs
@@ -1,9 +1,17 @@
+using CircluarLinkedList;
+
 namespace CircularLinkedList.Tests;
 
 public class NodeTests
 {
     [Fact]
-    public void Test1_Test()
+    public void Constructor_ValidInput_CreatesNode()
     {
+        // Act
+        Node node = new();
+
+        //Assert
+        Assert.NotNull(node);
+        Assert.IsType<Node>(node);
     }
 }
