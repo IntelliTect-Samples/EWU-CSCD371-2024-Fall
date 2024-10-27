3a40e46af9fdef9dbde4657620e7eefa893b650b
Brady <bdullanty@ewu.edu>
2024-10-27 14:50:38 -0700

    Added test for Node ToString
    And added null as an Inline data to all current tests

--- GenericsHomework/CircularLinkedList.Tests/NodeTests.cs
+++ GenericsHomework/CircularLinkedList.Tests/NodeTests.cs
@@ -8,6 +8,7 @@ public class NodeTests
     [InlineData(1)]
     [InlineData(11231.2)]
     [InlineData("MyDataValue")]
+    [InlineData(null)]
     public void Constructor_ValidInput_CreatesValidNode<T>(T value)
     {
        
@@ -20,4 +21,23 @@ public class NodeTests
         Assert.IsType<Node<T>>(node);
         Assert.Equal(value, node.Value);
     }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData("SomeData")]
+    [InlineData(null)]
+    public void ToString_ValidCall_ReturnsString<T>(T? val)
+    {
+        //Arrange
+        Node<T> node = new(val);
+        //Act
+
+        //Assert
+        Assert.NotNull(node);
+        Assert.Equal(val?.ToString(), node.ToString());
+    }
+
+  
+    }
+
 }
