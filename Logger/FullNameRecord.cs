69f0d42b2b856b3947b9f7e0c40379cb3e2a76de
Eric Vo <tvo12@ewu.edu>
2024-10-27 20:49:03 -0700

    create FullNameRecord class (no comment yet)

---
+++ Logger/FullNameRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace Logger;
+
+public record struct FullNameRecord(string firstName, string lastName, string? middleName = null)
+{
+    public string firstName { get; } = firstName ?? throw new ArgumentNullException(nameof(firstName));
+    public string lastName { get; } = lastName ?? throw new ArgumentNullException(nameof(lastName));
+    public string? middleName { get; } = middleName;
+}
