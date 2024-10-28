cbab7b86ed6a5a27cb2ff1107834dcbdfb83f813
Eric Leachman <eleachman04@ewu.edu>
2024-10-27 17:08:05 -0700

    Added to Node class

--- GenericsHomework/LinkedList/Node.cs
+++ GenericsHomework/LinkedList/Node.cs
@@ -2,4 +2,12 @@
 
 public class Node<T>
 {
+    public T Value { get; set; }
+    public Node<T>? Next { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+        Next = null;
+    }
 }
