0b7695f6ee1ff5122d3d01283bf62f9af4ac8b0b
Windeko <tylerjameswoody@gmail.com>
2024-10-27 17:26:48 -0700

    Added tests checking that the test exists, and that the types are what's expected.

--- GenericsHomework/LinkedList.Tests/NodeTests.cs
+++ GenericsHomework/LinkedList.Tests/NodeTests.cs
@@ -2,13 +2,35 @@ namespace CircularLinkedList.Tests;
 
 public class NodeTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(13)]
+    [InlineData(13.0)]
+    [InlineData("Hello!")]
+    public void Constructor_VariousDataTypes_Exists<T>(T value)
     {
         // Arrange
+        Node<T> node = new(value);
 
         // Act
 
         // Assert
+        Assert.NotNull(node);
+    }
+
+    [Theory]
+    [InlineData(13)]
+    [InlineData(13.0)]
+    [InlineData("Hello!")]
+    public void Constructor_VariousDataTypes_ContainsExpectedDataTypes<T>(T value)
+    {
+        // Arrange
+        Node<T> node = new(value);
+
+        // Act
+        Type expectedType = typeof(T);
+        Type actualType = node.Value!.GetType();
+
+        // Assert
+        Assert.Equal(expectedType, node.Value.GetType());
     }
 }
