a7f247e777bb4e8a81f3cd337ae9486af65938ce
Eric Vo <tvo12@ewu.edu>
2024-10-27 21:42:42 -0700

    Rename FullNameRecord to FullName

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -1,8 +1,8 @@
 ﻿namespace Logger;
 
-//FullNameRecord is a vaule type of record struct for efficiency and immutability.
+//FullName is a vaule type of record struct for efficiency and immutability.
 //record struct is immutable by design and cannot be change.
-public readonly record struct FullNameRecord(string firstName, string lastName, string? middleName = null)
+public readonly record struct FullName(string firstName, string lastName, string? middleName = null)
 {
     public string firstName { get; } = firstName ?? throw new ArgumentNullException(nameof(firstName));
     public string lastName { get; } = lastName ?? throw new ArgumentNullException(nameof(lastName));
