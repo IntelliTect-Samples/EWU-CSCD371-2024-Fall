ad7e8d9abbc7248f08d51f962f4d001f9e84a01b
David <david_e_moore@hotmail.com>
2024-10-28 20:25:49 -0700

    Created Failed EmployeeTest.

--- Logger.Tests/EmployeeTests.cs
+++ Logger.Tests/EmployeeTests.cs
@@ -3,4 +3,25 @@
 namespace Logger.Tests
     public class EmployeeTests
     {
+
+    [Fact]
+    public void Employee_ValidData_CreatesRecord()
+    {
+        // Arrange
+        var name = "David Middle Moore";
+        var employer = "Coding Dojo";
+        var employeeRole = "Manager";
+        var employeeID = 319171;
+
+        //Act
+        Employee david = new(name, employeeRole, employeeID);
+
+        // Act & Assess
+        Assert.NotNull(david);
+        Assert.Equal(david.EmployeeID, employeeID);
+        Assert.Equal("David", david.EmployeeName.FirstName);
+        Assert.Equal("Middle", david.EmployeeName.MiddleName);
+        Assert.Equal("Moore", david.EmployeeName.LastName);
+        Assert.Equal(david.EmployeeRole, EmployeeRole);
     }
+}
