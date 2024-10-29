ef4ef25497f71f6bab4e7464ff1f100b0f849c6e
Jake Lucas <156277910+JacobLucas02@users.noreply.github.com>
2024-10-28 21:28:27 -0700

    Update Student.cs

--- Logger/Student.cs
+++ Logger/Student.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Logger;
 
-namespace Logger;
-
-public record Student (FullName FullName): Entity
+public record Student : Person
 {
-    public override string Name => FullName.ToString();
+    public string StudentId { get; init; }
+
+    public Student(FullName fullName, string studentId) : base(fullName)
+    {
+        StudentId = studentId;
+    }
+
+    // Explicitly implementing GetId to return the StudentId
+    // This provides a way to get the unique id for a Student
+    protected override string GetId() => StudentId;
 }
