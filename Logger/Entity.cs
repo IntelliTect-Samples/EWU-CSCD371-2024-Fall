2e070e95406bcfff538c58809cda8cecdf6f9f5f
Jake Lucas <156277910+JacobLucas02@users.noreply.github.com>
2024-10-28 21:30:56 -0700

    Update Entity.cs

--- Logger/Entity.cs
+++ Logger/Entity.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Logger;
 
 public abstract record class Entity : IEntity
