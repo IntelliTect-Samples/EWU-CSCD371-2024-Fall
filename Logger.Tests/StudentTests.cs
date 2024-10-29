aa0464254ab705b5fb44899983c826249a2e9622
David <david_e_moore@hotmail.com>
2024-10-28 20:20:07 -0700

    Created No Middle Student Test.

--- Logger.Tests/StudentTests.cs
+++ Logger.Tests/StudentTests.cs
@@ -21,6 +21,25 @@ namespace Logger.Tests;
         Assert.Equal("Middle", david.StudentName.MiddleName);
         Assert.Equal("Doe", david.StudentName.LastName);
         Assert.Equal(david.SchoolName, schoolName);
+    }
 
+    [Fact]
+    public void Student_ValidDatNoMiddle_CreatesRecordNoMiddleName()
+    {
+        // Arrange
+        var name = "John Doe";
+        var schoolName = "Rogers High School";
+        var studentID = 319171;
+
+        //Act
+        Student david = new(name, schoolName, studentID);
+
+        // Act & Assess
+        Assert.NotNull(david);
+        Assert.Equal(david.StudentID, studentID);
+        Assert.Equal("John", david.StudentName.FirstName);
+        Assert.Null(david.StudentName.MiddleName);
+        Assert.Equal("Doe", david.StudentName.LastName);
+        Assert.Equal(david.SchoolName, schoolName);
     }
 }
