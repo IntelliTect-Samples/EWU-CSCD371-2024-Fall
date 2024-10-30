379170da4a8b7494e692b5dab1b94d38030df255
Windeko <tylerjameswoody@gmail.com>
2024-10-29 21:28:37 -0700

    Implemented initial test for Add

--- GenericsHomework/LinkedList.Tests/NodeTests.cs
+++ GenericsHomework/LinkedList.Tests/NodeTests.cs
@@ -210,4 +210,18 @@ public class NodeTests
         // Assert
         Assert.Equal(expectedMessage, actualMessage);
     }
+
+    [Fact]
+    public void Add_GivenValues_AppendsItems()
+    {
+        // Arrange
+        Node<int> node = new(12);
+        int expectedValue = 42;
+
+        // Act
+        node.Add(42);
+
+        // Assert
+        Assert.Equal(node.Next.Value, expectedValue);
+    }
 }
