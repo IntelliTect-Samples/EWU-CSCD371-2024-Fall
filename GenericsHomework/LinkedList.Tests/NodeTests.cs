09ac55fb02f2aa40705718270b54f4d3348e784d
Eric Leachman <eleachman04@ewu.edu>
2024-10-29 20:31:37 -0700

    Deleted unnecessary constructor test

--- GenericsHomework/LinkedList.Tests/NodeTests.cs
+++ GenericsHomework/LinkedList.Tests/NodeTests.cs
@@ -6,21 +6,6 @@ public class NodeTests
     [InlineData(13)]
     [InlineData(13.0)]
     [InlineData("Hello!")]
-    public void Constructor_VariousDataTypes_Exists<T>(T value)
-    {
-        // Arrange
-        Node<T> node = new(value);
-
-        // Act
-
-        // Assert
-        Assert.NotNull(node);
-    }
-
-    [Theory]
-    [InlineData(13)]
-    [InlineData(13.0)]
-    [InlineData("Hello!")]
     public void Constructor_VariousDataTypes_ContainsExpectedDataTypes<T>(T value)
     {
         // Arrange
