030e0854202b90d7184a6f88188336304dc0ef5a
David <david_e_moore@hotmail.com>
2024-10-30 18:08:14 -0700

    Create Test and Class Library.

---
+++ GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,11 @@
+namespace GenericsHomework.Tests
+{
+    public class NodeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ GenericsHomework/Node.cs
@@ -0,0 +1,5 @@
+﻿namespace GenericsHomework;
+    public class Node
+    {
+
+    }
