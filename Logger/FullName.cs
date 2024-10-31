1a96a25b95eee3633e50605278217211f3d60f15
dpalmer10 <dpalmer10@ewu.edu>
2024-10-30 20:41:57 -0700

    updated comment

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -2,6 +2,8 @@
 
 //FullName is a value type of record struct because we want it to be immutable.
 //record struct is immutable by design and cannot be change.
+// While a person can change their name, it is not very common, and this ensures that data related to an individual
+// is being associated with the correct individual.
 public readonly record struct FullName(string firstName, string lastName, string? middleName = null)
 {
     public string firstName { get; } = firstName ?? throw new ArgumentNullException(nameof(firstName));
