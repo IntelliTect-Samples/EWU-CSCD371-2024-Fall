8d46d8c3bda4e02b5f0a83d0cc28473657b85eff
ccornell88 <cjcornell38@gmail.com>
2024-10-31 13:55:07 -0700

    Update Employee
    added null/whitespace exceptions

--- Logger/Employee.cs
+++ Logger/Employee.cs
@@ -2,8 +2,13 @@
 
 public record class Employee(FullName FullName, string Position, string Department) : Person(FullName)
 {
-    public string Position { get; init; } = Position; 
-    public string Department { get; init; } = Department; 
+    public string Position { get; init; } = !string.IsNullOrWhiteSpace(Position)
+        ? Position
+        : throw new ArgumentException("Position cannot be null or whitespace.", nameof(Position));
+
+    public string Department { get; init; } = !string.IsNullOrWhiteSpace(Department)
+        ? Department
+        : throw new ArgumentException("Department cannot be null or whitespace.", nameof(Department));
 
     // The Name property is implemented in the Person class, no need to override it here.
     // The Id property is inherited from BaseEntity, no need to define it.
