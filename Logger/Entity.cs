696741e7cdcb3a106d77aa2b496038e9029e1100
Jake Lucas <156277910+JacobLucas02@users.noreply.github.com>
2024-10-30 20:10:39 -0700

    Fixed Guid implementation

--- Logger/Entity.cs
+++ Logger/Entity.cs
@@ -3,7 +3,7 @@ namespace Logger;
 public abstract record class Entity : IEntity
 {
     // Explicit implementation
-    Guid IEntity.Id { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+    public Guid Id { get; init; } = Guid.NewGuid();
 
     // Implicit implementation
     public abstract string Name { get; }
