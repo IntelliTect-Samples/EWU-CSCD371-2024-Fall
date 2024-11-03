a472bb887243c1dcc920710ad094c68336564d46
Eric Leachman <eleachman04@ewu.edu>
2024-11-03 10:35:35 -0800

    Stubbed out calculator class

--- Calculate.Core/Calculator.cs
+++ Calculate.Core/Calculator.cs
@@ -2,4 +2,27 @@
 
 public class Calculator
 {
+    public static int TryCalculate(Func<int, int, int> operation)
+    {
+        return 0;
+    }
+    public static int Add(int a, int b)
+    {
+        return 0;
+    }
+
+    public static int Subtract(int a, int b)
+    {
+        return 0;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return 0;
+    }
+
+    public static int Divide(int a, int b)
+    {
+        return 0;
+    }
 }
