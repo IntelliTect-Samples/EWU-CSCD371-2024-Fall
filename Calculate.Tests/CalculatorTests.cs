af6cc3a85e1f11902e2f8e48885b19e74648d6cc
Windeko <tylerjameswoody@gmail.com>
2024-11-03 12:23:54 -0800

    Added test for MathematicalOperations property. Added test for TryCalculate method.

--- Calculate.Tests/CalculatorTests.cs
+++ Calculate.Tests/CalculatorTests.cs
@@ -1,10 +1,4 @@
 ﻿using Calculate.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace Calculate.Tests;
 
@@ -77,4 +71,36 @@ public class CalculatorTests
         Assert.Throws<DivideByZeroException>(() => Calculator.Divide(a, b));
     }
 
+    [Fact]
+    public void MathematicalOperations_OnInstantiation_HasCorrectMappings()
+    {
+        // Arrange
+        Calculator calculator = new();
+
+        // Act
+
+        // Assert
+        Assert.Equal(Calculator.Add, calculator.MathematicalOperations['+']);
+        Assert.Equal(Calculator.Subtract, calculator.MathematicalOperations['-']);
+        Assert.Equal(Calculator.Multiply, calculator.MathematicalOperations['*']);
+        Assert.Equal(Calculator.Divide, calculator.MathematicalOperations['/']);
+    }
+
+    [Theory]
+    [InlineData("42 + 42", 84)]
+    [InlineData("10 - 5", 5)]
+    [InlineData("6 * 7", 42)]
+    [InlineData("10 / 2", 5)]
+    public void TryCalculate_WhenCalled_ReturnsExpectedResult(string input, double expected)
+    {
+        // Arrange
+        Calculator calculator = new();
+
+        // Act
+        bool success = calculator.TryCalculate(input, out double actual);
+
+        // Assert
+        Assert.Equal(expected, actual);
+        Assert.True(success);
+    }
 }
