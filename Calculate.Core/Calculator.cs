504b7b9204ea63a8eacfeda9829ff62a474312a4
Eric Leachman <eleachman04@ewu.edu>
2024-11-03 11:07:58 -0800

    Updated fields and properties

--- Calculate.Core/Calculator.cs
+++ Calculate.Core/Calculator.cs
@@ -2,13 +2,15 @@
 
 public class Calculator
 {
-    public IReadOnlyDictionary<char, Func<int, int, double>> Operations = new Dictionary<char, Func<int, int, double>>
+    private readonly IReadOnlyDictionary<char, Func<int, int, double>> _mathmaticalOperations = new Dictionary<char, Func<int, int, double>>
     {
         ['+'] = Add,
         ['-'] = Subtract,
         ['*'] = Multiply,
         ['/'] = Divide
     };
+
+    public IReadOnlyDictionary<char, Func<int, int, double>> MathmaticalOperations => _mathmaticalOperations;
 
     public static double TryCalculate(int a, int b, Func<int, int, double> operation)
     {
