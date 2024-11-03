2ab584dd0a4c510565235be47913595ba0ce08c4
Eric Leachman <eleachman04@ewu.edu>
2024-11-03 10:55:13 -0800

    removed white space

--- Calculate.Core/Calculator.cs
+++ Calculate.Core/Calculator.cs
@@ -1,28 +1,29 @@
 ﻿namespace Calculate.Core;
 
-public class Calculator
+public static class Calculator
 {
+
     public static int TryCalculate(int a, int b, Func<int, int, double> operation)
     {
         return 0;
     }
     public static double Add(int a, int b)
     {
-        return 0;
+        return a + b;
     }
 
     public static double Subtract(int a, int b)
     {
-        return 0;
+        return a - b;
     }
 
     public static double Multiply(int a, int b)
     {
-        return 0;
+        return a * b;
     }
 
     public static double Divide(int a, int b)
     {
-        return 0;
+        return a / b;
     }
 }
