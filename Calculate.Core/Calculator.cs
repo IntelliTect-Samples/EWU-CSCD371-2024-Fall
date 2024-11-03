e087fb942b33bb78758e13c8393c71e9accd400a
Eric Leachman <eleachman04@ewu.edu>
2024-11-03 10:42:19 -0800

    Altered return types

--- Calculate.Core/Calculator.cs
+++ Calculate.Core/Calculator.cs
@@ -2,26 +2,26 @@
 
 public class Calculator
 {
-    public static int TryCalculate(int a, int b, Func<int, int, int> operation)
+    public static int TryCalculate(int a, int b, Func<int, int, double> operation)
     {
         return 0;
     }
-    public static int Add(int a, int b)
-    {
-        return 0;
-    }
-
-    public static int Subtract(int a, int b)
+    public static double Add(int a, int b)
     {
         return 0;
     }
 
-    public static int Multiply(int a, int b)
+    public static double Subtract(int a, int b)
     {
         return 0;
     }
 
-    public static int Divide(int a, int b)
+    public static double Multiply(int a, int b)
+    {
+        return 0;
+    }
+
+    public static double Divide(int a, int b)
     {
         return 0;
     }
