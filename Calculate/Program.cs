d47fe827c51e302b725019d16a1392c44beaa1c1
Windeko <tylerjameswoody@gmail.com>
2024-11-02 20:55:13 -0700

    Added fields and properties for ReadLine and WriteLine

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,7 +1,22 @@
 ﻿namespace Calculate;
 
-public static class Program
+public class Program
 {
+    private Action<string> _writeLine = Console.WriteLine;
+    private Func<string> _readLine = Console.ReadLine!;
+
+    public Action<string> WriteLine
+    {
+        get => _writeLine;
+        init => _writeLine = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public Func<string> ReadLine
+    {
+        get => _readLine;
+        init => _readLine = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
     public static void Main()
     {
     }
