6b63eb8e4e282d0b4b934e3bbfeefb851839aa47
Austin Harrison <aharrison13@ewu.edu>
2024-11-04 12:40:02 -0800

    Implemented ReadLine property

--- Calculate/Calculate/Program.cs
+++ Calculate/Calculate/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public Func<string> ReadLine { get; init; }
+
+    public Program()
+    {
+        ReadLine = Console.ReadLine;
+    }
+
+    public static void Main()
+    {
+    }
+}
