6671d2c5a7da8e214922d28433b301d7876a8af9
Austin Harrison <aharrison13@ewu.edu>
2024-11-04 13:32:14 -0800

    Created test for add

--- Calculate/CalculateTests/CalculatorTests.cs
+++ Calculate/CalculateTests/CalculatorTests.cs
@@ -1,5 +1,20 @@
 ﻿namespace CalculateTests;
 
+[TestClass]
 public class CalculatorTests
 {
+    [TestMethod]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        // Arrange
+        Calculator calculator = new();
+        int number1 = 5;
+        int number2 = 10;
+
+        // Act
+        int sum = calculator.Add(number1, number2);
+
+        // Assert
+        Assert.AreEqual(15, sum);
+    }
 }
