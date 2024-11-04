bcd0d6269b163d290ef7a77f0e4c8f6287eca271
David <david_e_moore@hotmail.com>
2024-11-03 19:45:50 -0800

    Remove duplicate and update comments.

--- Logger.Tests/BookTests.cs
+++ Logger.Tests/BookTests.cs
@@ -16,7 +16,7 @@ public class BookTests
         //Act
         Book newBook = new(title, authorName, yearPublished, isbn);
 
-        // Act & Assess
+        // Assert
         Assert.NotNull(newBook);
         Assert.Equal(newBook.ISBN, isbn);
         Assert.Equal("John", newBook.Author.FirstName);
@@ -39,7 +39,7 @@ public class BookTests
         //Act
         Book newBook = new(title, authorName, yearPublished, isbn);
 
-        // Act & Assess
+        // Assert
         Assert.NotNull(newBook);
         Assert.Equal(newBook.ISBN, isbn);
         Assert.Equal("John", newBook.Author.FirstName);
@@ -47,23 +47,6 @@ public class BookTests
         Assert.Equal("Doe", newBook.Author.LastName);
         Assert.Equal(newBook.YearPublished, yearPublished);
         Assert.Equal(newBook.Title, title);
-
-    }
-
-    [Fact]
-    public void ConstructorTest_ValidData_CreatesRecord()
-    {
-        // Arrange & Act
-        Book newBook = new("Harry Potter", "J.K. Rowling", 2000, 33-0987654);
-
-        // Act & Assess
-        Assert.NotNull(newBook);
-        Assert.Equal(expected: 33 - 0987654, actual: newBook.ISBN);
-        Assert.Equal("J.K.", newBook.Author.FirstName);
-        Assert.Null(newBook.Author.MiddleName);
-        Assert.Equal("Rowling", newBook.Author.LastName);
-        Assert.Equal(2000, newBook.YearPublished);
-        Assert.Equal("Harry Potter", newBook.Title);
 
     }
 
