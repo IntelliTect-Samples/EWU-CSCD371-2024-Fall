54f4e0dded2b37e4753bb27baf374e3e014e8b1d
Austin Harrison <aharrison13@ewu.edu>
2024-11-04 13:57:46 -0800

    Made test for divide by 0

--- Calculate/CalculateTests/CalculatorTests.cs
+++ Calculate/CalculateTests/CalculatorTests.cs
@@ -46,4 +46,29 @@ public class CalculatorTests
         // Assert
         Assert.AreEqual(50, result);
     }
+
+    [TestMethod]
+    public void Divide_TwoNumbers_ReturnsQuotient()
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 5;
+
+        // Act
+        Calculator.Divide(num1, num2, out int result);
+
+        // Assert
+        Assert.AreEqual(2, result);
+    }
+
+    [TestMethod]
+    public void Divide_ByZero_ThrowsException()
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 0;
+
+        // Act and Assert
+        Assert.ThrowsException<DivideByZeroException>(() => Calculator.Divide(num1, num2, out int result));
+    }
 }
