0f82ecc230c4da1fa54064e0ef3e373bd53081be
Austin Harrison <aharrison13@ewu.edu>
2024-11-04 13:58:01 -0800

    Implemented logic for divide by 0 exception

--- Calculate/Calculate/Calculator.cs
+++ Calculate/Calculate/Calculator.cs
@@ -5,4 +5,11 @@ public class Calculator
     public static int Add(int num1, int num2, out int result) => result = num1 + num2;
 
     public static int Subtract(int num1, int num2, out int result) => result = num1 - num2;
+
+    public static int Multiply(int num1, int num2, out int result) => result = num1 * num2;
+
+    public static int Divide(int num1, int num2, out int result)
+    {
+        return num2 == 0 ? throw new DivideByZeroException() : (result = num1 / num2);
+    }
 }
