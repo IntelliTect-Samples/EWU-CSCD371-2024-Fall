1cd14c1e59acc801b504ed8c5d8c0d578999aad4
Austin Harrison <aharrison13@ewu.edu>
2024-11-04 12:39:47 -0800

    Created Test for ReadLine property

--- Calculate/CalculateTests/ProgramTests.cs
+++ Calculate/CalculateTests/ProgramTests.cs
@@ -1,11 +1,40 @@
-namespace CalculateTests
+namespace CalculateTests;
+
+[TestClass]
+public class ProgramTests
 {
-    [TestClass]
-    public class ProgramTests
+    [TestMethod]
+    public void ReadLine_NoInput_ReturnsValidConstructor()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        // Arrange
+        Program program = new();
+        string simulatedInput = "test";
+
+        // Act
+        var stringReader = new StringReader(simulatedInput);
+
+        Console.SetIn(stringReader);
+        string testInput = program.ReadLine();
+
+        // Assert
+        Assert.IsNotNull(program);
+        Assert.AreEqual("test", testInput);
+    }
+
+    [TestMethod]
+    public void WriteLine_NoInput_ReturnsValidConstructor()
+    {
+        // Arrange
+        Program program = new();
+        StringWriter stringWriter = new();
+        Console.SetOut(stringWriter);
+        string expectedOutput = "test";
+
+        // Act
+        program.WriteLine("test");
+        string output = stringWriter.ToString();
+
+        // Assert
+        Assert.AreEqual(expectedOutput, output);
     }
 }
