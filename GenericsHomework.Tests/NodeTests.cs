afd9e9a157ddb61c985bcc95bca844d3bb1b8f83
ccornell88 <cjcornell38@gmail.com>
2024-11-03 22:48:16 -0800

    Added Clear Test

--- GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework.Tests/NodeTests.cs
@@ -20,5 +20,22 @@ public class NodeTests
         Assert.Same(node, node.Next.Next); // should point back to node.
     }
 
-}
+    [Fact]
+    public void Clear_RemovesNodes_ExceptSelf()
+    {
+        // Arrange
+        Node<int> node = new(10);
+        node.Append(20);
+        node.Append(30);
 
+        // Act
+        node.Clear();
+
+        // Assert
+        Assert.Same(node, node.Next);
+        Assert.Equal(10, node.Value);
+        Assert.False(node.Exists(20));
+        Assert.False(node.Exists(30));
+    }
+
+}
