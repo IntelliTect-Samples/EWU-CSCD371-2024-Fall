640e24d18c42cb8f822cc0f66450b29d4d6262ee
Brady <bdullanty@ewu.edu>
2024-11-04 19:37:05 -0800

    Added Run and EVC for Program
    Used evc in Run test

--- Calculate/Calculate/Program.cs
+++ Calculate/Calculate/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+using Calculate;
+
+public class Program
 {
     public Func<string?> ReadLine { get; init; }
     public Action<string> WriteLine { get; init; }
@@ -8,8 +10,43 @@
         ReadLine = Console.ReadLine;
         WriteLine = Console.WriteLine;
     }
+    public Program(Func<string?> readline,Action<string> writeLine)
+    {
+
+
+        ReadLine = readline;
+        WriteLine = writeLine;
+    }
+    public void Run()
+    {
+        string input;
+        double output;
+        bool status;
+        Calculator calculator = new();
+
+        WriteLine("Please type a simple mathematical equation or type exit");
+        while (true)
+        {
+            input =ReadLine() ?? "";
+
+            if (input.Equals("exit", StringComparison.OrdinalIgnoreCase) || input.Equals("")) break;
+            status = calculator.TryCalculate(input,out output);
+            if (status)
+            {
+                WriteLine(output.ToString());
+            }
+            else
+            {
+                WriteLine("Invalid string. Please follow format of '# operator #'");
+            }
+        }
+    }
 
     public static void Main()
     {
+        Program p = new(Console.ReadLine,Console.WriteLine);
+        p.Run();
     }
+
+ 
 }
