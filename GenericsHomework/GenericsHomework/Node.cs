f853a84ab8a19ce8e6fc6e76b047687e08914b7a
ccornell88 <cjcornell38@gmail.com>
2024-11-04 18:33:53 -0800

    Update Node.cs

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,5 +1,20 @@
 ﻿namespace GenericsHomework;
 
-internal class Node
+public class Node<T>
 {
+    public T Value { get; }
+    public Node<T> Next { get; private set; }
+
+    // initialize the node with a value
+    // set the Next reference to itself which makes it circular
+    public Node(T value)
+    {
+        Value = value;
+        Next = this;
+    }
+
+    public override string ToString()
+    {
+        return Value?.ToString() ?? string.Empty;
+    }
 }
