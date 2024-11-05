afa4b9e432ddc7493362878b6fbbb8b80e926b7e
Brady <bdullanty@ewu.edu>
2024-11-04 19:36:29 -0800

    Added Run test

--- Calculate/CalculateTests/ProgramTests.cs
+++ Calculate/CalculateTests/ProgramTests.cs
@@ -37,4 +37,22 @@ public class ProgramTests
         // Assert
         Assert.AreEqual(expectedOutput, output);
     }
+
+    [TestMethod]
+    public void Run_ValidInputs_ReturnsExpectedResults()
+    {
+        //Arrange
+        StringWriter writer = new StringWriter();
+        Console.SetOut(writer);
+        StringReader reader = new($"12 + 2{Environment.NewLine}2 - 2{Environment.NewLine}2.0 * 16.6{Environment.NewLine}12-2{Environment.NewLine}hi there{Environment.NewLine} 2/2{Environment.NewLine}exit");
+        String expectedResult =$"Please type a simple mathematical equation or type exit{Environment.NewLine}14{Environment.NewLine}0{Environment.NewLine}33.2{Environment.NewLine}Invalid string. Please follow format of '# operator #'{Environment.NewLine}Invalid string. Please follow format of '# operator #'{Environment.NewLine}Invalid string. Please follow format of '# operator #'{Environment.NewLine}".Trim();
+        Console.SetIn(reader);
+        Program program = new(Console.ReadLine,Console.WriteLine);
+        //Act
+        program.Run();
+                
+
+        //Assert
+        Assert.AreEqual(expectedResult, writer.ToString().Trim());
+    }
 }
