0481f5286e3051b0e59c9ac393858e51f1397b86
Brady <bdullanty@ewu.edu>
2024-11-04 18:57:23 -0800

    Added U incase first T was int

--- Calculate/Calculate/Calculator.cs
+++ Calculate/Calculate/Calculator.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Calculate;
 
-public delegate void MathOperation(int num1, int num2, out double result);
+public delegate void MathOperation(double num1, double num2, out double result);
 
 public class Calculator
 {
@@ -20,12 +20,12 @@ public class Calculator
             };
     }
 
-    public static void Add<T>(T num1, T num2, out double result) where T  : IConvertible=> result = Convert.ToDouble(num1) + Convert.ToDouble(num2);
+    public static void Add<T, U>(T num1, U num2, out double result) where T : IConvertible where U : IConvertible => result = Convert.ToDouble(num1) + Convert.ToDouble(num2);
 
-    public static void Subtract<T>(T num1, T num2, out double result) where T : IConvertible => result = Convert.ToDouble(num1) - Convert.ToDouble(num2);
+    public static void Subtract<T, U>(T num1, U num2, out double result) where T : IConvertible where U : IConvertible => result = Convert.ToDouble(num1) - Convert.ToDouble(num2);
 
-    public static void Multiply<T>(T num1, T num2, out double result) where T : IConvertible => result = Convert.ToDouble(num1) * Convert.ToDouble(num2);
+    public static void Multiply<T, U>(T num1, U num2, out double result) where T : IConvertible where U : IConvertible => result = Math.Round(Convert.ToDouble(num1) * Convert.ToDouble(num2),12);
 
-    public static void Divide<T>(T num1, T num2, out double result) where T : IConvertible => result = Convert.ToDouble(num2) == 0 ? throw new DivideByZeroException() : Convert.ToDouble(num1) / Convert.ToDouble(num2);
+    public static void Divide<T, U>(T num1, U num2, out double result) where T : IConvertible where U : IConvertible => result = Math.Round(Convert.ToDouble(num2) == 0 ? throw new DivideByZeroException() : Convert.ToDouble(num1) / Convert.ToDouble(num2),12);
 
 }
