0c957196c8b7a5eaf21232a6933072e408ee1d96
Brady <bdullanty@ewu.edu>
2024-11-04 18:59:46 -0800

    Added TryCalculate method

--- Calculate/Calculate/Calculator.cs
+++ Calculate/Calculate/Calculator.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Calculate;
@@ -28,4 +28,28 @@ public class Calculator
 
     public static void Divide<T, U>(T num1, U num2, out double result) where T : IConvertible where U : IConvertible => result = Math.Round(Convert.ToDouble(num2) == 0 ? throw new DivideByZeroException() : Convert.ToDouble(num1) / Convert.ToDouble(num2),12);
 
+    public bool TryCalculate(string input, out double result)
+    {
+        result = double.NaN;
+
+        string[] split = input.Split(' ');
+        if (split.Length != 3) throw new ArgumentException("Invalid passed in string: " + input);
+
+        try
+        {
+            double left = Convert.ToDouble(split[0]);
+            double right = Convert.ToDouble(split[2]);
+            char givenOperator = Convert.ToChar(split[1]);
+            MathematicalOperations[givenOperator](left,right,out result);
+
+        }
+        catch (Exception)
+        {
+            return false;
+            
+        }
+
+        return true;
+
+    }
 }
