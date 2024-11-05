288c15a1507e854701458a5541295d89795ccd2d
Austin Harrison <aharrison13@ewu.edu>
2024-11-04 16:29:07 -0800

    Implemented dictionary, had to creat a custom delegate.

--- Calculate/Calculate/Calculator.cs
+++ Calculate/Calculate/Calculator.cs
@@ -1,7 +1,25 @@
-﻿namespace Calculate;
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculate;
+
+public delegate void MathOperation(int num1, int num2, out double result);
 
 public class Calculator
 {
+    public IReadOnlyDictionary<char, MathOperation> MathematicalOperations { get; }
+
+    public Calculator()
+    {
+        MathematicalOperations = new Dictionary<char, MathOperation>
+            {
+                { '+', Add },
+                { '-', Subtract },
+                { '*', Multiply },
+                { '/', Divide }
+            };
+    }
+
     public static void Add(int num1, int num2, out double result) => result = num1 + num2;
 
     public static void Subtract(int num1, int num2, out double result) => result = num1 - num2;
