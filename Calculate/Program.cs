9db8a46adb8f655bb66dae74a735db816f612711
Oscar Ruelas <oruelas@ewu.edu>
2024-11-06 13:40:00 -0800

    Implemented Program.cs and added test

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace Calculate;
 
-class Program
+public class Program
 {
+    public Action<string> Writeline { get; init; } = Console.WriteLine;
+    public Func<string> Readline { get; init; } = Console.ReadLine!;
+
+    
+    // Empty Constructor
+    public Program() { }
+    
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new();
     }
 }
