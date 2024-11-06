7b0fb36c4ef443707c055a6c7a33f63712ff21fe
Austin Harrison <aharrison13@ewu.edu>
2024-11-05 19:03:10 -0800

    Changed TryCalculate to other implementaion

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -21,37 +21,34 @@ public class Calculator
             };
     }
 
-    public static void Add<T, T2>(T num1, T2 num2, out double result) where T : IConvertible where T2 : IConvertible => result = Convert.ToDouble(num1,new CultureInfo("en-US")) + Convert.ToDouble(num2,new CultureInfo("en-US"));
+    public static void Add<T, T2>(T num1, T2 num2, out double result) where T : IConvertible where T2 : IConvertible => result = Convert.ToDouble(num1, new CultureInfo("en-US")) + Convert.ToDouble(num2, new CultureInfo("en-US"));
 
-    public static void Subtract<T, T2>(T num1, T2 num2, out double result) where T : IConvertible where T2 : IConvertible => result = Convert.ToDouble(num1,new CultureInfo("en-US")) - Convert.ToDouble(num2,new CultureInfo("en-US"));
+    public static void Subtract<T, T2>(T num1, T2 num2, out double result) where T : IConvertible where T2 : IConvertible => result = Convert.ToDouble(num1, new CultureInfo("en-US")) - Convert.ToDouble(num2, new CultureInfo("en-US"));
 
-    public static void Multiply<T, T2>(T num1, T2 num2, out double result) where T : IConvertible where T2 : IConvertible => result = Math.Round(Convert.ToDouble(num1,new CultureInfo("en-US")) * Convert.ToDouble(num2,new CultureInfo("en-US")),12);
+    public static void Multiply<T, T2>(T num1, T2 num2, out double result) where T : IConvertible where T2 : IConvertible => result = Math.Round(Convert.ToDouble(num1, new CultureInfo("en-US")) * Convert.ToDouble(num2, new CultureInfo("en-US")), 12);
 
-    public static void Divide<T, T2>(T num1, T2 num2, out double result) where T : IConvertible where T2 : IConvertible => result = Math.Round(Convert.ToDouble(num2,new CultureInfo("en-US")) == 0 ? throw new DivideByZeroException() : Convert.ToDouble(num1,new CultureInfo("en-US")) / Convert.ToDouble(num2,new CultureInfo("en-US")),12);
+    public static void Divide<T, T2>(T num1, T2 num2, out double result) where T : IConvertible where T2 : IConvertible => result = Math.Round(Convert.ToDouble(num2, new CultureInfo("en-US")) == 0 ? throw new DivideByZeroException() : Convert.ToDouble(num1, new CultureInfo("en-US")) / Convert.ToDouble(num2, new CultureInfo("en-US")), 12);
 
     public bool TryCalculate(string input, out double result)
     {
         result = double.NaN;
 
         string[] split = input.Split(' ');
-        
+        if (split.Length != 3) return false;
 
-        try
-        {
-            if (split.Length != 3) throw new ArgumentException("Invalid passed in string: " + input);
-            double left = Convert.ToDouble(split[0], new CultureInfo("en-US"));
-            double right = Convert.ToDouble(split[2], new CultureInfo("en-US"));
-            char givenOperator = Convert.ToChar(split[1], new CultureInfo("en-US"));
-            MathematicalOperations[givenOperator](left,right,out result);
-
-        }
-        catch (Exception)
+        if (!double.TryParse(split[0], out double left) || !double.TryParse(split[2], out double right))
         {
             return false;
-            
         }
 
-        return true;
+        if (split[1].Length != 1 || !MathematicalOperations.ContainsKey(split[1][0]))
+        {
+            return false;
+        }
 
+        char givenOperator = split[1][0];
+        MathematicalOperations[givenOperator](left, right, out result);
+
+        return true;
     }
 }
