582da6cb57d79236735a0499e6fb86ab3f9838c5
Windeko <tylerjameswoody@gmail.com>
2024-11-05 20:23:42 -0800

    made a console utilities class library, moved the properies and fields to that.

---
+++ ConsoleUtilities/ProgramBase.cs
@@ -0,0 +1,19 @@
+namespace ConsoleUtilities;
+
+public abstract class ProgramBase
+{
+    private Action<string> _writeLine = Console.WriteLine;
+    private Func<string> _readLine = Console.ReadLine!;
+
+    public Action<string> WriteLine
+    {
+        get => _writeLine;
+        init => _writeLine = value ?? throw new ArgumentNullException(nameof(value), $"{nameof(value)} cannot be null.");
+    }
+
+    public Func<string> ReadLine
+    {
+        get => _readLine;
+        init => _readLine = value ?? throw new ArgumentNullException(nameof(value), $"{nameof(value)} cannot be null.");
+    }
+}
