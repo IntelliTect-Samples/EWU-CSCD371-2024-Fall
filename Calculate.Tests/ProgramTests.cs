9be1ac146d50b6bb4e3ccba0fd0efe51cfe65996
Windeko <tylerjameswoody@gmail.com>
2024-11-06 17:29:18 -0800

    Removed a "Check that C# is working properly" test

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -6,28 +6,6 @@ public class ProgramTests
 {
     [Theory]
     [InlineData("Test input")]
-    public void Constructor_LambdaCapture_InvokesProperties(string input)
-    {
-        // Arrange
-        string capturedOutput = "";
-
-        Program program = new()
-        {
-            WriteLine = (message) => capturedOutput = message,
-            ReadLine = () => input
-        };
-
-        // Act
-        program.WriteLine("Hello World!");
-        string result = program.ReadLine();
-
-        // Assert
-        Assert.Equal("Hello World!", capturedOutput);
-        Assert.Equal("Test input", result);
-    }
-
-    [Theory]
-    [InlineData("Test input")]
     public void Constructor_ReaderAndWriter_InvokesProperties(string input)
     {
         // Arrange
