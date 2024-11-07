c71e604b3a4c6240f6e934895549891ee656294a
David <david_e_moore@hotmail.com>
2024-11-06 19:29:59 -0800

    Implement Run & Test.

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -13,5 +13,31 @@ public class Program
     public static void Main(string[] args)
     {
         Program program = new();
+        program.Run();
+    }
+
+    public void Run()
+    {
+        Writeline("Enter a mathematical expression: ");
+        string input = Readline();
+        Calculator calculator = new();
+        if (calculator.TryCalculate(input, out double result))
+        {
+            Writeline($"Result: {result}");
+        }
+        else
+        {
+            Writeline("Invalid Equation. Here's an Example: 1 + 1");
+            Writeline("Would you like to try again?");
+            string input2 = Readline().ToLower();
+            if (input2 == "yes")
+            {
+                Run();
+            }
+            else
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
     }
 }
