b80455e181c2ba74aebeabfbc21265b4e8656460
David <david_e_moore@hotmail.com>
2024-11-06 18:59:47 -0800

    Updated Return Value to double

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -1,21 +1,27 @@
 
 
 
+
 namespace Calculate;
 
 public class Calculator
 {
-    public static void Add(int num1, int num2, out int result)
+    public static void Add(int num1, int num2, out double result)
     {
         result = num1 + num2;
     }
 
-    public static void Multiply(int num1, int num2, out int result)
+    public static void Divide(int num1, int num2, out double result)
+    {
+        result = num1/num2;
+    }
+
+    public static void Multiply(int num1, int num2, out double result)
     {
         result = num1 * num2;
     }
 
-    public static void Subtract(int num1, int num2, out int result)
+    public static void Subtract(int num1, int num2, out double result)
     {
         result = num1 - num2;
     }
