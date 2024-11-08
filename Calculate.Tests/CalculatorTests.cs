109a6c5db8d202da135711a81c4d77ced55fa2ad
Oscar Ruelas <oruelas@ewu.edu>
2024-11-07 19:47:52 -0800

    Fixed test

--- Calculate.Tests/CalculatorTests.cs
+++ Calculate.Tests/CalculatorTests.cs
@@ -6,7 +6,8 @@ public class CalculatorTests
     public void Add_TwoArguments_ReturnResult()
     {
         // Arrange & Act
-        Calculator<int>.Add(1, 2, out double result);
+        Calculator<int> calc = new();
+        calc.Add(1, 2, out double result);
 
         // Assert
         Assert.Equal(3, result);
@@ -16,7 +17,8 @@ public class CalculatorTests
     public void Subtract_TwoArguments_ReturnResult()
     {
         // Arrange & Act
-        Calculator<int>.Subtract(3, 2, out double result);
+        Calculator<int> calc = new();
+        calc.Subtract(3, 2, out double result);
 
         // Assert
         Assert.Equal(1, result);
@@ -26,7 +28,8 @@ public class CalculatorTests
     public void Multiply_TwoArguments_ReturnResult()
     {
         // Arrange & Act
-        Calculator<int>.Multiply(3, 2, out double result);
+        Calculator<int> calc = new();
+        calc.Multiply(3, 2, out double result);
 
         // Assert
         Assert.Equal(6, result);
@@ -36,7 +39,8 @@ public class CalculatorTests
     public void Divide_TwoArguments_ReturnResult()
     {
         // Arrange & Act
-        Calculator<int>.Divide(10, 5, out double result);
+        Calculator<int> calc = new();
+        calc.Divide(10, 5, out double result);
 
         // Assert
         Assert.Equal(2, result);
