92151eec64791f05112e5cb8cade5992d724b9e0
Austin Harrison <aharrison13@ewu.edu>
2024-11-10 13:18:43 -0800

    Created test for CsvRows property. It will fail becasause no definition.

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -1,5 +1,24 @@
-﻿namespace Assignment.Tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace Assignment.Tests;
+
+[TestClass]
 public class SampleDataTests
 {
+    [TestMethod]
+    public void CsvRows_LoadDataFromCsv_SkipFirstRow()
+    {
+        // Arrange
+        SampleData sampleData = new();
+
+        // Act
+        List<string> csvRows = sampleData.CsvRows.ToList();
+
+        // Assert
+        Assert.IsNotNull(csvRows);
+        Assert.IsTrue(csvRows.Count > 0);
+        Assert.AreEqual("Priscilla,Jenyns,pjenyns0@7884 Corry,Helena,MT,70577", csvRows[0]);
+    }
 }
