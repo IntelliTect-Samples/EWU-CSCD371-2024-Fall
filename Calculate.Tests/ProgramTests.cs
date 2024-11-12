f2d33ca871a01fdfaca43f5689ff4390c26df819
ccornell88 <cjcornell38@gmail.com>
2024-11-11 23:27:34 -0800

    Update Calculate.Tests
    2 tests to make sure WriteLine and ReadLine invokes correctly.

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -1,5 +1,41 @@
-﻿namespace Calculate.Tests;
-
-public class ProgramTests
+﻿namespace Calculate.Tests
 {
+    public class ProgramTests
+    {
+        private string Output = "";
+        private const string Input = "Testing";
+
+        [Fact]
+        public void Program_WriteLine_InvokesCorrectly()
+        {
+            // Arrange
+            var program = new Program(
+                writeLine: text => Output = text,
+                readLine: () => Input
+            );
+
+            // Act
+            program.WriteLine("Howdy");
+
+            // Assert
+            Assert.Equal("Howdy", Output);
+        }
+
+        [Fact]
+        public void Program_ReadLine_InvokesCorrectly()
+        {
+            // Arrange
+            var program = new Program(
+                writeLine: text => Output = text,
+                readLine: () => Input
+            );
+
+            // Act
+            var readResult = program.ReadLine();
+
+            // Assert
+            Assert.Equal(Input, readResult);
+        }
+    }
 }
+
