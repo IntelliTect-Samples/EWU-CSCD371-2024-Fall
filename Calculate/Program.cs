1a31a383b9d2f3395ce17ed7d8176f4aea048d77
Eric Vo <tvo12@ewu.edu>
2024-11-11 22:44:30 -0800

    Program class

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -2,5 +2,23 @@
 
 public class Program
 {
+    public Action<string> WriteLine { get; set; } = Console.WriteLine;
+    public Func<string?> ReadLine { get; set; } = Console.ReadLine;
+
+    public static void Main(string[] args)
+    {
+        Program program = new();
+        Calculator cal = new();
+        string input;
+        int? ans;
+
+        do
+        {
+            program.WriteLine("Enter the expression to calculate:");
+            input = program.ReadLine() ?? string.Empty;
+        } while (!cal.TryCalculate(input, out ans));
+        program.WriteLine($"Answer: {ans}");
+    }
+
 
 }
