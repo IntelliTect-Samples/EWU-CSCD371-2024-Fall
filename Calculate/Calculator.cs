54f9c834c17a0d843f9d5241f7536815b70f0d11
Ben Knoll <bknoll@ewu.edu>
2024-11-12 22:44:22 -0800

    Calculator class start
    * Have a functioning calculator class that needs some work
    * Slightly modified program to meet assignment requirements

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -7,54 +7,53 @@ namespace Calculate;
 
 public class Calculator
 {
-    //private IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations;
+    private IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations;
 
 
-    //public Calculator()
-    //{
-    //    MathematicalOperations = new Dictionary<char, Action<int,int,out int>>
-    //    {
-    //        { '-', Subtract },
-    //        { '+', Add },
-    //        { '/', Divide },
-    //        { '*', Multiply },
-    //    };
-    //}
+    public Calculator()
+    {
+        MathematicalOperations = new Dictionary<char, Func<int, int, int>>
+        {
+            { '-', Subtract },
+            { '+', Add },
+            { '/', Divide },
+            { '*', Multiply },
+        };
+    }
 
-    //public static int Add(int first, int second, out int res)
-    //{
-    //    return res = first + second;
-    //}
+    public static int Add(int first, int second)
+    {
+        return first + second;
+    }
 
-    //public static int Subtract(int first, int second, out int res)
-    //{
-    //    return res = first - second;
-    //}
-    
-    //public static int Multiply(int first, int second, out int res)
-    //{
-    //    return res = first * second;
-    //}
+    public static int Subtract(int first, int second)
+    {
+        return first - second;
+    }
 
-    //public static int Divide(int first, int second, out int res)
-    //{
-    //    return res = first / second;
-    //}
+    public static int Multiply(int first, int second)
+    {
+        return first * second;
+    }
 
-    //public bool TryCalculate(string s)
-    //{
-    //    string[] sArray = s.Split(" ");
-    //    int first;
-    //    int second;
-    //    char op;
-    //    if (sArray.Length == 3 && Int32.TryParse(sArray[0], out first) && Int32.TryParse(sArray[2], out second))
-    //    {
-    //        Char.TryParse(sArray[1], out op);
-    //        Func<int,int,int> target = MathematicalOperations[op];
-    //        target(first, second);
-    //        return true;
-    //    }
-    //    return false;
-    //}
+    public static int Divide(int first, int second)
+    {
+        return first / second;
+    }
+
+    public bool TryCalculate(string s, out int result)
+    {
+        string[] sArray = s.Trim().Split(" ");
+
+        if (sArray.Length == 3 && Int32.TryParse(sArray[0], out int first) && Int32.TryParse(sArray[2], out int second) && Char.TryParse(sArray[1], out char mathOperator) && MathematicalOperations.ContainsKey(mathOperator))
+        {
+            Func<int, int, int> target = MathematicalOperations[mathOperator];
+
+            result = target(first, second);
+            return true;
+        }
+        result = -1;
+        return false;
+    }
 
 }
