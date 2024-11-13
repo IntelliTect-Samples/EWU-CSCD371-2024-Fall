82fca14bde03760a69426e3fc5b496576e9d627f
Ben Knoll <bknoll@ewu.edu>
2024-11-12 23:40:45 -0800

    Added tests for Calculator

--- Calculate.Tests/CalculatorTests.cs
+++ Calculate.Tests/CalculatorTests.cs
@@ -50,4 +50,52 @@ public class CalculatorTests
         Assert.False(calculator.TryCalculate("d + 2", out int resString));
         Assert.False(calculator.TryCalculate("2.2 + 2", out int resDouble));
     }
+
+    [Fact]
+    public void Add_GivenValidInts_ReturnsCorrectAnswer()
+    {
+        // Arrange
+        Calculator calculator = new();
+
+        // Act
+
+        // Assert
+        Assert.Equal(3, Calculator.Add(1, 2));
+    }
+
+    [Fact]
+    public void Subtract_GivenValidInts_ReturnsCorrectAnswer()
+    {
+        // Arrange
+        Calculator calculator = new();
+
+        // Act
+
+        // Assert
+        Assert.Equal(-1, Calculator.Subtract(1, 2));
+    }
+
+    [Fact]
+    public void Multiply_GivenValidInts_ReturnsCorrectAnswer()
+    {
+        // Arrange
+        Calculator calculator = new();
+
+        // Act
+
+        // Assert
+        Assert.Equal(2, Calculator.Multiply(1, 2));
+    }
+
+    [Fact]
+    public void Divide_GivenValidInts_ReturnsCorrectAnswer()
+    {
+        // Arrange
+        Calculator calculator = new();
+
+        // Act
+
+        // Assert
+        Assert.Equal(1, Calculator.Divide(2, 2));
+    }
 }
