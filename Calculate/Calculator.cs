eb8f76ff71e2e07dc71287398f838646607f434e
Kmiller <kmiller71@ewu.edu>
2024-11-12 21:35:59 -0800

    Renaming

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -4,47 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculate
+namespace Calculate;
+
+public class Calculator
 {
-    public class Calculator
+
+    public static double Add(int i, int j) => i + j;
+    public static double Subtract(int i, int j) => i - j;
+    public static double Multiply(int i, int j) => i * j;
+    public static double Divide(int i, int j)
     {
-
-        public static double Add(int i, int j) => i + j;
-        public static double Subtract(int i, int j) => i - j;
-        public static double Multiply(int i, int j) => i * j;
-        public static double Divide(int i, int j)
+        if (j == 0)
         {
-            if (j == 0)
-            {
-                throw new DivideByZeroException();
-            }
-            return (double)i / (double)j;
+            throw new DivideByZeroException();
         }
+        return (double)i / (double)j;
+    }
 
-        public IReadOnlyDictionary<char, Func<int, int, double>> ArithmeticOperators { get;} = new Dictionary<char, Func<int, int, double>>
-        {
-            ['+'] = Add,
-            ['-'] = Subtract,
-            ['*'] = Multiply,
-            ['/'] = Divide
-        };
+    public IReadOnlyDictionary<char, Func<int, int, double>> ArithmeticOperators { get;} = new Dictionary<char, Func<int, int, double>>
+    {
+        ['+'] = Add,
+        ['-'] = Subtract,
+        ['*'] = Multiply,
+        ['/'] = Divide
+    };
 
-        public bool TryCalculate(string equation, out double result)
-        {
-            result = 0.0;
-            string[] parts = equation.Split(' ');
+    public bool TryCalculate(string equation, out double result)
+    {
+        result = 0.0;
+        string[] parts = equation.Split(' ');
 
-            if (parts.Length != 3) return false;
+        if (parts.Length != 3) return false;
 
-            if (!int.TryParse(parts[0], out var i)) return false;
-            if (!int.TryParse(parts[2], out var j)) return false;
+        if (!int.TryParse(parts[0], out var i)) return false;
+        if (!int.TryParse(parts[2], out var j)) return false;
 
-            if (parts[1].Length != 1) return false;
-           
-            if (!ArithmeticOperators.TryGetValue(parts[1][0], out var operation)) return false;
+        if (parts[1].Length != 1) return false;
+       
+        if (!ArithmeticOperators.TryGetValue(parts[1][0], out var operation)) return false;
 
-            result = operation(i, j);
-            return true;
-        }
+        result = operation(i, j);
+        return true;
     }
 }
--- Calculator.tests/ProgramTests.cs
+++ Calculator.tests/ProgramTests.cs
@@ -5,32 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Calculate.tests
+namespace Calculate.tests;
+
+public class ProgramTests
 {
-    public class ProgramTests
+
+    [Fact]
+    public void Main_ValidEquation_PrintResult() 
     {
+        
+        // Arrange
+        var input = "8 + 2";
+        var expected = "Enter an arithmetic Operation to calculate (Ex, 4 + 3) : \r\nSolution : 10\r\n";
 
-        [Fact]
-        public void Main_ValidEquation_PrintResult() 
-        {
-            
-            // Arrange
-            var input = "8 + 2";
-            var expected = "Enter an arithmetic Operation to calculate (Ex, 4 + 3) : \r\nSolution : 10\r\n";
+        var consoleReader = new StringReader(input);                
+        var consoleWriter = new StringWriter();
+        
+        Console.SetIn(consoleReader);
+        Console.SetOut(consoleWriter);
 
-            var consoleReader = new StringReader(input);                
-            var consoleWriter = new StringWriter();
-            
-            Console.SetIn(consoleReader);
-            Console.SetOut(consoleWriter);
+        // Act
+        Program.Main();
 
-            // Act
-            Program.Main();
-
-            // Assert
-            var actual = consoleWriter.ToString();
-            Assert.Equal(expected, actual);
-            
-        }
+        // Assert
+        var actual = consoleWriter.ToString();
+        Assert.Equal(expected, actual);
+        
     }
 }
