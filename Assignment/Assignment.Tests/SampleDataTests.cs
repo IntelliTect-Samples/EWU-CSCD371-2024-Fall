db1d23529069f3535abdb974ff1bf289d89eef39
EL <eleachman04@ewu.edu>
2024-11-14 13:28:08 -0800

    Created SampleDataTests

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment.Tests;
+
+
+[TestClass]
+public class SampleDataTests
+{
+    [TestMethod]
+    public void CsvRows_GivenData_ContainsAllRows()
+    {
+
+    }
+}
