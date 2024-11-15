a1fa33f1d47c9b6ba14b3852524162b2cf214944
Ben Knoll <bknoll@ewu.edu>
2024-11-14 20:52:20 -0800

    Tests for other cases

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -53,5 +53,39 @@ public class ProgramTests
         // Assert
         Assert.Equal($"Welcome to ultimate calculator.{Environment.NewLine}Ensure format: 'int validOperator int'{Environment.NewLine}Type EXIT to close the program.{Environment.NewLine}It's not possible to divide by zero, please try again{Environment.NewLine}", writer.ToString());
     }
+
+    [Fact]
+    public void TryCalculate_GivenValid_OutputsResult()
+    {
+        // Arrange
+        StringWriter writer = new();
+        Console.SetOut(writer);
+
+        StringReader reader = new($"2 / 2{Environment.NewLine}EXIT{Environment.NewLine}");
+        Console.SetIn(reader);
+        // Act
+        Program program = new(Console.WriteLine, Console.ReadLine);
+        Program.Main();
+
+        // Assert
+        Assert.Equal($"Welcome to ultimate calculator.{Environment.NewLine}Ensure format: 'int validOperator int'{Environment.NewLine}Type EXIT to close the program.{Environment.NewLine}Result: 1{Environment.NewLine}", writer.ToString());
+    }
+
+    [Fact]
+    public void TryCalculate_GivenStringNoSpaces_OutputsInvalidInputMessage()
+    {
+        // Arrange
+        StringWriter writer = new();
+        Console.SetOut(writer);
+
+        StringReader reader = new($"2/2{Environment.NewLine}EXIT{Environment.NewLine}");
+        Console.SetIn(reader);
+        // Act
+        Program program = new(Console.WriteLine, Console.ReadLine);
+        Program.Main();
+
+        // Assert
+        Assert.Equal($"Welcome to ultimate calculator.{Environment.NewLine}Ensure format: 'int validOperator int'{Environment.NewLine}Type EXIT to close the program.{Environment.NewLine}Invalid input string. Ensure format: 'int validOperator int'{Environment.NewLine}", writer.ToString());
+    }
 }
 
--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -56,7 +56,7 @@ public class Program
 
             if (tryCalculateSuccess)
             {
-                program.WriteLine("" + tryCalculateResult);
+                program.WriteLine($"Result: {tryCalculateResult.ToString()}");
             }
             else
             {
