ca77964b599510cb708a26ca0150ea2585e0e6a0
Brady Dullanty <bdullanty@ewu.edu>
2024-11-15 17:45:02 -0800

    Added in IEnumerator and DVC tests for CSVRows

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -1,20 +1,63 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.Metrics;
 
 namespace Assignment.Tests;
+
+
+//We understand that logic in tests is typically frowned upon, but the assignment recommends we don't use collection assertions,
+//Thus we must iterate :)
 
 
 [TestClass]
 public class SampleDataTests
 {
     [TestMethod]
-    public void CsvRows_GivenData_ContainsAllRows()
+    public void CsvRows_IEnumeratorEVC_ContainsAllRows()
     {
         // Arrange
-        string[] data = ["Inigo", "Montoya"];
-
-
+        SampleData sampleData = new(File.ReadAllLines("People.csv"));
+        List<string> expectedvalues = File.ReadAllLines("People.csv").Skip(1).ToList();
         // Act
+        IEnumerable<string> resultEnumerable = sampleData.CsvRows;
 
         //  Assert
+        Assert.IsNotNull(sampleData);
+        Assert.IsNotNull(resultEnumerable);
+        Assert.IsTrue(sampleData.CsvRows.Any());
+        Assert.AreEqual(expectedvalues.Count, resultEnumerable.Count());
+        Assert.AreEqual('1', resultEnumerable.First().First());
+        int counter = 0;
+
+        foreach (string value in expectedvalues)
+        {
+            Assert.AreEqual(value, expectedvalues[counter]);
+            counter++;
+        }
+    }
+
+
+
+    [TestMethod]
+    public void CsvRows_DVC_ContainsAllRows()
+    {
+        //Arrange
+        SampleData sampleData = new();
+        List<string> expectedValues = File.ReadAllLines("People.csv").Skip(1).ToList();
+        //Act
+
+        //Assert
+        Assert.IsNotNull(sampleData);
+        Assert.IsNotNull(expectedValues);
+        Assert.AreEqual(expectedValues.Count, sampleData.CsvRows.Count());
+        Assert.IsTrue(sampleData.CsvRows.Any());
+
+        Assert.AreEqual('1', sampleData.CsvRows.First().First());
+
+        int counter = 0;
+        foreach (string value in sampleData.CsvRows)
+        {
+            Assert.AreEqual(value, expectedValues[counter]);
+            counter++;
+        }
     }
 }
