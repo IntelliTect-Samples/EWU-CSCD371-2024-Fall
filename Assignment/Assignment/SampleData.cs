92d3719921250ad731e05b3503190e33778dd801
Eric Leachman <eleachman04@ewu.edu>
2024-11-16 19:01:07 -0800

    Built out people property to specs

--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -30,7 +30,16 @@ public class SampleData : ISampleData
     }
 
     // 4.
-    public IEnumerable<IPerson> People => throw new NotImplementedException();
+    public IEnumerable<IPerson> People =>
+        CsvRows.Select(row =>
+         {
+             string[] values = row.Split(',');
+             return new Person(values[1], values[2],
+                 new Address(values[4], values[5], values[6], values[7]), values[3]);
+         }).
+        OrderBy(person => person.Address.State).
+        ThenBy(person => person.Address.City).
+        ThenBy(person => person.Address.Zip);
 
     // 2.
     public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
