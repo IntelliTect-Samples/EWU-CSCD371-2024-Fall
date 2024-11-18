bcf47de03fbc0b10ace95ad4197898abd0a0feb6
Brady Dullanty <bdullanty@ewu.edu>
2024-11-18 13:04:20 -0800

    Moved all files up one directory

---
+++ Assignment.Interfaces/IAddress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment;
+
+public interface IAddress
+{
+    string StreetAddress { get; }
+    string City { get; }
+    string State { get; }
+    string Zip { get; }
+}
--- Assignment/Assignment/Address.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assignment;
-
-public class Address(string streetAddress, string city, string state, string zip) : IAddress
-{
-    public string StreetAddress { get; set; } = streetAddress;
-    public string City { get; set; } = city;
-    public string State { get; set; } = state;
-    public string Zip { get; set; } = zip;
-}
