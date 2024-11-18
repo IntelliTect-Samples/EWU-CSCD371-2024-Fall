f91c33d1cc6390ad78e5f7a5455062dd4b1edf82
EL <eleachman04@ewu.edu>
2024-11-18 11:49:40 -0800

    Added node class

---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+
+namespace Assignment;
+
+public class Node<T>
+{
+    public T Value { get; set; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
+    {
+        Value = value;
+        Next = this;
+    }
+
+    public override string ToString()
+    {
+        return Value?.ToString() ?? $"{nameof(Value)} does not exist.";
+    }
+
+    public void Append(T value)
+    {
+        if (Exists(value))
+        {
+            throw new InvalidOperationException("Value already exists in this list.");
+        }
+
+        Node<T> node = new(value);
+        Node<T> current = this;
+
+        while (current.Next != this)
+        {
+            current = current.Next;
+        }
+
+        current.Next = node;
+        node.Next = this;
+    }
+
+    public void Clear()
+    {
+        /*
+         * We have decided to iterate through the list, clearing each node.
+         * This does not necessarily have to be done, as the garbage collector
+         * should be able to handle this. However, this is a good practice to
+         * ensure that the garbage collector can do its job efficiently.
+        */
+
+        Node<T> current = Next;
+
+        while (current != this)
+        {
+            Node<T> temp = current;
+            current = current.Next;
+            temp.Next = temp;
+        }
+
+        Next = this;
+    }
+
+    public bool Exists(T value)
+    {
+        Node<T> current = this;
+
+        do
+        {
+            if (current.Value != null && current.Value.Equals(value))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        while (current != this);
+
+        return false;
+    }
+}
