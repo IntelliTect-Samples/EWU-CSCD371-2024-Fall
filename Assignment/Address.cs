e9bd78746da4b5350f183fa0adfca66a42dfec54
Brady Dullanty <bdullanty@ewu.edu>
2024-11-18 13:06:58 -0800

    Added Assignment folder back

---
+++ Assignment/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment;
+
+public class Address(string streetAddress, string city, string state, string zip) : IAddress
+{
+    public string StreetAddress { get; set; } = streetAddress;
+    public string City { get; set; } = city;
+    public string State { get; set; } = state;
+    public string Zip { get; set; } = zip;
+}
---
+++ Assignment/Person.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Assignment;
+
+public class Person(string firstName, string lastName, IAddress address, string emailAddress) : IPerson
+{
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public IAddress Address { get; set; } = address;
+    public string EmailAddress { get; set; } = emailAddress;
+}
