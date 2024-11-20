c34068cb0838818bdc73e395418dd17f6d257125
Austin Harrison <aharrison13@ewu.edu>
2024-11-19 20:46:19 -0800

    Added old Node classes

---
+++ Assignment/Assignment.Tests/NodeTests.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Assignment;
+
+namespace Assignment.Tests;
+
+[TestClass]
+public class NodeTests
+{
+    [DataTestMethod]
+    [DataRow(1)]
+    [DataRow(11231.2)]
+    [DataRow("MyDataValue")]
+    [DataRow(null)]
+    public void Constructor_ValidInput_CreatesValidNode<T>(T value)
+    {
+        // Act
+        Node<T> node = new(value);
+
+        // Assert
+        Assert.IsNotNull(node);
+        Assert.AreEqual(typeof(Node<T>), node.GetType());
+        Assert.AreEqual(value, node.Value);
+    }
+
+    [DataTestMethod]
+    [DataRow(1)]
+    [DataRow("SomeData")]
+    [DataRow(null)]
+    public void ToString_ValidCall_ReturnsString<T>(T val)
+    {
+        // Arrange
+        Node<T> node = new(val);
+
+        // Act & Assert
+        Assert.IsNotNull(node);
+        Assert.AreEqual(val?.ToString(), node.ToString());
+    }
+
+    [DataTestMethod]
+    [DataRow(1)]
+    [DataRow("SomeData")]
+    [DataRow(null)]
+    public void Next_NoSet_ReturnsBaseNode<T>(T val)
+    {
+        // Arrange
+        Node<T> node = new(val);
+
+        // Act & Assert
+        Assert.IsNotNull(node);
+        Assert.IsNotNull(node.Next);
+        Assert.AreEqual(node, node.Next);
+        Assert.AreSame(node, node.Next);
+    }
+
+    [DataTestMethod]
+    [DataRow(1, 2, 3)]
+    [DataRow("SomeData", "21", "thirdValue")]
+    [DataRow(45.6, null, 43.5)]
+    public void Append_ValidChange_ReturnsExpected<T>(T val, T val2, T val3)
+    {
+        // Arrange
+        Node<T> node = new(val);
+        node.Append(val3);
+
+        // Act
+        node.Append(val2);
+
+        // Assert
+        Assert.IsNotNull(node);
+        Assert.IsNotNull(node.Next);
+        Assert.IsNotNull(node.Next.Next);
+        Assert.AreEqual(val, node.Value);
+        Assert.AreEqual(val2, node.Next.Value);
+        Assert.AreEqual(val3, node.Next.Next.Value);
+        Assert.AreSame(node, node.Next.Next.Next);
+    }
+
+    [DataTestMethod]
+    [DataRow(1, 2, 3, 3, true)]
+    [DataRow(1, 2, 3, 13, false)]
+    [DataRow("SomeData", "21", "thirdValue", "21", true)]
+    [DataRow("SomeData", "21", "thirdValue", "22", false)]
+    [DataRow("SomeData", "21", "thirdValue", null, false)]
+    [DataRow(null, 32.5, 42.1, null, true)]
+    public void Exists_ValidInput_ReturnsTrue<T>(T val, T val2, T val3, T expectedValue, bool expectedResult)
+    {
+        // Arrange
+        Node<T> node = new(val);
+        node.Append(val2);
+        node.Append(val3);
+
+        // Act
+        bool exists = node.Exists(expectedValue);
+
+        // Assert
+        Assert.AreEqual(expectedResult, exists);
+    }
+
+    [DataTestMethod]
+    [DataRow(1, 3, 1)]
+    [DataRow("SomeData", "21", "SomeData")]
+    [DataRow(null, "data", null)]
+    [DataRow("data", null, null)]
+    [DataRow(1.0, 2.8, 2.8)]
+    public void Append_DuplicateValues_ThrowsException<T>(T val, T val2, T val3)
+    {
+        // Arrange
+        Node<T> node = new(val);
+        node.Append(val2);
+
+        // Act & Assert
+        Assert.ThrowsException<ArgumentException>(() => node.Append(val3));
+    }
+
+    [DataTestMethod]
+    [DataRow(1, 3, 2)]
+    [DataRow("SomeData", "21", "SomeData2")]
+    [DataRow(null, "data2", "data")]
+    [DataRow("data", null, "t")]
+    [DataRow(1.0, 2.8, 2.9)]
+    public void Clear_ValidLoop_RemovesNodes<T>(T val, T val2, T val3)
+    {
+        // Arrange
+        Node<T> node = new(val);
+        node.Append(val2);
+        node.Append(val3);
+        Node<T> node2 = node.Next;
+        Node<T> node3 = node2.Next;
+
+        // Act
+        node.Clear();
+
+        // Assert
+        Assert.AreEqual(node, node.Next);
+        Assert.AreSame(node, node.Next);
+        Assert.AreSame(node2.Next, node2);
+        Assert.AreSame(node3.Next, node3);
+        Assert.AreEqual(node2, node2.Next);
+        Assert.AreEqual(node3, node3.Next);
+    }
+}
---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,69 @@
+﻿namespace Assignment;
+
+public class Node<T>
+{
+    public T? Value { get; set; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T? value)
+    {
+        Value = value;
+        Next = this;
+    }
+
+    public void Append(T? val)
+    {
+        if (Exists(val))
+        {
+            throw new ArgumentException("Value already exists in the list");
+        }
+
+        Node<T> newNode = new(val);
+        Node<T> lastNode = Next;
+        newNode.Next = lastNode;
+        Next = newNode;
+    }
+
+    public override string? ToString()
+    {
+        return Value?.ToString();
+    }
+
+    public bool Exists(T? expectedValue)
+    {
+        Node<T> current = this;
+
+        do
+        {
+            if ((current.Value is null && expectedValue is null) || current.Value?.Equals(expectedValue) == true)
+            {
+                return true;
+            }
+            current = current.Next;
+        } while (current != this);
+
+        return false;
+    }
+
+    // By ensuring that each node points only to itself, we effectively break the circular references.
+    // This allows the garbage collector to reclaim the memory for all nodes except the current one.
+    // If we just set the first node's Next to itself, this would work as long as no other nodes are stored in a variable.
+    // If one of the Nodes had been stored in a variable, some of the list would still exist.
+    public void Clear()
+    {
+        if (this.Next == null)
+        {
+            return;
+        }
+
+        Node<T> traversal = this;
+        Node<T> old = this;
+
+        do
+        {
+            traversal = traversal.Next;
+            old.Next = old;
+            old = traversal;
+        } while (traversal != this);
+    }
+}
