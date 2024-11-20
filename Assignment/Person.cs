c60ce98883bb8fae04724a68c14298e08aeddef4
Windeko <tylerjameswoody@gmail.com>
2024-11-19 22:55:14 -0800

    Moved everything up one folder

---
+++ Assignment/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment;
+
+public class Person : IPerson
+{
+    public Person(string firstName, string lastName, IAddress address, string emailAddress)
+    {
+        FirstName = string.IsNullOrWhiteSpace(firstName) ? throw new ArgumentException($"{nameof(firstName)} cannot be null or whitespace.") : firstName;
+        LastName = string.IsNullOrWhiteSpace(lastName) ? throw new ArgumentException($"{nameof(lastName)} cannot be null or whitespace.") : lastName;
+        Address = address ?? throw new ArgumentNullException(nameof(address), $"{nameof(address)} cannot be null.");
+        EmailAddress = string.IsNullOrWhiteSpace(emailAddress) ? throw new ArgumentException($"{nameof(emailAddress)} cannot be null or whitespace.") : emailAddress;
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public IAddress Address { get; set; }
+    public string EmailAddress { get; set; }
+}
