34adc9895f7d506a259f660e5c3bc4bf6eab5adf
Brady Dullanty <bdullanty@ewu.edu>
2024-11-23 15:42:01 -0800

    Added Async test for loading in CSV rows

---
+++ Assignment.Tests/SampleDataAsyncTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment.Tests;
+[TestClass]
+public class SampleDataAsyncTests
+{
+    [TestMethod]
+    public async Task CsvRows_IAsyncEnumeratorDVC_ContainsAllRows()
+    {
+        // Arrange
+        SampleDataAsync sampleData = new();
+        List<string> expectedvalues = File.ReadAllLines("People.csv").Skip(1).ToList();
+        // Act
+        IAsyncEnumerable<string> resultEnumerable = sampleData.CsvRows;
+
+        //  Assert
+        Assert.IsNotNull(sampleData);
+        Assert.IsNotNull(resultEnumerable);
+        Assert.IsTrue(await resultEnumerable.AnyAsync());
+        Assert.AreEqual(expectedvalues.Count, await resultEnumerable.CountAsync());
+        Assert.AreEqual('1', (await resultEnumerable.FirstAsync()).First());
+        int counter = 0;
+
+        await foreach (string value in resultEnumerable)
+        {
+            Assert.AreEqual(value, expectedvalues[counter]);
+            counter++;
+        }
+    }
+}
