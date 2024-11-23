abf580d716f894c143222047ce2937d7b935885f
Brady Dullanty <bdullanty@ewu.edu>
2024-11-23 15:44:12 -0800

    Implemented CSV rows for async class

--- Assignment/SampleDataAsync.cs
+++ Assignment/SampleDataAsync.cs
@@ -1,11 +1,25 @@
-﻿
-
+﻿using System.Linq;
 namespace Assignment;
+
 
 public class SampleDataAsync : IAsyncSampleData
 {
+
+    // 1.
+    private IAsyncEnumerable<string>? _csvRows;
+
+    public IAsyncEnumerable<string> CsvRows
+    {
+        get => _csvRows!;
+        set => _csvRows = value?.Skip(1) ?? AsyncEnumerable.Empty<string>();
+    }
+
     //1
-    IAsyncEnumerable<string> IAsyncSampleData.CsvRows => throw new NotImplementedException();
+    //We want to load the file async on construction
+    public SampleDataAsync()
+    {
+        CsvRows = File.ReadLinesAsync("People.csv");
+    }
 
     //2
     IAsyncEnumerable<IPerson> IAsyncSampleData.People => throw new NotImplementedException();
