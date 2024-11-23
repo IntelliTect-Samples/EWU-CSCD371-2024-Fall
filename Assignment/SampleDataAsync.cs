94fdc4664a95cbfa113048e211bfb4214f6c7f4d
Brady Dullanty <bdullanty@ewu.edu>
2024-11-23 15:52:17 -0800

    Fixed order to match interface

--- Assignment/SampleDataAsync.cs
+++ Assignment/SampleDataAsync.cs
@@ -14,34 +14,35 @@ public class SampleDataAsync : IAsyncSampleData
         set => _csvRows = value?.Skip(1) ?? AsyncEnumerable.Empty<string>();
     }
 
-    //1
     //We want to load the file async on construction
     public SampleDataAsync()
     {
         CsvRows = File.ReadLinesAsync("People.csv");
     }
-
     //2
-    IAsyncEnumerable<IPerson> IAsyncSampleData.People => throw new NotImplementedException();
-
-    //3
-    public string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people)
+    IAsyncEnumerable<string> IAsyncSampleData.GetUniqueSortedListOfStatesGivenCsvRows()
     {
         throw new NotImplementedException();
     }
-    //4
+    //3
     public string GetAggregateSortedListOfStatesUsingCsvRows()
     {
         throw new NotImplementedException();
     }
+
+
+    //4
+    IAsyncEnumerable<IPerson> IAsyncSampleData.People => throw new NotImplementedException();
+
     //5
     IAsyncEnumerable<(string FirstName, string LastName)> IAsyncSampleData.FilterByEmailAddress(Predicate<string> filter)
     {
         throw new NotImplementedException();
     }
     //6
-    IAsyncEnumerable<string> IAsyncSampleData.GetUniqueSortedListOfStatesGivenCsvRows()
+    public string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people)
     {
         throw new NotImplementedException();
     }
+
 }
