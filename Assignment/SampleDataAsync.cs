1f755e4b996b636171cf0c6fbc3f6b93857ff98d
Brady Dullanty <bdullanty@ewu.edu>
2024-11-23 14:42:38 -0800

    Added SampleDataAsync class

---
+++ Assignment/SampleDataAsync.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Assignment;
+
+public class SampleDataAsync : IAsyncSampleData
+{
+    //1
+    IAsyncEnumerable<string> IAsyncSampleData.CsvRows => throw new NotImplementedException();
+
+    //2
+    IAsyncEnumerable<IPerson> IAsyncSampleData.People => throw new NotImplementedException();
+
+    //3
+    public string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people)
+    {
+        throw new NotImplementedException();
+    }
+    //4
+    public string GetAggregateSortedListOfStatesUsingCsvRows()
+    {
+        throw new NotImplementedException();
+    }
+    //5
+    IAsyncEnumerable<(string FirstName, string LastName)> IAsyncSampleData.FilterByEmailAddress(Predicate<string> filter)
+    {
+        throw new NotImplementedException();
+    }
+    //6
+    IAsyncEnumerable<string> IAsyncSampleData.GetUniqueSortedListOfStatesGivenCsvRows()
+    {
+        throw new NotImplementedException();
+    }
+}
