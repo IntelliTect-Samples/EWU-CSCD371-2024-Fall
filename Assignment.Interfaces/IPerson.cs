2d144559e619b2acfcd8e140d206684a3b0ce64d
dpalmer10 <dpalmer10@ewu.edu>
2024-11-23 14:32:11 -0800

    update namespace scope on interfaces

--- Assignment.Interfaces/IPerson.cs
+++ Assignment.Interfaces/IPerson.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Assignment
-{
-    public interface IPerson
-    {
-        string FirstName { get; }
-        string LastName { get; }
-        IAddress Address { get; }
+namespace Assignment;
 
-        string EmailAddress { get; }
-    }
+public interface IPerson
+{
+    string FirstName { get; }
+    string LastName { get; }
+    IAddress Address { get; }
+
+    string EmailAddress { get; }
 }
