a9af7682ffb7e829bc00a9fded7eff3d587cb8d5
ccornell88 <cjcornell38@gmail.com>
2024-11-23 11:35:49 -0800

    Added Node Test
    Confirm Clear method works properly

--- Assignment/Assignment.Tests/NodeTests.cs
+++ Assignment/Assignment.Tests/NodeTests.cs
@@ -1,6 +1,29 @@
-﻿namespace Assignment.Tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Assignment;
+using System.Globalization;
+using System.Collections;
+
+namespace Assignment.Tests;
 
 [TestClass]
 public class NodeTests
 {
+
+    [TestMethod]
+    public void Clear_list_RemovesNodes()
+    {
+        // Arrange
+        Node<int> node = new(99);
+        node.Append(3);
+        node.Append(8);
+
+        // Act
+        node.Clear();
+
+        // Assert
+        Assert.AreEqual(node, node.Next);
+        Assert.AreSame(node, node.Next);
+    }
+
 }
