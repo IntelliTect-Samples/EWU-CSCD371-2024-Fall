9fb87baaac0a0557c143fa353eee7cd6a00a6d1f
Ben Knoll <bknoll@ewu.edu>
2024-11-24 01:56:50 -0800

    Initial 1-3 + tests
    * Implemented 1-3
    * Added tests for them
    * Some testing still needed

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment.Tests;
+
+[TestClass]
+public class SampleDataTests
+{
+    [TestMethod]
+    public void CsvRows_Initialized_IsNotNull()
+    {
+        // Arrange
+        SampleData sampleData = new();
+
+        // Act
+
+        // Assert
+        Assert.IsNotNull(sampleData.CsvRows);
+    }
+
+    [TestMethod]
+    public void CsvRows_Initialized_CorrectCount()
+    {
+        // Arrange
+        SampleData sampleData = new();
+
+        // Act
+
+        // Assert
+        Assert.AreEqual(sampleData.CsvRows.Count(), 50);
+    }
+
+    [TestMethod]
+    public void CsvRows_Initialized_FirstRowRemoved()
+    {
+        // Arrange
+        SampleData sampleData = new();
+
+        // Act
+
+        // Assert
+        Assert.AreNotEqual("Id,FirstName,LastName,Email,StreetAddress,City,State,Zip", sampleData.CsvRows.First());
+    }
+
+    [TestMethod]
+    public void GetUniqueSortedListOfStates_Called_ReturnsCorrectly()
+    {
+        // Arrange
+        SampleData sampleData = new();
+
+        // Act
+
+        // Assert
+        Assert.AreEqual(27,sampleData.GetUniqueSortedListOfStatesGivenCsvRows
+            ().Count());
+    }
+
+    [TestMethod]
+    public void GetUniqueSortedListOfStates_Called_SortsCorrectly()
+    {
+        // Arrange
+        SampleData sampleData = new();
+
+        // Act
+
+        // Assert
+        Assert.IsTrue(sampleData.GetUniqueSortedListOfStatesGivenCsvRows().First()[0] < sampleData.GetUniqueSortedListOfStatesGivenCsvRows().Last()[0]);
+    }
+
+    [TestMethod]
+    public void GetAggregateSortedListOfStates_Called_ReturnsCorrectly()
+    {
+        // Arrange
+        SampleData sampleData = new();
+
+        // Act
+
+        // Assert
+        Assert.AreEqual("AL,AZ,CA,DC,FL,GA,IN,KS,LA,MD,MN,MO,MT,NC,NE,NH,NV,NY,OR,PA,SC,TN,TX,UT,VA,WA,WV", sampleData.GetAggregateSortedListOfStatesUsingCsvRows());
+    }
+}
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Design;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows { get; } = from line in File.ReadAllLines("People.csv")
+                                                      where !line.Contains("Id,FirstName,LastName,Email,StreetAddress,City,State,Zip")
+                                                      select line;
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+
+            IEnumerable<string> peopleIn = from row in CsvRows.Select(row => row.Split(','))
+                                                     select row[6];
+
+            return (from pe in peopleIn
+                   orderby pe
+                   select pe).Distinct();
+        }
 
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+        {
+            IEnumerable<string> uniqueStates = GetUniqueSortedListOfStatesGivenCsvRows();
+            return string.Join(",", uniqueStates);
+        }
 
         // 4.
         public IEnumerable<IPerson> People
