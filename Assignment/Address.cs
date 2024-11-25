569cde24ea41241fb75693db309f771e5cbcc748
Ben Knoll <bknoll@ewu.edu>
2024-11-25 12:19:17 -0800

    Addressing More Warnings

--- Assignment/Address.cs
+++ Assignment/Address.cs
@@ -1,17 +1,16 @@
-﻿namespace Assignment
+﻿namespace Assignment;
+
+public class Address : IAddress
 {
-    public class Address : IAddress
+    public Address(string streetAddress, string city, string state, string zip)
     {
-        public Address(string streetAddress, string city, string state, string zip)
-        {
-            StreetAddress = streetAddress;
-            City = city;
-            State = state;
-            Zip = zip;
-        }
-        public string StreetAddress { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
+        StreetAddress = streetAddress;
+        City = city;
+        State = state;
+        Zip = zip;
     }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
 }
