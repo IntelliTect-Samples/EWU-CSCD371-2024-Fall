1dd59ee1ff2cfc9c6637e8b37fe811d934b51e21
Eric Vo <tvo12@ewu.edu>
2024-11-24 17:29:45 -0800

    #4 testing half finish

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -83,4 +86,33 @@ public class SampleDataTests
         // Assert
         Assert.AreEqual("AL,AZ,CA,DC,FL,GA,IN,KS,LA,MD,MN,MO,MT,NC,NE,NH,NV,NY,OR,PA,SC,TN,TX,UT,VA,WA,WV", sampleData.GetAggregateSortedListOfStatesUsingCsvRows());
     }
+
+    [TestMethod]
+    public void People_UsingCsvRows_Success()
+    {
+        // Arrange
+        SampleData sampleData = new();
+        List<string> cvsRows = 
+            [
+            "1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577",
+            "2,Karin,Joder,kjoder1@quantcast.com,03594 Florence Park,Tampa,FL,71961",
+            "3,Chadd,Stennine,cstennine2@wired.com,94148 Kings Terrace,Long Beach,CA,59721",
+            "4,Fremont,Pallaske,fpallaske3@umich.edu,16958 Forster Crossing,Atlanta,GA,10687",
+            "5,Melisa,Kerslake,mkerslake4@dion.ne.jp,283 Pawling Parkway,Dallas,TX,88632",
+            "6,Darline,Brauner,dbrauner5@biglobe.ne.jp,33 Menomonie Trail,Atlanta,GA,10687"
+            ];
+
+        sampleData.CsvRows = csvRows;
+        // Act
+        IEnumerable<IPerson> result = sampleData.People;
+        // Assert
+        IEnumerable<IPerson> expected = [];
+        expected = expected.Append(new Person("Chadd", "Stennine", new Address("94148 Kings Terrace", "Long Beach", "CA", "59721"), "cstennine2@wired.com"));
+        expected = expected.Append(new Person("Karin", "Joder", new Address("03594 Florence Park", "Tampa", "FL", "71961"), "kjoder1@quantcast.com"));
+        expected = expected.Append(new Person("Priscilla", "Jenyns", new Address("7884 Corry Way", "Helena", "MT", "70577"), "pjenyns0@state.gov"));
+        expected = expected.Append(new Person("Fremont", "Pallaske", new Address("16958 Forster Crossing", "Atlanta", "GA", "10687"), "fpallaske3@umich.edu"));
+        expected = expected.Append(new Person("Melisa", "Kerslake", new Address("283 Pawling Parkway", "Dallas", "TX", "88632"), "mkerslake4@dion.ne.jp"));
+
+    }
+
 }
