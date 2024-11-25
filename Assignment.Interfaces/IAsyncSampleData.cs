b47cf0207e98188f43af137d7241960d1cdc5fec
Ben Knoll <bknoll@ewu.edu>
2024-11-25 12:15:23 -0800

    Fixed Warnings

--- Assignment.Interfaces/IAsyncSampleData.cs
+++ Assignment.Interfaces/IAsyncSampleData.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Assignment
+namespace Assignment;
+
+public interface IAsyncSampleData
 {
-    public interface IAsyncSampleData
-    {
-        // 1.
-        IAsyncEnumerable<string> CsvRows { get; }
-        
-        // 2.
-        IAsyncEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows();
+    // 1.
+    IAsyncEnumerable<string> CsvRows { get; }
+    
+    // 2.
+    IAsyncEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows();
 
-        // 3.
-        string GetAggregateSortedListOfStatesUsingCsvRows();
+    // 3.
+    string GetAggregateSortedListOfStatesUsingCsvRows();
 
-        // 4.
-        IAsyncEnumerable<IPerson> People { get; }
+    // 4.
+    IAsyncEnumerable<IPerson> People { get; }
 
-        // 5.
-        IAsyncEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter);
+    // 5.
+    IAsyncEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter);
 
-        // 6.
-        string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people);
+    // 6.
+    string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people);
 
-    }
 }
