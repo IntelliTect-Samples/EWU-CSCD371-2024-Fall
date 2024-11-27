69264830f30b2a537ce994d178a75fd214d0712b
Kmiller <kmiller71@ewu.edu>
2024-11-26 23:36:16 -0800

    Added and Edited The Node File
    Added from Assignment 5 and edited to fit new IEnum...

--- Assignment/Assignment.Tests/NodeTests.cs
+++ Assignment/Assignment.Tests/NodeTests.cs
@@ -1,8 +1,119 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 namespace Assignment.Tests;
 
 [TestClass]
+
 public class NodeTests
 {
+    [Fact]
+    public void Node_SetValue_newNodeEqualtoValue()
+    {
+        // Arrange
+        int value = 5;
+
+        // Act
+        Node<int> newNode = new(value);
+
+        // Assert
+        Assert.AreEqual(value, newNode.Value);
+
+    }
+
+    [Fact]
+    public void NodeConstructor_AllowsHomogenousTypes_ValuesEqual()
+    {
+        // Arrange & Act
+        Node<int> intNode = new(10);
+        Node<string> stringNode = new("Hello");
+
+        // Assert
+        Assert.AreEqual(typeof(Node<int>), intNode.GetType());
+        Assert.AreEqual(typeof(Node<string>), stringNode.GetType());
+    }
+
+    [Fact]
+    public void ToString_OverrideToString_ExistsIsEqual()
+    {
+        // Arrange
+        Node<int> intNode = new(10);
+
+        // Act & Assert
+        Assert.AreEqual("10", intNode.ToString());
+    }
+
+    [Fact]
+    public void Next_RefersNextNode_ReturnsNextNodeValue()
+    {
+        // Arrange
+        Node<int> intNode = new(10);
+
+        // Act
+        intNode.Append(20);
+
+        // Assert
+        Assert.AreEqual(20, intNode.Next.Value);
+    }
+
+    [Fact]
+    public void Append_NextPointsBackOriginal_ReturnsIntNodeValue()
+    {
+        // Arrange
+        Node<int> intNode = new(10);
+
+        // Act
+        intNode.Append(20);
+        intNode.Next.Append(30);
+
+        // Assert
+        Assert.AreEqual(10, intNode.Next.Next.Next.Value);
+    }
+
+    [Fact]
+    public void Clear_ResetsNext_NextValueThis()
+    {
+        // Arrange
+        Node<int> nodeOne = new(10);
+        nodeOne.Append(20);
+        nodeOne.Next.Append(30);
+
+        // Act
+        nodeOne.Clear();
+
+        // Assert
+        Assert.AreEqual(10, nodeOne.Next.Value);
+    }
+
+    [Fact]
+    public void Exists_SearchForValue_ReturnsValueIfExists()
+    {
+        // Arrange
+        Node<string> nodeOne = new("one");
+        nodeOne.Append("Two");
+        nodeOne.Next.Append("Three");
+        nodeOne.Next.Next.Append("Four");
+
+        // Act
+        bool exists = nodeOne.Exists("Three");
+
+        // Assert
+        Assert.IsTrue(exists);
+    }
+
+    [Fact]
+    public void Append_DuplicateValue_ThrowsInvalidOperationsError()
+    {
+        // Arrange
+        Node<int> intNode = new(10);
+        intNode.Append(20);
+        intNode.Next.Append(30);
+        intNode.Next.Next.Append(40);
+
+        // Act & Assert
+        Assert.ThrowsException<InvalidOperationException>(() => intNode.Append(30));
+    }
+
+
+
 }
--- Assignment/Assignment/Node.cs
+++ Assignment/Assignment/Node.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,107 @@ using System.Threading.Tasks;
 
 namespace Assignment;
 
-public class Node
+public class Node<T> : IEnumerable<T>
 {
+    public T? Value { get; set; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
+    {
+        Value = value;
+        Next = this;
+    }
+
+    public override string? ToString()
+    {
+        return $"{Value}";
+    }
+
+    public void Append(T value)
+    {
+        if (Exists(value))
+            throw new InvalidOperationException($"Duplicate value exists, not allowed: {value}");
+
+
+        Node<T> newNode = new(value)
+        {
+            Next = this.Next // New Node points back to original.
+        };
+
+        this.Next = newNode; // current node now points to new node, using this. for clarity.
+    }
+
+    public void Clear()
+    {
+        // Check to see if Next is equal to itself, if so, it is the only node in the list.
+        if (this.Next == this)
+            return;
+
+        Node<T> current = this.Next;
+
+        // Traverse the list until we reach the final node.
+        while (current != this)
+        {
+            Node<T> nextNode = current.Next;
+            current.Next = current;
+            current = nextNode;
+        }
+
+        // Set the original node to point to itself. The list is now empty.
+        this.Next = this;
+
+        // Garbage collection isn't a concern after setting this.Next to this because the other nodes are no longer reachable, there is no other variables that host potential to have other nodes.
+        // However, to address the concern of any other potential pointers, we can set each node Next to itself. Garbage collection will take care of the rest.
+    }
+
+    public bool Exists(T? value)
+    {
+        Node<T> current = this;
+
+        // Traverse the list until we circle back to the starting node.
+        do
+        {
+            if (EqualityComparer<T>.Default.Equals(current.Value, value))
+            {
+                return true;
+            }
+            current = current.Next;
+        } while (current != this);
+
+        return false;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node<T> current = this;
+        do
+        {
+            yield return current.Value!;
+            current = current.Next;
+        } while (current != this);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    public IEnumerable<T> ChildItems(int num)
+    {
+        if (num <= 0) yield break;
+
+        int counter = 0;
+        Node<T> start = this;
+        Node<T> current = start;
+
+        do
+        {
+            yield return current.Value!;
+            current = current.Next;
+            counter++;
+
+        } while (counter < num && current != start);
+    }
 }
+
+
+
