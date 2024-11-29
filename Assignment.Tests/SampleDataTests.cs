a9dcbf69e910dd53894c21f383f58efc6de3fee7
Kmiller <kmiller71@ewu.edu>
2024-11-28 16:58:50 -0800

    More GitHub Changes
    fixed Assignment .Tests.csproj, added missing test & fixed Assignment.Tests Method Names

--- Assignment.Tests/SampleDataTests.cs
+++ Assignment.Tests/SampleDataTests.cs
@@ -9,7 +9,7 @@ namespace Assignment.Tests;
 public class SampleDataTests
 {
     [Fact]
-    public void SkipFirstRowCsvRowsSucess()
+    public void SkipFirstRow_CsvRows_Sucess()
     {
         //Arrange
         SampleData sampleData = new();
@@ -22,7 +22,7 @@ public class SampleDataTests
     }
 
     [Fact]
-    public void GetSortedListOfStatesGivenCsvRowSucess()
+    public void GetSortedListOfStates_GivenCsvRow_Sucess()
     {
         //Arrange
         SampleData sampleData = new();
@@ -32,7 +32,7 @@ public class SampleDataTests
     }
 
     [Fact]
-    public void GetAggergateSortedListOfStatesUseingCsvRowsSucess()
+    public void GetAggergateSortedListOfStates_UseingCsvRows_Sucess()
     {
         //Arrange
         SampleData sampleDate = new();
@@ -46,7 +46,7 @@ public class SampleDataTests
     }
 
     [Fact]
-    public void MakePersonListPeopleReturnsPersonCollection()
+    public void MakePersonList_People_ReturnsPersonCollection()
     {
         //Arrange
         SampleData sampleData = new();
@@ -62,12 +62,12 @@ public class SampleDataTests
         Assert.AreEqual("69152", people.ElementAt(3).Address.Zip);
     }
     [Fact]
-    public void filterByEmailEmailAddressSucess() 
+    public void filterByEmail_EmailAddress_Sucess()
     {
         //Arrange
         SampleData sampleData = new();
         // filter for nasa email 
-        Predicate<string> filter = email => email.Contains("nasa.org");
+        Predicate<string> filter = email => email.Contains("nasa.gov");
 
         //Act
         var result = sampleData.FilterByEmailAddress(filter);
@@ -80,7 +80,7 @@ public class SampleDataTests
     }
 
     [Fact]
-    public void GetAggregateListOfStatesGivenPeopleCollectionReturnSucessfuly() 
+    public void GetAggregateListOfStates_GivenPeopleCollection_ReturnSucessfuly()
     {
         //Arrange
         SampleData sampleData = new();
@@ -88,12 +88,35 @@ public class SampleDataTests
 
         //Act
         var stateList = sampleData.GetUniqueSortedListOfStatesGivenCsvRows();
-        var expected = string.Join(",", stateList);
-        
+        var expected = string.Join(", ", stateList);
+
         var result = sampleData.GetAggregateListOfStatesGivenPeopleCollection(people);
 
         //Assert
         Assert.AreEqual(expected, result);
+    }
+
+    [Fact]
+    public void GetUniqueSortedListOfStatesGivenHarcodeData_GivenCsvRoew_Success()
+    {
+        //Arrange
+        SampleData sampleData = new();
+        List<string> csvData = [
+            "1,bob,joe,bobjoe@place.org,8675 North Bute Street,Lakewood Prairie,WA,30900",
+            "2,Korie,Cole,colestopper@trains.org,1627 Ty Merchant,Starlight Cove,OR,14598",
+            "3,Sunfields,Pineboots,sunny@forestprotect.gov,144656 Whitehorse Pend,Whispering Pines River,MI,66895",
+            "4,Luys,FeralWraith,Luyswraith@wraithleagel.com,12122 Bannerdale Close,Willowbrooke Ridge,KS,46567",
+            "5,Aarynn,Gardiner,agardiner@copany.com,159753 S Matson Court,Starview Cove,OR,55230",
+            "6,Raven,Blaze,blazingBird@yahoo.com,14765 Avon Bridge,Cedar Mill Crossing,KS,76958"];
+
+        sampleData.CsvRows = csvData;
+
+        //Act
+        var result = sampleData.GetUniqueSortedListOfStatesGivenCsvRows().ToList();
+        var expected = new List<string> { "KS", "MI", "OR", "WA" };
+
+        //Assert
+        CollectionAssert.AreEqual(expected, result);
     }
 
 }
