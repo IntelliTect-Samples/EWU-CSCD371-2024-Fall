c7ef04219d557b019a13ee04ee5b578bbec885e8
David <david_e_moore@hotmail.com>
2024-12-08 16:04:24 -0800

    Update WildCard.

--- Assignment.Tests/WildCardPattern.cs
+++ Assignment.Tests/WildCardPattern.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace IntelliTect.TestTools
+namespace IntelliTect.TestTools;
 
     /// <summary>
     /// Provides enumerated values to use to set wildcard pattern
