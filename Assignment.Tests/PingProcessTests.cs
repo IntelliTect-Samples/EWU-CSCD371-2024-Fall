3d198b61c2eb01214db55dac0f97a666ac3ae294
ccornell88 <cjcornell38@gmail.com>
2024-12-10 21:02:58 -0800

    error fix

--- Assignment.Tests/PingProcessTests.cs
+++ Assignment.Tests/PingProcessTests.cs
@@ -204,7 +204,7 @@ Ping statistics for ::1:
     Packets: Sent = *, Received = *, Lost = 0 (0% loss),
 Approximate round trip times in milli-seconds:
     Minimum = *, Maximum = *, Average = *".Trim();
-    private void AssertValidPingOutput(int exitCode, string? stdOutput)
+    private static void AssertValidPingOutput(int exitCode, string? stdOutput)
     {
         Assert.IsFalse(string.IsNullOrWhiteSpace(stdOutput));
         stdOutput = WildcardPattern.NormalizeLineEndings(stdOutput!.Trim());
@@ -215,7 +215,6 @@ Approximate round trip times in milli-seconds:
         Assert.AreEqual(0, exitCode);
     }
 
-
-    private void AssertValidPingOutput(PingResult result) =>
+    private static void AssertValidPingOutput(PingResult result) =>
         AssertValidPingOutput(result.ExitCode, result.StdOutput);
 }
