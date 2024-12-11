4064e53bd0a37910f28b9b4e17d7a5be464f24fe
Windeko <tylerjameswoody@gmail.com>
2024-12-11 11:41:23 -0800

    Changed namespace back

--- Assignment.Tests/StringExtensions.cs
+++ Assignment.Tests/StringExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace Assignment.TestTools;
+namespace Assignment.Tests;
 
 /// <summary>
 /// Useful string extensions for performing assertions.
