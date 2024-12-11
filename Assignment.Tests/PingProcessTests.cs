c01723487f9bbeb3b7e79eb20293d84f66c50f5e
Windeko <tylerjameswoody@gmail.com>
2024-12-10 19:28:38 -0800

    More testing

--- Assignment.Tests/PingProcessTests.cs
+++ Assignment.Tests/PingProcessTests.cs
@@ -245,29 +245,29 @@ public class PingProcessTests
         AssertValidPingOutput(result);
     }*/
 
-    private readonly string PingOutputLikeExpression = @"
-    Pinging * with 32 bytes of data:
-    Reply from ::1: time<*
-    Reply from ::1: time<*
-    Reply from ::1: time<*
-    Reply from ::1: time<*
+    //private readonly string PingOutputLikeExpression = @"
+    //Pinging * with 32 bytes of data:
+    //Reply from ::1: time<*
+    //Reply from ::1: time<*
+    //Reply from ::1: time<*
+    //Reply from ::1: time<*
 
-    Ping statistics for ::1:
-        Packets: Sent = *, Received = *, Lost = 0 (0% loss),
-    Approximate round trip times in milli-seconds:
-        Minimum = *, Maximum = *, Average = *".Trim();
+    //Ping statistics for ::1:
+    //    Packets: Sent = *, Received = *, Lost = 0 (0% loss),
+    //Approximate round trip times in milli-seconds:
+    //    Minimum = *, Maximum = *, Average = *".Trim();
 
-    private void AssertValidPingOutput(int exitCode, string? stdOutput)
-    {
-        Assert.IsFalse(string.IsNullOrWhiteSpace(stdOutput));
-        stdOutput = WildcardPattern.NormalizeLineEndings(stdOutput!.Trim());
-        Assert.IsTrue(stdOutput?.IsLike(PingOutputLikeExpression) ?? false,
-            $"Output is unexpected: {stdOutput}");
-        Assert.AreEqual<int>(0, exitCode);
-    }
+    //private void AssertValidPingOutput(int exitCode, string? stdOutput)
+    //{
+    //    Assert.IsFalse(string.IsNullOrWhiteSpace(stdOutput));
+    //    stdOutput = WildcardPattern.NormalizeLineEndings(stdOutput!.Trim());
+    //    Assert.IsTrue(stdOutput?.IsLike(PingOutputLikeExpression) ?? false,
+    //        $"Output is unexpected: {stdOutput}");
+    //    Assert.AreEqual<int>(0, exitCode);
+    //}
 
-    private void AssertValidPingOutput(PingResult result)
-    {
-        AssertValidPingOutput(result.ExitCode, result.StdOutput);
-    }
+    //private void AssertValidPingOutput(PingResult result)
+    //{
+    //    AssertValidPingOutput(result.ExitCode, result.StdOutput);
+    //}
 }
